9fe882f412efb5be8cb0823ec63c27f2429f7c83
Daryl Lim <daryllimyt@gmail.com>
2024-03-07 00:39:07 -0800

    feat(engine): Add webhook table + endpoints and action key slug

--- tracecat/db.py
+++ tracecat/db.py
@@ -1,9 +1,9 @@
-from __future__ import annotations
-
 import os
 from pathlib import Path
 from uuid import uuid4
 
+from pydantic import computed_field
+from slugify import slugify
 from sqlmodel import Field, Relationship, SQLModel, create_engine
 
 
@@ -13,8 +13,9 @@ class Workflow(SQLModel, table=True):
     description: str
     status: str = "offline"  # "online" or "offline"
     object: str | None = None  # JSON-serialized String of react flow object
-    actions: list[Action] | None = Relationship(back_populates="workflow")
-    runs: list[WorkflowRun] | None = Relationship(back_populates="workflow")
+    actions: list["Action"] | None = Relationship(back_populates="workflow")
+    runs: list["WorkflowRun"] | None = Relationship(back_populates="workflow")
+    webhooks: list["Webhook"] | None = Relationship(back_populates="workflow")
 
 
 class WorkflowRun(SQLModel, table=True):
@@ -33,6 +34,28 @@ class Action(SQLModel, table=True):
     inputs: str | None = None  # JSON-serialized String of inputs
     workflow_id: str | None = Field(foreign_key="workflow.id")
     workflow: Workflow | None = Relationship(back_populates="actions")
+
+    @computed_field
+    @property
+    def action_key(self) -> str:
+        slug = slugify(self.title, separator="_")
+        return f"{self.id}.{slug}"
+
+
+class Webhook(SQLModel, table=True):
+    """Webhook is a URL that can be called to trigger a workflow.
+
+    Notes
+    -----
+    - We need this because we need a way to trigger a workflow from an external source.
+    - External sources only have access to the path
+    """
+
+    id: str | None = Field(default_factory=lambda: uuid4().hex, primary_key=True)
+    secret: str
+    action_id: str | None = Field(foreign_key="action.id")
+    workflow_id: str | None = Field(foreign_key="workflow.id")
+    workflow: Workflow | None = Relationship(back_populates="webhooks")
 
 
 def create_db_engine():
