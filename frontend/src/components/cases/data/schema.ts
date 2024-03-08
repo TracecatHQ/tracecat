db31d3fed0a8b470160c92bf3ebe1d0017f83e1f
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2024-03-08 23:56:43 +0000

    feat(engine): Add log indexer and vectordb engines

--- frontend/src/components/cases/data/schema.ts
+++ frontend/src/components/cases/data/schema.ts
@@ -4,10 +4,10 @@ import { z } from "zod"
 export const caseSchema = z.object({
   id: z.number().int(),
   title: z.string(),
-  payload:z.record(z.string()).transform((val) => JSON.stringify(val)),
+  payload: z.record(z.string()).transform((val) => JSON.stringify(val)),
   malice: z.enum(["malicious", "benign"]),
+  context: z.record(z.string()),
   action: z.array(z.string()).transform((val) => val[0]),
-  context: z.array(z.string()),
   suppression: z.record(z.boolean()).transform((val) => JSON.stringify(val)),
   status: z.string(),
   priority: z.enum(["low", "medium", "high", "critical"]),
