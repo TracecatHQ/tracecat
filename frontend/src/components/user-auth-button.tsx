6876d0f5e1475474b48f7c340951e7cba77b6658
Daryl Lim <daryllimyt@gmail.com>
2024-03-09 01:51:32 -0800

    feat(ui): Add basic auth flow

---
+++ frontend/src/components/user-auth-button.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link"
+import { redirect } from "next/navigation"
+import { createClient } from "@/utils/supabase/server"
+
+import { Button } from "./ui/button"
+
+export default async function AuthButton() {
+  const supabase = createClient()
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  const signOut = async () => {
+    "use server"
+
+    const supabase = createClient()
+    await supabase.auth.signOut()
+    return redirect("/login")
+  }
+
+  return user ? (
+    <div className="flex items-center gap-4">
+      Hi, {user.email}
+      <form action={signOut}>
+        <Button className="bg-btn-background hover:bg-btn-background-hover rounded-md px-4 py-2 no-underline">
+          Logout
+        </Button>
+      </form>
+    </div>
+  ) : (
+    <Link
+      href="/login"
+      className="bg-btn-background hover:bg-btn-background-hover flex rounded-md px-3 py-2 no-underline"
+    >
+      Login
+    </Link>
+  )
+}
--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,9 +1,19 @@
-import { NextResponse, type NextRequest } from "next/server"
+import { type NextRequest } from "next/server"
+import { updateSession } from "@/utils/supabase/middleware"
 
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL(`/workflows`, request.url))
+export async function middleware(request: NextRequest) {
+  return await updateSession(request)
 }
 
 export const config = {
-  matcher: "/:path", // Match only on root path
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
 }
