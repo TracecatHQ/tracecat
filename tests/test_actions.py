bb44d25d4002c6bd3b056056e7f6b6da799b30e9
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2024-03-10 23:09:51 +0000

    feat(engine)!: Replace SMTP with resend SaaS API

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -7,6 +7,7 @@ from tracecat.actions import (
     DEFAULT_TEMPLATE_PATTERN,
     evaluate_jsonpath_str,
     evaluate_templated_fields,
+    run_send_email_action,
 )
 
 
@@ -249,3 +250,77 @@ def test_evaluate_templated_fields_raises_exception():
         evaluate_templated_fields(
             action_kwargs=mock_templated_kwargs, action_trail_json=mock_json_data
         )
+
+
+@pytest.mark.webtest
+@pytest.mark.asyncio
+async def test_send_email_action_all_success():
+    test_sender = "Mewtwo <test@tracecat.com>"
+    recipients = ["chris+test@tracecat.com", "daryl+test@tracecat.com"]
+    subject = "[TEST] Hello from Tracecat!"
+    body = "Meow! 🐱"
+    email_response = await run_send_email_action(
+        sender=test_sender,
+        recipients=recipients,
+        subject=subject,
+        body=body,
+    )
+    assert email_response == {
+        "status": "ok",
+        "message": "Successfully sent email",
+        "provider": "resend",
+        "sender": test_sender,
+        "recipients": recipients,
+        "subject": subject,
+        "body": body,
+    }
+
+
+@pytest.mark.webtest
+@pytest.mark.asyncio
+async def test_send_email_action_unrecognized_provider():
+    test_sender = "Mewtwo <test@tracecat.com>"
+    recipients = ["chris+test@tracecat.com", "daryl+test@tracecat.com"]
+    subject = "[TEST] Hello from Tracecat!"
+    body = "Meow! 🐱"
+    provider = "e-meow"
+    email_response = await run_send_email_action(
+        sender=test_sender,
+        recipients=recipients,
+        subject=subject,
+        body=body,
+        provider=provider,
+    )
+    assert email_response == {
+        "status": "error",
+        "message": "Email provider not recognized",
+        "provider": provider,
+        "sender": test_sender,
+        "recipients": recipients,
+        "subject": subject,
+        "body": body,
+    }
+
+
+@pytest.mark.webtest
+@pytest.mark.asyncio
+async def test_send_email_action_bounced():
+    test_sender = "Mewtwo <test@tracecat.com>"
+    recipients = ["doesnotexist@tracecat.com"]
+    subject = "[TEST] Hello from Tracecat!"
+    body = "Meow! 🐱"
+    email_response = await run_send_email_action(
+        sender=test_sender,
+        recipients=recipients,
+        subject=subject,
+        body=body,
+    )
+    assert email_response == {
+        "status": "warning",
+        "message": "Email bounced",
+        "provider": "resend",
+        "sender": test_sender,
+        "recipients": recipients,
+        "subject": subject,
+        "body": body,
+    }
--- tracecat/mail.py
+++ tracecat/mail.py
@@ -1,134 +1,124 @@
-import asyncio
+import os
 import re
-import time
-from collections.abc import Sequence
-from typing import Any
+from abc import abstractmethod
 
-import aiosmtplib
-import yagmail
+import httpx
+from tenacity import retry, stop_after_attempt, wait_exponential
 
-EMAIL_PATTERN = re.compile(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
+SAFE_EMAIL_PATTERN = re.compile(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
 
 
-class AsyncSMTPClient(yagmail.SMTP):
-    """Wrapper around aioyagmail.SMTP."""
+class EmailBouncedError(Exception):
+    pass
 
-    async def login(self):
-        # aiosmtplib implementation specific
-        use_tls = str(self.port) == "465"
-        self.smtp_starttls = not use_tls
-        if self.oauth2_file is not None:
-            await self._login_oauth2(self.credentials, use_tls)
-        else:
-            await self._login(self.credentials, use_tls=use_tls)
 
-    async def _login_oauth2(self, oauth2_info: dict[str, Any], use_tls: bool) -> None:
-        if "email_address" in oauth2_info:
-            oauth2_info.pop("email_address")
-        self.smtp = self.connection(
-            hostname=self.host,
-            port=self.port,
-            use_tls=use_tls,
-            **self.kwargs,
-        )
-        await self.smtp.connect()
-        auth_string = self.get_oauth_string(self.user, oauth2_info)
-        await self.smtp.ehlo(hostname=oauth2_info["google_client_id"])
-        if self.starttls is True:
-            await self.smtp.starttls()
-        await self.smtp.execute_command(
-            b"AUTH", b"XOAUTH2", bytes(auth_string, "ascii")
-        )
+class EmailNotFoundError(Exception):
+    pass
 
-    @property
-    def connection(self):
-        return aiosmtplib.SMTP
 
-    async def send(
+class AsyncMailProvider:
+    def __init__(
         self,
-        to: str | None = None,
-        subject: str | None = None,
-        contents: str | None = None,
-        attachments: str | list[str] | None = None,
-        cc: str | list[str] | None = None,
+        sender: str,
+        recipients: str | list[str],
+        subject: str,
+        body: str | None = None,
         bcc: str | list[str] | None = None,
-        preview_only: bool = False,
-        headers: Any | None = None,
-    ) -> tuple[str, str] | bool:
-        """Use this to send an email with gmail"""
-        recipients, msg_string = self.prepare_send(
-            to, subject, contents, attachments, cc, bcc, headers
+        cc: str | list[str] | None = None,
+        reply_to: str | list[str] | None = None,
+        headers: dict[str, str] | None = None,
+    ):
+        self.sender = sender
+        self.recipients = recipients
+        self.subject = subject
+        self.body = body
+        self.bcc = bcc
+        self.cc = cc
+        self.reply_to = reply_to
+        self.headers = headers
+
+    @abstractmethod
+    async def _send(
+        self,
+        sender: str,
+        recipients: str | list[str],
+        subject: str,
+        body: str | None = None,
+        bcc: str | list[str] | None = None,
+        cc: str | list[str] | None = None,
+        reply_to: str | list[str] | None = None,
+        headers: dict[str, str] | None = None,
+    ):
+        pass
+
+    async def send(self):
+        await self._send(
+            sender=self.sender,
+            recipients=self.recipients,
+            subject=self.subject,
+            body=self.body,
+            bcc=self.bcc,
+            cc=self.cc,
+            reply_to=self.reply_to,
+            headers=self.headers,
         )
-        if preview_only:
-            return (recipients, msg_string)
-        return await self._attempt_send(recipients, msg_string)
 
-    async def _attempt_send(
-        self, recipients: str | Sequence[str], msg_string: str | bytes
-    ) -> dict[str, str] | bool:
-        attempts = 0
-        while attempts < 3:
-            try:
-                result = await self.smtp.sendmail(self.user, recipients, msg_string)
-                self.log.info("Message sent to %s", recipients)
-                self.num_mail_sent += 1
-                return result
-            except aiosmtplib.SMTPServerDisconnected as e:
-                self.log.error(e)
-                attempts += 1
-                time.sleep(attempts * 3)
-        self.unsent.append((recipients, msg_string))
-        return False
 
-    async def send_unsent(self):
-        """
-        Emails that were not being able to send will be stored in :attr:`self.unsent`.
-        Use this function to attempt to send these again
-        """
-        await asyncio.gather([self._attempt_send(*x) for x in self.unsent])
-        while self.unsent:
-            futures = [self._attempt_send(*self.unsent.pop()) for x in self.unsent]
-            await asyncio.gather(*futures)
+class ResendMailProvider(AsyncMailProvider):
+    @property
+    def api_headers(self):
+        api_key = os.environ["RESEND_API_KEY"]
+        api_headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {api_key}",
+        }
+        return api_headers
 
-    async def close(self):
-        raise ValueError("Should be `async with`")
+    @retry(stop=stop_after_attempt(3), wait=wait_exponential())
+    async def _get_email_status(self, client: httpx.AsyncClient, email_id: str):
+        email_response = await client.get(
+            f"https://api.resend.com/emails/{email_id}", headers=self.api_headers
+        )
+        email_response.raise_for_status()
+        email_status = email_response.json().get("last_event")
+        if email_status is None:
+            raise Exception("Email status is None")
+        return email_status
 
-    async def __aenter__(self):
-        await self.login()
-        return self
+    async def _send(
+        self,
+        sender: str,
+        recipients: str | list[str],
+        subject: str,
+        body: str | None = None,
+        bcc: str | list[str] | None = None,
+        cc: str | list[str] | None = None,
+        reply_to: str | list[str] | None = None,
+        headers: dict[str, str] | None = None,
+    ):
+        api_headers = self.api_headers
+        params = {
+            "from": sender,
+            "to": recipients,
+            "subject": subject,
+            "text": body,
+            "bcc": bcc,
+            "cc": cc,
+            "reply_to": reply_to,
+            "headers": headers,
+        }
+        async with httpx.AsyncClient() as client:
+            rsps = await client.post(
+                "https://api.resend.com/emails", json=params, headers=api_headers
+            )
+            rsps.raise_for_status()
+            email_id = rsps.json()["id"]
+            email_status = await self._get_email_status(client, email_id)
+            if email_status is None:
+                raise EmailNotFoundError(
+                    "Successfully posted email, but failed to find email in Resend after 3 retries."
+                )
+            elif email_status == "bounced":
+                raise EmailBouncedError("Email bounced")
 
-    async def __exit(self):
-        if not self.is_closed:
-            await self.aclose()
-        return False
-
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
-        if not self.is_closed:
-            await self.aclose()
-        return False
-
-    async def aclose(self):
-        """Close the connection to the SMTP server"""
-        self.is_closed = True
-        try:
-            await self.smtp.quit()
-        except (TypeError, AttributeError, aiosmtplib.SMTPServerDisconnected):
-            pass
-
-    async def _login(self, password: str, use_tls: bool | None = None) -> None:
-        """
-        Login to the SMTP server using password. `login` only needs to be manually run when the
-        connection to the SMTP server was closed by the user.
-        """
-        self.smtp = self.connection(hostname=self.host, port=self.port)
-        await self.smtp.connect(port=self.port, use_tls=use_tls)
-        if self.starttls:
-            await self.smtp.starttls()
-        if not self.smtp_skip_login:
-            password = self.handle_password(self.user, password)
-            await self.smtp.login(self.user, password)
-        self.is_closed = False
-
-    def __del__(self):
-        """Not required in async"""
-        return NotImplemented
+        return email_status
