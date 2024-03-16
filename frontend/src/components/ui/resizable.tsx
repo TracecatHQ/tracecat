224825cf107f1a566a1234293b5f569ef2a557e4
Daryl Lim <daryllimyt@gmail.com>
2024-03-16 16:40:25 -0700

    feat(ui): Add custom resiable handle

--- frontend/src/components/ui/resizable.tsx
+++ frontend/src/components/ui/resizable.tsx
@@ -42,4 +42,29 @@ const ResizableHandle = ({
   </ResizablePrimitive.PanelResizeHandle>
 )
 
-export { ResizablePanelGroup, ResizablePanel, ResizableHandle }
+const CustomResizableHandle = ({
+  className,
+  children,
+  ...props
+}: React.ComponentProps<typeof ResizablePrimitive.PanelResizeHandle> & {
+  children: React.ReactNode
+}) => (
+  <ResizablePrimitive.PanelResizeHandle
+    className={cn(
+      "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
+      className
+    )}
+    {...props}
+  >
+    <div className="z-10 flex h-5 w-5 items-center justify-center rounded-sm border bg-border">
+      {children}
+    </div>
+  </ResizablePrimitive.PanelResizeHandle>
+)
+
+export {
+  ResizablePanelGroup,
+  ResizablePanel,
+  ResizableHandle,
+  CustomResizableHandle,
+}
