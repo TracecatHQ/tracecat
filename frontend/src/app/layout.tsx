df23b91d2e492cb7b5fc883ce81be5687ffa8178
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2024-03-19 11:46:07 +0000

    feat(ui): Setup posthog analytics

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css"
 
 import { Metadata } from "next"
+import dynamic from "next/dynamic"
 import { SessionContextProvider } from "@/providers/session"
 import { createClient } from "@/utils/supabase/server"
 
@@ -22,28 +23,62 @@ interface RootLayoutProps {
   children: React.ReactNode
 }
 
+let PostHogPageView: any
+let PHProvider: any
+
+if (process.env.NEXT_PUBLIC_APP_ENV === "prod") {
+  PostHogPageView = dynamic(
+    () => import("@/components/analytics/PostHogPageView"),
+    {
+      ssr: false,
+    }
+  )
+  PHProvider = require("@/providers/posthog").PHProvider
+  console.log("PostHog initialized for production environment.")
+}
+
 export default async function RootLayout({ children }: RootLayoutProps) {
   const supabase = createClient()
 
   const {
     data: { session },
   } = await supabase.auth.getSession()
+
   return (
     <>
       <html lang="en" className="h-full min-h-screen" suppressHydrationWarning>
         <head />
-        <body
-          className={cn(
-            "h-screen min-h-screen overflow-hidden bg-background font-sans antialiased",
-            fontSans.className
-          )}
-        >
-          <SessionContextProvider initialSession={session}>
-            {children}
-          </SessionContextProvider>
-          <Toaster />
-        </body>
+        {PHProvider ? (
+          <PHProvider>
+            <BodyContent session={session} children={children} />
+          </PHProvider>
+        ) : (
+          <BodyContent session={session} children={children} />
+        )}
       </html>
     </>
+  )
+}
+
+function BodyContent({
+  session,
+  children,
+}: {
+  session: any
+  children: React.ReactNode
+}) {
+  return (
+    <body
+      className={cn(
+        "h-screen min-h-screen overflow-hidden bg-background font-sans antialiased",
+        fontSans.className
+      )}
+    >
+      <SessionContextProvider initialSession={session}>
+        {PostHogPageView && <PostHogPageView />}
+        {children}
+      </SessionContextProvider>
+      <Toaster />
+    </body>
   )
 }
