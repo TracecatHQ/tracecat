dc1534b954b9c0420dd763525b882e81848caec9
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2024-03-19 11:37:39 +0000

    chore(ui): Update site description

--- frontend/src/config/site.ts
+++ frontend/src/config/site.ts
@@ -3,9 +3,9 @@ import { SiteConfig } from "@/types"
 import { env } from "@/env.mjs"
 
 export const siteConfig: SiteConfig = {
-  name: "Next Entree",
+  name: "Tracecat",
   author: "Tracecat",
-  description: "The AI-native Tines alternative.",
+  description: "The AI-native Tines / Palo Alto XSOAR alternative.",
   keywords: ["Next.js", "React", "Tailwind CSS", "Radix UI", "shadcn/ui"],
   url: {
     base: env.NEXT_PUBLIC_APP_URL,
