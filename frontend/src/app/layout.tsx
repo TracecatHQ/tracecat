932706c969e571b9cadd9c2c0e6a14c73c5554ef
Daryl Lim <daryllimyt@gmail.com>
2024-03-21 17:43:41 -0700

    Revert "fix(ui): Try fix missing font bug by adding link to css"
    This reverts commit e632a786fc580700718b3800f233158dafbbe660.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -48,12 +48,7 @@ export default async function RootLayout({ children }: RootLayoutProps) {
   return (
     <>
       <html lang="en" className="h-full min-h-screen" suppressHydrationWarning>
-        <head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-            rel="stylesheet"
-          />
-        </head>
+        <head />
         {PHProvider ? (
           <PHProvider>
             <BodyContent session={session}>{children}</BodyContent>
