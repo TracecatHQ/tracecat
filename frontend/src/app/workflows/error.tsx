1aa0ad51da510c8ea18c3f8e9f9e297c85670c72
Daryl Lim <daryllimyt@gmail.com>
2024-03-22 15:07:18 -0700

    feat(ui): Improve page error handling

--- frontend/src/app/workflows/error.tsx
+++ frontend/src/app/workflows/error.tsx
@@ -2,7 +2,11 @@
 
 // Error components must be Client Components
 import { useEffect } from "react"
+import Image from "next/image"
+import Link from "next/link"
+import TracecatIcon from "public/icon.png"
 
+import { Button } from "@/components/ui/button"
 import { AlertNotification } from "@/components/notifications"
 
 export default function Error({
@@ -18,8 +22,15 @@ export default function Error({
   }, [error])
 
   return (
-    <main className="container flex h-full w-full max-w-[400px] items-center justify-center">
-      <AlertNotification level="error" message={error.message} reset={reset} />
+    <main className="container flex h-full w-full max-w-[400px] flex-col items-center justify-center space-y-4">
+      <Image src={TracecatIcon} alt="Tracecat" className="mb-8 h-16 w-16" />
+      <h1 className="text-2xl font-medium">Oh no! An error occurred :(</h1>
+
+      <Link href="/" className="">
+        <Button variant="outline">Return to the home page</Button>
+      </Link>
+
+      <AlertNotification level="error" message={error.message} />
     </main>
   )
 }
