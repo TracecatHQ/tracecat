841def45f13e41b86014741f6ebc9231000e1d62
Daryl Lim <daryllimyt@gmail.com>
2024-03-24 01:29:49 -0700

    chore(engine): Delete unused path for old gmail oauth

--- tracecat/config.py
+++ tracecat/config.py
@@ -1,13 +1,9 @@
 import os
-from pathlib import Path
 
 HTTP_MAX_RETRIES = 6
 LLM_MAX_RETRIES = 3
 
 TRACECAT__APP_ENV = os.environ.get("TRACECAT__APP_ENV", "dev")
-TRACECAT__OAUTH2_GMAIL_PATH = (
-    Path("~/tracecat-runner-client-secret.json").expanduser().resolve()
-)
 TRACECAT__API_URL = os.environ.get("TRACECAT__API_URL", "http://api:8000")
 TRACECAT__RUNNER_URL = os.environ.get("TRACECAT__RUNNER_URL", "http://runner:8000")
 
