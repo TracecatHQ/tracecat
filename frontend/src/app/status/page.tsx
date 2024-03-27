e0b632a011c018dec0ee00074e7a05ec9ec85a68
Daryl Lim <daryllimyt@gmail.com>
2024-03-27 03:25:54 -0700

    feat(ui): Add status page controlled by vercel edge config

---
+++ frontend/src/app/status/page.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image"
+import TracecatIcon from "public/icon.png"
+
+import { AlertNotification } from "@/components/notifications"
+
+export default async function Page() {
+  return (
+    <main className="container flex h-full w-full max-w-[400px] flex-col items-center justify-center space-y-4">
+      <Image src={TracecatIcon} alt="Tracecat" className="mb-8 h-16 w-16" />
+      <h1 className="text-lg font-semibold">
+        We're sorry for the inconvenience.
+      </h1>
+      <AlertNotification
+        level="info"
+        className="text-2xl font-medium"
+        message="Our site is currently down for maintenance. Please check our Discord for
+        updates."
+      />
+    </main>
+  )
+}
--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,7 +1,13 @@
-import { type NextRequest } from "next/server"
+import { NextResponse, type NextRequest } from "next/server"
 import { updateSession } from "@/utils/supabase/middleware"
+import { get } from "@vercel/edge-config"
 
 export async function middleware(request: NextRequest) {
+  if (await get("isUnderMaintenance")) {
+    request.nextUrl.pathname = `/status`
+    console.log("Redirecting to status page")
+    return NextResponse.rewrite(request.nextUrl)
+  }
   return await updateSession(request)
 }
 
@@ -15,5 +21,6 @@ export const config = {
      * Feel free to modify this pattern to include more paths.
      */
     "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/status",
   ],
 }
