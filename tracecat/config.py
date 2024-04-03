ab21dcff7a7e16b2e08855c88920f595606c8380
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2024-04-03 06:27:39 +0000

    feat: Register tracecat-scheduler service role

--- tracecat/config.py
+++ tracecat/config.py
@@ -11,4 +11,8 @@ TRACECAT__APP_ENV = os.environ.get("TRACECAT__APP_ENV", "dev")
 TRACECAT__API_URL = os.environ.get("TRACECAT__API_URL", "http://api:8000")
 TRACECAT__RUNNER_URL = os.environ.get("TRACECAT__RUNNER_URL", "http://runner:8000")
 
-TRACECAT__SERVICE_ROLES_WHITELIST = ["tracecat-runner", "tracecat-api"]
+TRACECAT__SERVICE_ROLES_WHITELIST = [
+    "tracecat-runner",
+    "tracecat-api",
+    "tracecat-scheduler",
+]
