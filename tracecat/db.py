a93214916bbfb14828c83557fc73e0fad69349c2
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2024-04-03 07:36:59 +0000

    feat: Change default sqlite uri path

--- tracecat/db.py
+++ tracecat/db.py
@@ -33,7 +33,7 @@ DEFAULT_CASE_ACTIONS = [
     "Sinkholed",
 ]
 STORAGE_PATH.mkdir(parents=True, exist_ok=True)
-TRACECAT__DB_URI = os.environ.get("TRACECAT__DB_URI", "sqlite:///tracecat.db")
+TRACECAT__DB_URI = os.environ.get("TRACECAT__DB_URI", "sqlite:///.tracecat/database.db")
 
 
 class User(SQLModel, table=True):
