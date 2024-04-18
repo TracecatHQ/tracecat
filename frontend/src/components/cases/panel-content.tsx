0863e92adc4138130b06e8ce389307e845ced9d8
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-04-18 16:26:42 +0100

    feat(ui): Standardize styles in case sliding panel

--- frontend/src/components/cases/panel-content.tsx
+++ frontend/src/components/cases/panel-content.tsx
@@ -2,10 +2,9 @@
 
 import React from "react"
 import SyntaxHighlighter from "react-syntax-highlighter"
-import { atomOneLight } from "react-syntax-highlighter/dist/esm/styles/hljs"
+import { atomOneDark } from "react-syntax-highlighter/dist/esm/styles/hljs"
 
 import { Case } from "@/types/schemas"
-import { ScrollArea } from "@/components/ui/scroll-area"
 import {
   Select,
   SelectContent,
@@ -20,12 +19,14 @@ import { SheetHeader, SheetTitle } from "@/components/ui/sheet"
 import { StatusBadge } from "@/components/badges"
 import { statuses } from "@/components/cases/data/categories"
 
+type TStatus = (typeof statuses)[number]
+
 interface CasePanelContentProps {
-  caseData: Case
+  currentCase: Case
 }
 
-export function CasePanelContent({ caseData }: CasePanelContentProps) {
-  const {
+export function CasePanelContent({
+  currentCase: {
     id,
     title,
     priority,
@@ -33,7 +34,8 @@ export function CasePanelContent({ caseData }: CasePanelContentProps) {
     payload,
     context,
     status: caseStatus,
-  } = caseData
+  },
+}: CasePanelContentProps) {
   const currentStatus = statuses.find((status) => status.value === caseStatus)
   const renderedPayload = JSON.stringify(payload, null, 2)
   const renderedContext = context
@@ -41,60 +43,81 @@ export function CasePanelContent({ caseData }: CasePanelContentProps) {
     : "No context available"
 
   return (
-    <ScrollArea className="h-full">
-      <div className="flex flex-col space-y-4">
-        <SheetHeader>
-          <small>Case #{id}</small>
-          <div className="flex items-center justify-between">
-            <SheetTitle className="text-md">{title}</SheetTitle>
-            <Select defaultValue={currentStatus?.value}>
-              <SelectTrigger className="w-40">
-                <SelectValue />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-                  <SelectLabel>Status</SelectLabel>
-                  {statuses.map((status) => (
-                    <SelectItem key={status.value} value={status.value}>
-                      <span className="flex items-center">
-                        {status.icon && (
-                          <status.icon className="mr-2 h-4 w-4 text-muted-foreground" />
-                        )}
-                        {status.label}
-                      </span>
-                    </SelectItem>
-                  ))}
-                </SelectGroup>
-              </SelectContent>
-            </Select>
-          </div>
-          <div className="flex items-center space-x-2">
-            <StatusBadge status={priority ?? ""}>
-              priority: {priority}
-            </StatusBadge>
-            <StatusBadge status={malice ?? ""}>{malice}</StatusBadge>
-          </div>
-        </SheetHeader>
-        <Separator />
-        <div className="flex flex-col space-y-4 text-sm">
-          <div className="flex flex-col space-y-2">
-            <div className="text-md">Payload</div>
-            <SyntaxHighlighter language="json" style={atomOneLight}>
-              {renderedPayload}
-            </SyntaxHighlighter>
-          </div>
-          <div className="flex flex-col space-y-2">
-            <div className="text-md">Context</div>
-            <pre>
-              <SyntaxHighlighter language="json" style={atomOneLight}>
-                {renderedContext}
-              </SyntaxHighlighter>
-            </pre>
-          </div>
-          <div className="text-md">Runbook</div>
-          <div className="text-md">Metrics</div>
+    <div className="flex flex-col space-y-4 overflow-auto">
+      <SheetHeader>
+        <small>Case #{id}</small>
+        <div className="flex items-center justify-between">
+          <SheetTitle className="text-md">{title}</SheetTitle>
+          <CaseStatusSelect status={currentStatus} />
         </div>
+        <div className="flex items-center space-x-2">
+          <StatusBadge status={priority ?? ""}>
+            priority: {priority}
+          </StatusBadge>
+          <StatusBadge status={malice ?? ""}>{malice}</StatusBadge>
+        </div>
+      </SheetHeader>
+      <Separator />
+      <div className="flex flex-col space-y-4 text-sm">
+        <div className="flex flex-col space-y-2">
+          <h5 className="text-xs font-semibold">Payload</h5>
+          <CodeContent data={renderedPayload} />
+        </div>
+        <div className="flex flex-col space-y-2">
+          <h5 className="text-xs font-semibold">Context</h5>
+          <pre>
+            <CodeContent data={renderedContext} />
+          </pre>
+        </div>
+        <h5 className="text-xs font-semibold">Actions</h5>
       </div>
-    </ScrollArea>
+    </div>
+  )
+}
+
+function CaseStatusSelect({ status }: { status?: TStatus }) {
+  return (
+    <Select defaultValue={status?.value}>
+      <SelectTrigger className="w-40 focus:ring-0">
+        <SelectValue />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Status</SelectLabel>
+          {statuses.map((status) => (
+            <SelectItem key={status.value} value={status.value}>
+              <span className="flex items-center text-xs">
+                {status.icon && (
+                  <status.icon className="mr-2 h-4 w-4 text-muted-foreground" />
+                )}
+                {status.label}
+              </span>
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  )
+}
+
+function CodeContent({ data }: { data: string }) {
+  return (
+    <SyntaxHighlighter
+      language="json"
+      style={atomOneDark}
+      wrapLines
+      customStyle={{
+        width: "100%",
+        maxWidth: "100%",
+        overflowX: "auto",
+      }}
+      codeTagProps={{
+        className:
+          "text-xs text-background rounded-md max-w-full overflow-auto",
+      }}
+      className="w-full max-w-full overflow-auto rounded-md p-4"
+    >
+      {data}
+    </SyntaxHighlighter>
   )
 }
