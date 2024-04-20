e86f0c5f06ccbae3bda5f169b35401907625ddba
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-04-20 21:16:52 +0100

    refactor(ui): Simplify badge color logic

--- frontend/src/components/badges.tsx
+++ frontend/src/components/badges.tsx
@@ -5,53 +5,29 @@ import { Badge } from "@/components/ui/badge"
 
 interface StatusBadgeProps
   extends PropsWithChildren<React.HTMLAttributes<HTMLElement>> {
-  status: string
+  status?: string
+}
+
+const statusColors = {
+  medium: "bg-blue-100 border-blue-400 text-blue-700",
+  high: "bg-orange-100 border-orange-400 text-orange-700",
+  critical: "bg-red-100 border-red-400 text-red-700",
+  malicious: "bg-red-100 border-red-400 text-red-700",
+  success: "bg-green-100 border-green-400 text-green-700",
+  benign: "bg-green-100 border-green-400 text-green-700",
 }
 
 export function StatusBadge({ status, children }: StatusBadgeProps) {
-  let bg_color
-  let border_color
-  let text_color
-
-  switch (status) {
-    case "medium":
-      bg_color = "bg-blue-100"
-      border_color = "border-blue-400"
-      text_color = "text-blue-700"
-      break
-    case "high":
-      bg_color = "bg-orange-100"
-      border_color = "border-orange-400"
-      text_color = "text-orange-700"
-      break
-    case "critical":
-      bg_color = "bg-red-100"
-      border_color = "border-red-400"
-      text_color = "text-red-700"
-      break
-    case "malicious":
-      bg_color = "bg-red-100"
-      border_color = "border-red-400"
-      text_color = "text-red-700"
-      break
-    case "critical":
-      bg_color = "bg-red-100"
-      border_color = "border-red-400"
-      text_color = "text-red-700"
-      break
-    case "success":
-      bg_color = "bg-green-100"
-      border_color = "border-green-400"
-      text_color = "text-green-700"
-      break
-    default:
-      bg_color = "bg-gray-100"
-      border_color = "border-gray-400"
-      text_color = "text-gray-700"
-  }
-
   return (
-    <Badge variant="outline" className={cn(bg_color, border_color, text_color)}>
+    <Badge
+      variant="outline"
+      className={cn(
+        "border-gray-400 bg-gray-100 text-gray-700",
+        status &&
+          status in statusColors &&
+          statusColors[status as keyof typeof statusColors]
+      )}
+    >
       {children}
     </Badge>
   )
