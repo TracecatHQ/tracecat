288ad7dd08166c7495a00660c54907453f652f27
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-04-21 16:39:54 +0100

    fix(ui): Prevent checkbox from propagating click event (#81)

--- frontend/src/components/cases/columns.tsx
+++ frontend/src/components/cases/columns.tsx
@@ -28,6 +28,7 @@ export const columns: ColumnDef<Case>[] = [
     ),
     cell: ({ row }) => (
       <Checkbox
+        onClick={(e) => e.stopPropagation()}
         checked={row.getIsSelected()}
         onCheckedChange={(value) => row.toggleSelected(!!value)}
         aria-label="Select row"
