c49cd5ad3069a3ddbe30a30ab3606c2137f7eb2e
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2024-04-22 16:56:46 +0000

    fix: Replace model_dump with model_dump_json

--- tests/integrations/test_urlscan.py
+++ tests/integrations/test_urlscan.py
@@ -11,8 +11,8 @@ def urlscan_secret(create_mock_secret) -> dict[str, str | bytes]:
     mock_secret = create_mock_secret(
         "urlscan", {"URLSCAN_API_KEY": os.environ["URLSCAN_API_KEY"]}
     )
-    serialized_secret = mock_secret.model_dump()
-    return serialized_secret
+    mock_secret_obj = mock_secret.model_dump_json()
+    return mock_secret_obj
 
 
 def test_analyze_url_live(urlscan_secret, respx_mock):
@@ -20,7 +20,7 @@ def test_analyze_url_live(urlscan_secret, respx_mock):
 
     # Mock secrets manager
     route = respx_mock.get(f'{os.environ["TRACECAT__API_URL"]}/secrets/urlscan').mock(
-        return_value=Response(status_code=200, json=urlscan_secret)
+        return_value=Response(status_code=200, content=urlscan_secret)
     )
     result = analyze_url(test_url)
 
