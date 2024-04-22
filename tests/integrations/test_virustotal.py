240a5e06096bc97cb09cc24eab06e617023c78b4
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2024-04-22 08:38:37 +0000

    test: Mock secrets endpoints

--- tests/integrations/test_virustotal.py
+++ tests/integrations/test_virustotal.py
@@ -1,4 +1,7 @@
+import os
+
 import pytest
+from httpx import Response
 
 from tracecat.integrations.virustotal import (
     get_domain_report,
@@ -8,13 +11,25 @@ from tracecat.integrations.virustotal import (
 )
 
 
+@pytest.fixture
+def virustotal_secret(create_mock_secret) -> dict[str, str | bytes]:
+    mock_secret = create_mock_secret(
+        "virustotal", {"VIRUSTOTAL_API_KEY": os.environ["VIRUSTOTAL_API_KEY"]}
+    )
+    serialized_secret = mock_secret.model_dump()
+    return serialized_secret
+
+
 @pytest.mark.parametrize(
     "file_hash",
-    # Mirai hash
-    ["10c796b7308ac0b9c38f1caa95c798b2b28c46adaa037a9c3a9ebdd3569824e3"],
+    [
+        "10c796b7308ac0b9c38f1caa95c798b2b28c46adaa037a9c3a9ebdd3569824e3"
+    ],  # Example hash of Mirai malware
 )
-def test_get_file_report(file_hash):
-    """Check expected attributes in File object."""
+def test_get_file_report(virustotal_secret, respx_mock, file_hash):
+    respx_mock.get(f'{os.environ["TRACECAT__API_URL"]}/secrets/virustotal').mock(
+        return_value=Response(status_code=200, json=virustotal_secret)
+    )
     result = get_file_report(file_hash)
     assert result["sha256"] == file_hash
     required_keys = [
@@ -28,9 +43,11 @@ def test_get_file_report(file_hash):
     ), "Some keys are missing in the file report"
 
 
-def test_get_url_report():
-    """Check expected attributes in URL object."""
+def test_get_url_report(virustotal_secret, respx_mock):
     url = "http://example.com"
+    respx_mock.get(f'{os.environ["TRACECAT__API_URL"]}/secrets/virustotal').mock(
+        return_value=Response(status_code=200, json=virustotal_secret)
+    )
     result = get_url_report(url)
     assert result["url"] == url
     required_keys = [
@@ -45,8 +62,11 @@ def test_get_url_report():
     ), "Some keys are missing in the URL report"
 
 
-def test_get_domain_report():
+def test_get_domain_report(virustotal_secret, respx_mock):
     domain = "ycombinator.com"
+    respx_mock.get(f'{os.environ["TRACECAT__API_URL"]}/secrets/virustotal').mock(
+        return_value=Response(status_code=200, json=virustotal_secret)
+    )
     result = get_domain_report(domain)
     assert result["domain"] == domain
     required_keys = [
@@ -60,8 +80,11 @@ def test_get_domain_report():
     ), "Some keys are missing in the domain report"
 
 
-def test_get_ip_address_report():
-    ip = "8.8.8.8"  # Google's DNS
+def test_get_ip_address_report(virustotal_secret, respx_mock):
+    ip = "8.8.8.8"  # Google's IP
+    respx_mock.get(f'{os.environ["TRACECAT__API_URL"]}/secrets/virustotal').mock(
+        return_value=Response(status_code=200, json=virustotal_secret)
+    )
     result = get_ip_address_report(ip)
     assert result["id"] == ip
     required_keys = ["title", "regional_internet_registry", "whois"]
