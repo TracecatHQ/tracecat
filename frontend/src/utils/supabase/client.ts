fd2b10f3753cc56130cd76de55ccc753a8f77f54
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-04-27 18:20:47 +0100

    refactor(ui): Remove Supabase auth [3/N]

--- frontend/src/utils/supabase/client.ts
+++
@@ -1,9 +0,0 @@
-import { createBrowserClient } from "@supabase/ssr"
-
-export const createClient = () =>
-  createBrowserClient(
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:8000" // In local development mode, the client (in browser) resolves localhost as the host machine
-      : process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  )
