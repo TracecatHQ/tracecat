67ceb06588b04f55f59bc56f02bc4449cc9a9099
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-04-27 18:01:03 +0100

    feat(ui): Replace Supabase middleware with Clerk middleware

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,19 +1,56 @@
 import { NextResponse, type NextRequest } from "next/server"
-import { updateSession } from "@/utils/supabase/middleware"
+import {
+  clerkMiddleware,
+  ClerkMiddlewareAuth,
+  createRouteMatcher,
+} from "@clerk/nextjs/server"
 import { get } from "@vercel/edge-config"
 
-export async function middleware(request: NextRequest) {
-  if (
-    process.env.NEXT_PUBLIC_APP_ENV === "production" &&
-    (await get("isUnderMaintenance"))
-  ) {
-    request.nextUrl.pathname = `/status`
-    console.log("Redirecting to status page")
-    return NextResponse.rewrite(request.nextUrl)
-  }
-  return await updateSession(request)
-}
+const isProtectedRoute = createRouteMatcher([
+  "/workflows(.*)",
+  "/settings(.*)",
+  "/library(.*)",
+  "/onboarding(.*)",
+])
+export default clerkMiddleware(
+  async (auth: ClerkMiddlewareAuth, req: NextRequest) => {
+    // ** Site down **
+    if (
+      process.env.NEXT_PUBLIC_APP_ENV === "production" &&
+      (await get("isUnderMaintenance"))
+    ) {
+      req.nextUrl.pathname = "/status"
+      console.log("Redirecting to status page")
+      return NextResponse.rewrite(req.nextUrl)
+    }
 
+    const { userId, sessionClaims, redirectToSignIn } = auth()
+    const isProtected = isProtectedRoute(req)
+
+    // ** Unauthorized **
+    // User isn't signed in and the route is private -- redirect to sign-in
+    if (!userId && isProtected) {
+      // NOTE: Using this API requires NEXT_PUBLIC_CLERK_SIGN_IN_URL
+      // Otherwise, use `return NextResponse.redirect("/sign-in")`
+      return redirectToSignIn({ returnBackUrl: req.url })
+    }
+
+    // ** Authorized **
+    // // For user visiting /onboarding, don't try and redirect
+    // if (userId && req.nextUrl.pathname === "/onboarding") {
+    //   return NextResponse.next()
+    // }
+    // Catch users who doesn't have `onboardingComplete: true` in PublicMetata
+    // Redirect them to the /onboading out to complete onboarding
+    if (userId && !sessionClaims?.metadata?.onboardingComplete) {
+      const onboardingUrl = new URL("/onboarding", req.url)
+      console.log("Redirecting to onboarding", onboardingUrl.toString())
+      return NextResponse.rewrite(onboardingUrl)
+    }
+    // Handle normally
+    if (isProtectedRoute(req)) auth().protect()
+  }
+)
 export const config = {
   matcher: [
     /*
