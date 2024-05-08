c8e1c6aba668cd57e413d206705212d32114fcd2
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-05-07 17:25:16 -0700

    refactor(engine): Simplify loguru logger wrapper

--- tracecat/logging/__init__.py
+++ tracecat/logging/__init__.py
@@ -1,4 +1,4 @@
-from tracecat.logging.default import file_logger, standard_logger
-from tracecat.logging.server import LoggerFactory
+from tracecat.logging._default import file_logger, standard_logger
+from tracecat.logging._logger import Logger, logger
 
-__all__ = ["LoggerFactory", "standard_logger", "file_logger"]
+__all__ = ["Logger", "logger", "standard_logger", "file_logger"]
