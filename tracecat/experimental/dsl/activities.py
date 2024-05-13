dccac3e05556bccedcee74089ace21b061fb0fff
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-05-13 02:29:23 -0700

    feat(engine): Add templating to workflow execution

--- tracecat/experimental/dsl/activities.py
+++ tracecat/experimental/dsl/activities.py
@@ -1,61 +1,80 @@
-import asyncio
+from __future__ import annotations
 
+from typing import Any
+
+from pydantic import BaseModel
 from temporalio import activity
+
+from tracecat.experimental.udf import my_function
+
+
+class ActionInputs(BaseModel):
+    pass
+
+
+class WebhookAction(ActionInputs):
+    inputs: dict[str, Any]
+
+
+class HTTPRequestAction(ActionInputs):
+    url: str
+
+
+class PrintAction(ActionInputs):
+    value: int
+
+
+class UDFAction(ActionInputs):
+    inputs: dict[str, Any]
 
 
 class DSLActivities:
-    def __init__(self):
-        raise RuntimeError("This class should not be instantiated")
+    # We likely want to get these validators dynamically
+    # from the function definition of each activity
+    validators: dict[str, ActionInputs] = {
+        "run_webhook_action": WebhookAction,
+        "run_http_request_action": HTTPRequestAction,
+        "run_udf": UDFAction,
+        "run_print_action": PrintAction,
+    }
 
     def __new__(cls):
         raise RuntimeError("This class should not be instantiated")
 
     @staticmethod
     @activity.defn
-    async def activity1(arg: int) -> int:
-        activity.logger.info(f"Executing activity1 with arg: {arg=}")
-        return arg
+    async def run_webhook_action(args: WebhookAction) -> dict[str, Any]:
+        activity.logger.info("Webhook action")
+        print(args.inputs)
+        return args.inputs
 
     @staticmethod
     @activity.defn
-    async def activity2(arg: int) -> int:
-        activity.logger.info(f"Executing activity2 with arg: {arg=}")
-        return arg * 2
+    async def run_http_request_action(args: HTTPRequestAction) -> str:
+        activity.logger.info("Run HTTP request action")
+        result = f"Result from hitting {args.url}"
+        return result
 
     @staticmethod
     @activity.defn
-    async def activity3(arg: int) -> int:
-        activity.logger.info(f"Executing activity3 with args: {arg=}")
-        activity.logger.info("********** WAITING FOR 3 SECONDS **********")
-        await asyncio.sleep(3)
-        return arg * 2
+    async def run_udf(args: UDFAction) -> Any:
+        activity.logger.info("Run udf")
+        print(args)
+        result = my_function()
+        activity.logger.info(f"Result: {result}")
+        return result
 
     @staticmethod
     @activity.defn
-    async def activity4(arg: int) -> int:
-        activity.logger.info(f"Executing activity4 with arg: {arg=}")
-        return arg * 2
-
-    @staticmethod
-    @activity.defn
-    async def activity5(arg: int) -> int:
-        activity.logger.info(f"Executing activity5 with args: {arg=}")
-        activity.logger.info("********** WAITING FOR 5 SECONDS **********")
-        await asyncio.sleep(5)
-        return arg * 2
-
-    @staticmethod
-    @activity.defn
-    async def activity6(arg1: int, arg2: int) -> int:
-        activity.logger.info(f"Executing activity6 with args: {arg1=}, {arg2=}")
-        return arg1 + arg2
+    async def run_print_action(args: PrintAction) -> str:
+        activity.logger.info("Run print action")
+        print(type(args.value), args.value)
+        return args.value
 
 
+# Dynamically register all static methods as activities
 dsl_activities = [
-    DSLActivities.activity1,
-    DSLActivities.activity2,
-    DSLActivities.activity3,
-    DSLActivities.activity4,
-    DSLActivities.activity5,
-    DSLActivities.activity6,
+    getattr(DSLActivities, method_name)
+    for method_name in dir(DSLActivities)
+    if hasattr(getattr(DSLActivities, method_name), "__temporal_activity_definition")
 ]
--- tracecat/experimental/dsl/workflow.py
+++ tracecat/experimental/dsl/workflow.py
@@ -2,12 +2,18 @@ from __future__ import annotations
 
 import asyncio
 from datetime import timedelta
-from typing import Any
+from typing import Any, TypedDict
 
 from temporalio import workflow
 
+from tracecat.experimental.dsl.activities import DSLActivities
+
 with workflow.unsafe.imports_passed_through():
+    import jsonpath_ng.lexer  # noqa
+    import jsonpath_ng.parser  # noqa
     from pydantic import BaseModel, ConfigDict, Field
+
+    from tracecat.experimental.templates.eval import eval_templated_object
 
 
 class _DSLNode(BaseModel):
@@ -19,13 +25,17 @@ class DSLInput(_DSLNode):
     variables: dict[str, Any] = Field(default_factory=dict)
 
 
+SLUG_PATTERN = r"^[a-z0-9_]+$"
+
+
 class ActivityStatement(_DSLNode):
-    action: str
-    arguments: list[str] = Field(default_factory=list)
-    result: str | None = None
+    ref: str = Field(pattern=SLUG_PATTERN)
+    action: str = Field(pattern=SLUG_PATTERN)
+    # Lower snake case slug is used for the key
+    args: dict[str, Any] = Field(default_factory=dict)
 
 
-class SequenceStatement(_DSLNode):
+class SequentialStatement(_DSLNode):
     sequence: list[Statement]
 
 
@@ -33,34 +43,51 @@ class ParallelStatement(_DSLNode):
     parallel: list[Statement]
 
 
-Statement = ActivityStatement | SequenceStatement | ParallelStatement
+Statement = ActivityStatement | SequentialStatement | ParallelStatement
+
+
+class DSLContext(TypedDict):
+    INPUTS: dict[str, Any]
+    ACTIONS: dict[str, Any]
+
+
+class DSLNodeResult(TypedDict):
+    result: Any
+    result_typename: str
 
 
 @workflow.defn
 class DSLWorkflow:
     @workflow.run
-    async def run(self, input: DSLInput) -> dict[str, Any]:
-        self.variables = dict(input.variables)
-        workflow.logger.info("Running DSL workflow")
+    async def run(self, input: DSLInput) -> DSLContext:
+        # NOTE: This variable is like our action_result_store in the previous world
+        self.context = DSLContext(INPUTS=input.variables, ACTIONS={})
+        wfr_id = workflow.info().run_id
+        workflow.logger.info(f"Running DSL workflow {wfr_id}")
         await self.execute_statement(input.root)
         workflow.logger.info("DSL workflow completed")
-        return self.variables
+        return self.context
 
     async def execute_statement(self, stmt: Statement) -> None:
         match stmt:
-            case ActivityStatement(
-                action=action, arguments=arguments, result=stmt_result
-            ):
-                # Invoke activity loading arguments from variables and optionally
-                # storing result as a variable
+            case ActivityStatement(ref=ref, action=action, args=args):
+                # Resolve all templated arguments
+                processed_args = eval_templated_object(args, operand=self.context)
+
+                # Validate the arguments
+                validator = DSLActivities.validators[action]
+                # We use strict=True to ensure that no type coercion is done
+                validated_args = validator.model_validate(processed_args, strict=True)
                 activity_result = await workflow.execute_activity(
                     action,
-                    args=[self.variables.get(arg, "") for arg in arguments],
+                    arg=validated_args,
                     start_to_close_timeout=timedelta(minutes=1),
                 )
-                if stmt_result:
-                    self.variables[stmt_result] = activity_result
-            case SequenceStatement(sequence=sequence):
+                self.context["ACTIONS"][ref] = DSLNodeResult(
+                    result=activity_result,
+                    result_typename=type(activity_result).__name__,
+                )
+            case SequentialStatement(sequence=sequence):
                 # Execute each statement in order
                 for elem in sequence:
                     await self.execute_statement(elem)
