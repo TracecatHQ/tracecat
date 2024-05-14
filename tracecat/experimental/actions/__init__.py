361b48caabcfd290302108f0c4a411b86d8ac67e
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-05-14 12:27:11 -0700

    feat(engine): Move registry into actions and add core

--- tracecat/experimental/actions/__init__.py
+++ tracecat/experimental/actions/__init__.py
@@ -1,3 +1,10 @@
-from tracecat.experimental.actions import example
+"""Actions module for tracecat.
 
-__all__ = ["example"]
+WARNING!!!
+----------
+Do not add `from __future__ import annotations` to any action module. This will cause class types to be resolved as strings."""
+
+from tracecat.experimental.actions import core, example
+from tracecat.experimental.actions._registry import registry
+
+__all__ = ["registry", "core", "example"]
