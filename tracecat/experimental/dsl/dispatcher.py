6578460dfdf3dc9a25282eafebf884bfc7ff969f
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-05-25 11:50:36 +0100

    feat(engine): Update RFGraph interface

--- tracecat/experimental/dsl/dispatcher.py
+++ tracecat/experimental/dsl/dispatcher.py
@@ -6,6 +6,7 @@ import uuid
 from temporalio.client import Client
 
 from tracecat import config
+from tracecat.contexts import ctx_role
 from tracecat.experimental.dsl._converter import pydantic_data_converter
 from tracecat.experimental.dsl.workflow import DSLInput, DSLWorkflow
 from tracecat.logging import standard_logger
@@ -15,27 +16,24 @@ logger = standard_logger("tracecat.experimental.dsl.dispatcher")
 
 async def dispatch_wofklow(dsl: DSLInput) -> None:
     # Connect client
+    role = ctx_role.get()
     client = await Client.connect(
         config.TEMPORAL__CLUSTER_URL, data_converter=pydantic_data_converter
     )
 
     # Run workflow
+    logger.info(f"Executing DSL workflow: {dsl.title!r} {role=}")
     result = await client.execute_workflow(
         DSLWorkflow.run,
         dsl,
-        id=f"dsl-workflow-id-{uuid.uuid4()}",
+        id=f"dsl-workflow-{uuid.uuid4()}",
         task_queue="dsl-task-queue",
     )
     logger.info(f"Workflow result:\n{json.dumps(result, indent=2)}")
 
 
 if __name__ == "__main__":
-    # Require the YAML file as an argument. We read this _outside_ of the async
-    # def function because thread-blocking IO should never happen in async def
-    # functions.
     if len(sys.argv) != 2:
         raise RuntimeError("Expected single argument for YAML file")
     path = sys.argv[1]
-
-    # Run
     asyncio.run(dispatch_wofklow(DSLInput.from_yaml(path)))
