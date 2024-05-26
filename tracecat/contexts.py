c0415fa32045252ec448ad93e2c94b443c9e9061
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-05-26 02:26:57 +0100

    refactor: Remove old runner + messaging + some types + some contexts

--- tracecat/contexts.py
+++ tracecat/contexts.py
@@ -5,23 +5,8 @@ from contextvars import ContextVar
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from aio_pika import Channel
-    from aio_pika.pool import Pool
-
     from tracecat.auth import Role
-    from tracecat.runner.actions import ActionRun
-    from tracecat.runner.workflows import Workflow
-    from tracecat.types.workflow import WorkflowRunContext
 
 
 ctx_role: ContextVar[Role] = ContextVar("role", default=None)
-# TODO: Deprecate this contextvar
-ctx_workflow: ContextVar[Workflow] = ContextVar("workflow", default=None)
-ctx_workflow_run: ContextVar[WorkflowRunContext] = ContextVar(
-    "workflow_run", default=None
-)
-ctx_action_run: ContextVar[ActionRun] = ContextVar("action_run", default=None)
-ctx_mq_channel_pool: ContextVar[Pool[Channel]] = ContextVar(
-    "mq_channel_pool", default=None
-)
 ctx_logger: ContextVar[logging.Logger] = ContextVar("logger", default=None)
