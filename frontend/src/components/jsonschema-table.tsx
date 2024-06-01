ddd6c150d0b3ef04eddd5f0de04c0bbc3647af93
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-06-02 00:53:49 +0100

    refactor: Hide constraints and description

--- frontend/src/components/jsonschema-table.tsx
+++ frontend/src/components/jsonschema-table.tsx
@@ -15,25 +15,21 @@ export function JSONSchemaTable({ schema }: { schema: JSONSchema7 }) {
   return (
     <Table>
       <TableHeader>
-        <TableRow className="grid h-6 grid-cols-5 text-xs capitalize ">
-          <TableHead className="col-span-1 font-bold">Parmaeter</TableHead>
+        <TableRow className="grid h-6 grid-cols-3 text-xs capitalize ">
+          <TableHead className="col-span-1 font-bold">Parameter</TableHead>
           <TableHead className="col-span-1 font-bold">Type</TableHead>
           <TableHead className="col-span-1 font-bold">Default</TableHead>
-          <TableHead className="col-span-1 font-bold">Description</TableHead>
-          <TableHead className="col-span-1 font-bold">Constraints</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
         {rows.map((row, idx) => (
           <TableRow
             key={idx}
-            className="grid grid-cols-5 text-xs text-muted-foreground"
+            className="grid grid-cols-3 text-xs text-muted-foreground"
           >
             <TableCell className="col-span-1">{row.parameter}</TableCell>
             <TableCell className="col-span-1">{row.type}</TableCell>
             <TableCell className="col-span-1">{row.default}</TableCell>
-            <TableCell className="col-span-1">{row.description}</TableCell>
-            <TableCell className="col-span-1">{row.constraints}</TableCell>
           </TableRow>
         ))}
       </TableBody>
