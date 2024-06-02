5b2d86d1ea1e275ac7ee169556e4d22f8648664f
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-06-02 16:09:37 +0100

    feat(engine): Add env var to control integrations import

--- tracecat/actions/__init__.py
+++ tracecat/actions/__init__.py
@@ -5,6 +5,8 @@ WARNING!!!
 Do not add `from __future__ import annotations` to any action module. This will cause class types to be resolved as strings."""
 
 # Bring all actions into the namespace to be registered
+import os
+
 from tracecat.actions.core import (
     cases,
     condition,
@@ -12,17 +14,6 @@ from tracecat.actions.core import (
     example,
     http,
     llm,
-)
-from tracecat.actions.integrations import (
-    aws_cloudtrail,
-    aws_guardduty,
-    datadog,
-    emailrep,
-    example_integration,
-    project_discovery,
-    sublime,
-    urlscan,
-    virustotal,
 )
 
 __all__ = [
@@ -33,14 +24,32 @@ __all__ = [
     "llm",
     "condition",
     "cases",
-    # Integrations
-    "example_integration",
-    "datadog",
-    "aws_cloudtrail",
-    "aws_guardduty",
-    "sublime",
-    "urlscan",
-    "project_discovery",
-    "emailrep",
-    "virustotal",
 ]
+
+
+if str(os.environ.get("TRACECAT__ENABLE_INTEGRATIONS")).lower() in ("true", "1"):
+    from tracecat.actions.integrations import (
+        aws_cloudtrail,  # noqa: F401
+        aws_guardduty,  # noqa: F401
+        datadog,  # noqa: F401
+        emailrep,  # noqa: F401
+        example_integration,  # noqa: F401
+        project_discovery,  # noqa: F401
+        sublime,  # noqa: F401
+        urlscan,  # noqa: F401
+        virustotal,  # noqa: F401
+    )
+
+    __all__.extend(
+        [
+            "example_integration",
+            "datadog",
+            "aws_cloudtrail",
+            "aws_guardduty",
+            "sublime",
+            "urlscan",
+            "project_discovery",
+            "emailrep",
+            "virustotal",
+        ]
+    )
