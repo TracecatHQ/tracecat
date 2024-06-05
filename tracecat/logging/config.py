fd514feddfaae517461cb979bb3496b315014f9c
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-06-05 03:02:39 +0100

    feat(engine): Add simplified logging without queue

--- tracecat/logging/config.py
+++ tracecat/logging/config.py
@@ -34,7 +34,7 @@ LOG_CONFIG = {
         "level": "INFO",
         "colorize": True,
         "backtrace": True,
-        "enqueue": True,
+        # "enqueue": True,  # Causes pickling errors if enabled
         "format": "{time} | <level>{level: <8}</level> <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level> | {extra}",
     },
     "file:log": {
