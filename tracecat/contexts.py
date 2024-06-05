68eb777926e81dc7d71a168b6928b2437f19d4c1
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-06-05 21:15:31 +0100

    feat(engine): Add run context

--- tracecat/contexts.py
+++ tracecat/contexts.py
@@ -4,9 +4,17 @@ import logging
 from contextvars import ContextVar
 from typing import TYPE_CHECKING
 
+from pydantic import BaseModel
+
 if TYPE_CHECKING:
     from tracecat.auth import Role
 
 
+class RunContext(BaseModel):
+    wf_id: str
+    wf_run_id: str
+
+
+ctx_run: ContextVar[RunContext] = ContextVar("run", default=None)
 ctx_role: ContextVar[Role] = ContextVar("role", default=None)
 ctx_logger: ContextVar[logging.Logger] = ContextVar("logger", default=None)
