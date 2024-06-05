2744f57cc40c91934522497c81ecfdc160a2dd6c
Chris Lo <46541035+topher-lo@users.noreply.github.com>
2024-06-05 12:56:13 -0700

    feat: Add more informative error message newUserFlow

--- frontend/src/lib/onboarding.ts
+++ frontend/src/lib/onboarding.ts
@@ -9,21 +9,28 @@ import { client } from "@/lib/api"
  * Initialize user settings in the database
  * @returns void
  */
+
 export async function newUserFlow(): Promise<void> {
-  console.log("Start new user flow")
+  console.log("Start new user flow");
 
   try {
-    const response = await client.put("/users")
+    const response = await client.put("/users");
     if (response.status !== 201) {
-      throw new Error("Unexpected response status")
+      throw new Error("Unexpected response status");
     }
-    console.log("New user created")
+    console.log("New user created");
   } catch (e) {
     if (e instanceof AxiosError) {
       if (e.response?.status !== 409) {
-        throw new Error("Error creating new user")
+        if (process.env.TRACECAT__APP_ENV !== "production") {
+          throw new Error("Internal Server Error. Please check API service logs to debug.");
+        } else {
+          throw new Error("Unexpected error creating new user in production. Try running in development mode for more detailed error logs.");
+        }
       }
-      console.log("User already exists")
+      console.log("User already exists");
+    } else {
+      throw e; // Re-throw non-Axios errors
     }
   }
 }
