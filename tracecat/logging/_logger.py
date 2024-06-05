3dc2cd1515e23ae033fa9266555c956b6714e8b4
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-06-05 21:18:43 +0100

    feat(engine): Use tracecat.logging in temporal workflow/workers

--- tracecat/logging/_logger.py
+++ tracecat/logging/_logger.py
@@ -1,10 +1,19 @@
 """Loggers to override default FastAPI uvicorn logger behavior."""
 
+import sys
+
 from loguru import logger as base_logger
 
-from .config import LOG_CONFIG
+try:
+    base_logger.remove(0)
+except ValueError:
+    pass
 
-base_logger.remove(0)
-base_logger.add(**LOG_CONFIG["stderr:log"])
+base_logger.add(
+    sink=sys.stderr,
+    colorize=True,
+    level="INFO",
+    format="{time} | <level>{level: <8}</level> <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level> | {extra}",
+)
 
 logger = base_logger
