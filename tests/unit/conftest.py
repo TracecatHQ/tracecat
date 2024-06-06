b49f043fcdb930a77977cbcd39346a3b0da5e734
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-06-06 04:00:20 +0100

    refactor: Remove logger from unit conftest

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,11 +1,1 @@
 """Set up shared environment variables and S3 proxy server (MinIO) for integration tests."""
-
-import sys
-
-from loguru import logger
-
-logger.add(
-    sink=sys.stderr,
-    level="INFO",
-    format="{time} | <level>{level: <8}</level> <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level> | {extra}",
-)
