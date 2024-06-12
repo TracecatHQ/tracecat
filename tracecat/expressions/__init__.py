5cf82f635ae3b4e11a12d2703ca188043db54165
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-06-13 00:52:52 +0100

    refactor(engine): Reexpport ExprContext in expressions root

--- tracecat/expressions/__init__.py
+++ tracecat/expressions/__init__.py
@@ -1,10 +1,11 @@
-from .engine import TemplateExpression
+from .engine import ExprContext, TemplateExpression
 from .eval import eval_templated_object, extract_templated_secrets
 from .validators import TemplateValidator
 
 __all__ = [
     "TemplateValidator",
     "TemplateExpression",
+    "ExprContext",
     "eval_templated_object",
     "extract_templated_secrets",
 ]
