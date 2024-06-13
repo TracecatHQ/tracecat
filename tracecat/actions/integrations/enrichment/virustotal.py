dc62ae35a973d285f9f53e9756f12b1f0967f0f7
Chris Lo <46541035+topher-lo@users.noreply.github.com>
2024-06-12 18:34:17 -0700

    feat: Register virustotal

--- tracecat/actions/integrations/enrichment/virustotal.py
+++ tracecat/actions/integrations/enrichment/virustotal.py
@@ -32,9 +32,11 @@ analyze_malware_sample = {
 
 import base64
 import os
-from typing import Any
+from typing import Annotated, Any
 
 import httpx
+
+from tracecat.registry import Field, registry
 
 VT_BASE_URL = "https://www.virustotal.com/api/"
 
@@ -50,8 +52,13 @@ def create_virustotal_client() -> httpx.AsyncClient:
     return client
 
 
-async def analyze_url(url: str) -> dict[str, Any]:
-    # Recipe from https://docs.virustotal.com/reference/url#url-identifiers
+@registry.register(
+    description="Analyze a URL using VirusTotal.",
+    namespace="virustotal",
+)
+async def analyze_url(
+    url: Annotated[str, Field(..., description="The URL to analyze")],
+) -> dict[str, Any]:
     url_id = base64.urlsafe_b64encode(url.encode()).decode().strip("=")
     async with create_virustotal_client() as client:
         response = await client.get(f"/v3/urls/{url_id}")
@@ -59,14 +66,28 @@ async def analyze_url(url: str) -> dict[str, Any]:
         return response.json()
 
 
-async def analyze_ip_address(ip_address: str) -> dict[str, Any]:
+@registry.register(
+    description="Analyze an IP address using VirusTotal.",
+    namespace="virustotal",
+)
+async def analyze_ip_address(
+    ip_address: Annotated[str, Field(..., description="The IP address to analyze")],
+) -> dict[str, Any]:
     async with create_virustotal_client() as client:
         response = await client.get(f"/v3/ip_addresses/{ip_address}")
         response.raise_for_status()
         return response.json()
 
 
-async def analyze_malware_sample(file_hash: str) -> dict[str, Any]:
+@registry.register(
+    description="Analyze a malware sample using VirusTotal.",
+    namespace="virustotal",
+)
+async def analyze_malware_sample(
+    file_hash: Annotated[
+        str, Field(..., description="The hash of the malware sample to analyze")
+    ],
+) -> dict[str, Any]:
     async with create_virustotal_client() as client:
         response = await client.get(f"/v3/files/{file_hash}")
         response.raise_for_status()
