34e11a1048d8c64a71ee9b0df82768a4eb98429c
Chris Lo <46541035+topher-lo@users.noreply.github.com>
2024-06-13 21:29:20 -0700

    feat(integrations): Include sub and main address in email extraction

--- tracecat/actions/integrations/extraction/email.py
+++ tracecat/actions/integrations/extraction/email.py
@@ -16,9 +16,16 @@ def pl_extract_emails(texts: pl.Expr) -> pl.Expr:
     return texts.str.extract_all(EMAIL_REGEX)
 
 
+def normalize_email_address(email: str) -> str:
+    """Convert sub-addressed email to a normalized email address."""
+    local_part, domain = email.split("@")
+    local_part = local_part.split("+")[0]
+    return f"{local_part}@{domain}"
+
+
 @registry.register(
     description="Extract unique emails from a list of strings.",
-    namespace="core.extraction",
+    namespace="integrations.extraction",
     default_title="Email Extractor",
     display_group="Data Extraction",
 )
@@ -26,9 +33,21 @@ def extract_emails(
     texts: Annotated[
         list[str], Field(..., description="The list of strings to extract emails from")
     ],
+    normalize: Annotated[
+        bool,
+        Field(
+            default=False,
+            description="Whether to normalize emails by removing sub-addresses",
+        ),
+    ] = False,
 ) -> list[str]:
     """Extract unique emails from a list of strings."""
     emails = itertools.chain.from_iterable(
         re.findall(EMAIL_REGEX, text) for text in texts
     )
+    if normalize:
+        normalized_emails = [
+            normalize_email_address(email) for email in extract_emails(texts)
+        ]
+        emails += normalized_emails
     return list(set(emails))
