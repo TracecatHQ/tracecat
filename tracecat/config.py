a049d410c42e5f5d772511c1996fca9fcffdfc2f
Chris Lo <46541035+topher-lo@users.noreply.github.com>
2024-06-17 15:39:49 -0600

    chore: Change TRACECAT__APP_ENV from dev to development

--- tracecat/config.py
+++ tracecat/config.py
@@ -11,7 +11,7 @@ TRACECAT__SCHEDULE_INTERVAL_SECONDS = os.environ.get(
     "TRACECAT__SCHEDULE_INTERVAL_SECONDS", 60
 )
 TRACECAT__SCHEDULE_MAX_CONNECTIONS = 6
-TRACECAT__APP_ENV = os.environ.get("TRACECAT__APP_ENV", "dev")
+TRACECAT__APP_ENV = os.environ.get("TRACECAT__APP_ENV", "development")
 TRACECAT__API_URL = os.environ.get("TRACECAT__API_URL", "http://localhost:8000")
 TRACECAT__PUBLIC_RUNNER_URL = os.environ.get(
     "TRACECAT__PUBLIC_RUNNER_URL", "http://localhost:8001"
