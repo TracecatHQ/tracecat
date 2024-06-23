eef7c7069dadaec69f1ddcf0567bd8320aee505b
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-06-23 12:04:42 -0700

    ci: Configure logger verbosity (#200)

--- tracecat/logging/_logger.py
+++ tracecat/logging/_logger.py
@@ -1,5 +1,6 @@
 """Loggers to override default FastAPI uvicorn logger behavior."""
 
+import os
 import sys
 
 from loguru import logger as base_logger
@@ -12,7 +13,7 @@ except ValueError:
 base_logger.add(
     sink=sys.stderr,
     colorize=True,
-    level="INFO",
+    level=os.environ.get("LOG_LEVEL", "INFO"),
     format="{time} | <level>{level: <8}</level> <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level> | {extra}",
 )
 
