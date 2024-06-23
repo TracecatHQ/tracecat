ad9173e12cd3a53e08ad55479ffb0d5f54a96fa2
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-06-23 18:56:09 +0100

    fix(udf): Reexport crowdstrike udfs

--- tracecat/actions/integrations/edr/__init__.py
+++ tracecat/actions/integrations/edr/__init__.py
@@ -1,4 +1,9 @@
-from .crowdstrike import list_crowdstrike_alerts, list_crowdstrike_detects
+from .crowdstrike import (
+    list_crowdstrike_alerts,
+    list_crowdstrike_detects,
+    update_crowdstrike_alert_status,
+    update_crowdstrike_detect_status,
+)
 from .microsoft_defender import list_defender_endpoint_alerts
 from .sentinel_one import list_sentinelone_alerts
 
@@ -7,4 +12,6 @@ __all__ = [
     "list_crowdstrike_detects",
     "list_sentinelone_alerts",
     "list_defender_endpoint_alerts",
+    "update_crowdstrike_alert_status",
+    "update_crowdstrike_detect_status",
 ]
