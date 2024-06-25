26ac68b90d27b379c310abe5e57240dbe658bd09
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-06-25 15:55:46 +0100

    fix!: Remove sensitive print

--- tracecat/dsl/workflow.py
+++ tracecat/dsl/workflow.py
@@ -346,8 +346,6 @@ class DSLActivities:
         # NOTE: Regardless of loop iteration, we should only make this call/substitution once!!
         secret_refs = extract_templated_secrets(task.args)
         async with AuthSandbox(secrets=secret_refs, target="context") as sandbox:
-            logger.info("Evaluating task arguments", secrets=sandbox.secrets)
-
             # Skip evaluation of action-local expressions
             args = eval_templated_object(
                 task.args,
