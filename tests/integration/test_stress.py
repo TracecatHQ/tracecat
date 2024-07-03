101a00790659e87c374d0feaca7ac45beb80763f
Chris Lo <46541035+topher-lo@users.noreply.github.com>
2024-07-03 15:29:47 -0400

    fix: dsl

--- tests/integration/test_stress.py
+++ tests/integration/test_stress.py
@@ -4,7 +4,6 @@ from pathlib import Path
 
 import pytest
 
-from tracecat.dsl.common import DSLInput
 from tracecat.dsl.dispatcher import dispatch_workflow
 
 DATA_PATH = Path(__file__).parent.parent.joinpath("data/workflows")
@@ -12,7 +11,7 @@ TEST_WF_ID = "wf-00000000000000000000000000000000"
 
 
 @pytest.mark.parametrize(
-    "path_to_dsl",
+    "dsl",
     [DATA_PATH / "stress_adder_tree.yml"],
     ids=lambda path: path.stem,
     indirect=True,
@@ -22,7 +21,7 @@ TEST_WF_ID = "wf-00000000000000000000000000000000"
 )
 @pytest.mark.slow
 def test_concurrent_workflows(
-    path_to_dsl, num_workflows, temporal_cluster, mock_registry, auth_sandbox, benchmark
+    dsl, num_workflows, temporal_cluster, mock_registry, auth_sandbox, benchmark
 ):
     """Multiple executions of the same workflow run at the same time."""
 
@@ -30,11 +29,7 @@ def test_concurrent_workflows(
         return f"wf-{uuid.uuid4()}"
 
     tasks = [
-        dispatch_workflow(
-            dsl=DSLInput.from_yaml(path_to_dsl),
-            wf_id=generate_wf_id(),
-        )
-        for _ in range(num_workflows)
+        dispatch_workflow(dsl=dsl, wf_id=generate_wf_id()) for _ in range(num_workflows)
     ]
     workflow_runs = benchmark.pedantic(
         lambda: asyncio.gather(tasks()), iterations=3, rounds=1
