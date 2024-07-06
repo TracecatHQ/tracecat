5cbc8d2d7a76f3c9011194c60885dcb0d8e76f79
Chris Lo <46541035+topher-lo@users.noreply.github.com>
2024-07-05 20:32:13 -0400

    Update configs with temporal images

--- aws/stack/config.py
+++ aws/stack/config.py
@@ -5,6 +5,12 @@ import os
 # single security group with no external internet access
 
 # Used to pull Tracecat image from ghcr via the version tag
-TRACECAT_VERSION = os.getenv("TRACECAT_VERSION", "latest")
+TRACECAT_VERSION = os.getenv("TRACECAT__VERSION", "latest")
 TRACECAT_IMAGE = f"ghcr.io/tracecathq/tracecat:{TRACECAT_VERSION}"
 TRACECAT_UI_IMAGE = f"ghcr.io/tracecathq/tracecat-ui:{TRACECAT_VERSION}"
+
+# Temporal images
+TEMPORAL_VERSION = os.getenv("TEMPORAL__VERSION", "latest")
+TEMPORAL_UI_VERSION = os.getenv("TEMPORAL__UI_VERSION", "latest")
+TEMPORAL_SERVER_IMAGE = f"temporalio/auto-setup:{TEMPORAL_VERSION}"
+TEMPORAL_UI_IMAGE = f"temporalio/ui:{TEMPORAL_UI_VERSION}"
