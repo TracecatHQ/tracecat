6d559ea5125f62460cbe060ff180640420d638cf
Chris Lo <46541035+topher-lo@users.noreply.github.com>
2024-07-08 14:25:39 -0400

    Update rds settings

--- aws/stack/rds.py
+++ aws/stack/rds.py
@@ -3,27 +3,16 @@ from aws_cdk import aws_rds as rds
 from aws_cdk import core
 from constructs import Construct
 
-# EC2 instance type for databases
+# RDS settings
 INSTANCE_TYPE = ec2.InstanceType.of(
     ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MEDIUM
 )
-# 100 GB storage
-ALLOCATED_STORAGE = 100
-# Use General Purpose SSD (GP2) for balanced performance
 STORAGE_TYPE = rds.StorageType.GP2
-# Adjusted IOPS based on GP2
-IOPS = 1000
-# Retain backups for 7 days
-BACKUP_RETENTION = core.Duration.days(7)
-# Monitoring interval of 60 seconds
+BACKUP_RETENTION = core.Duration.days(14)
 MONITORING_INTERVAL = core.Duration.seconds(60)
-# Preferred backup window
 PREFERRED_BACKUP_WINDOW = "03:00-04:00"
-# Preferred maintenance window
 PREFERRED_MAINTENANCE_WINDOW = "Sun:04:00-Sun:05:00"
-# Enable performance insights
-PERFORMANCE_INSIGHTS = True
-PERFORMANCE_INSIGHT_RETENTION = rds.PerformanceInsightRetention.LONG_TERM
+STORAGE_TYPE = rds.StorageType.GP2
 
 
 class RdsStack(core.Stack):
@@ -41,6 +30,7 @@ class RdsStack(core.Stack):
         def create_rds_instance(
             instance_name: str,
             db_name: str,
+            allocated_storage: int,
             engine_version: rds.IInstanceEngine,
             security_group: ec2.SecurityGroup,
         ) -> None:
@@ -51,9 +41,8 @@ class RdsStack(core.Stack):
                 instance_type=INSTANCE_TYPE,
                 vpc=vpc,
                 multi_az=True,
-                allocated_storage=ALLOCATED_STORAGE,
+                allocated_storage=allocated_storage,
                 storage_type=STORAGE_TYPE,
-                iops=IOPS,
                 credentials=rds.Credentials.from_generated_secret(db_name),
                 deletion_protection=True,
                 database_name=db_name,
@@ -64,8 +53,6 @@ class RdsStack(core.Stack):
                 storage_encrypted=True,
                 preferred_backup_window=PREFERRED_BACKUP_WINDOW,
                 preferred_maintenance_window=PREFERRED_MAINTENANCE_WINDOW,
-                enable_performance_insights=PERFORMANCE_INSIGHTS,
-                performance_insight_retention=PERFORMANCE_INSIGHT_RETENTION,
             )
             return db
 
@@ -73,19 +60,21 @@ class RdsStack(core.Stack):
         tracecat_database = create_rds_instance(
             "TracecatRDSInstance",
             "tracecat-postgres",
-            rds.DatabaseInstanceEngine.postgres(
+            allocated_storage=10,
+            engine_version=rds.DatabaseInstanceEngine.postgres(
                 version=rds.PostgresEngineVersion.VER_16
             ),
-            core_security_group,
+            security_group=core_security_group,
         )
         self.tracecat_database = tracecat_database
 
         temporal_database = create_rds_instance(
             "TemporalRDSInstance",
             "temporal-postgres",
-            rds.DatabaseInstanceEngine.postgres(
+            allocated_storage=5,
+            engine_version=rds.DatabaseInstanceEngine.postgres(
                 version=rds.PostgresEngineVersion.VER_13
             ),
-            temporal_security_group,
+            security_group=temporal_security_group,
         )
         self.temporal_database = temporal_database
