f6a2b71e76100b7072d6ab619456d4d3522a0745
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-07-09 03:13:51 +0100

    fix: Allow jsonpath array syntax for actions validation

--- tracecat/expressions/parser/validator.py
+++ tracecat/expressions/parser/validator.py
@@ -101,6 +101,7 @@ class ExprValidator(Visitor):
     def actions(self, node: Tree):
         self.logger.trace("Visit action expression", expr=node)
         jsonpath = node.children[0].lstrip(".")
+        # ACTIONS.<ref>.<prop> [INDEX] [ATTRIBUTE ACCESS]
         ref, prop, *_ = jsonpath.split(".")
         if ref not in self._context.action_refs:
             self.add(
@@ -108,12 +109,14 @@ class ExprValidator(Visitor):
                 msg=f"Invalid action reference {ref!r} in ACTION expression {jsonpath!r}",
                 type=ExprType.ACTION,
             )
-        elif prop not in DSLNodeResult.__annotations__:
-            valid = list(DSLNodeResult.__annotations__.keys())
+        # Check prop
+        valid_properties = "|".join(DSLNodeResult.__annotations__.keys())
+        pattern = rf"({valid_properties})(\[(\d+|\*)\])?"  # e.g. "result[0], result[*], result"
+        if not re.match(pattern, prop):
             self.add(
                 status="error",
                 msg=f"Invalid property {prop!r} for action reference {ref!r} in ACTION expression {jsonpath!r}."
-                f" Use one of {valid}, e.g. `{ref}.{valid[0]}`",
+                f" Use one of {valid_properties}, e.g. `{ref}.{valid_properties[0]}`",
                 type=ExprType.ACTION,
             )
         else:
