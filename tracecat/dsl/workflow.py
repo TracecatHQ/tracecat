6119e356f80a348ea211a9b5cec1bccca5fdd8dc
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-07-09 15:16:28 +0100

    feat(engine): Add workflow start time to ENV context when workflow starts

--- tracecat/dsl/workflow.py
+++ tracecat/dsl/workflow.py
@@ -53,6 +53,9 @@ class DSLContext(TypedDict):
     TRIGGER: dict[str, Any]
     """DSL Trigger dynamic inputs context"""
 
+    ENV: dict[str, Any]
+    """DSL Environment context. Has metadata about the workflow."""
+
 
 class TaskMarker(StrEnum):
     SKIP = auto()
@@ -187,12 +190,13 @@ class DSLWorkflow:
         self.role = args.role
         self.tracecat_wf_id = args.wf_id
         # Temporal workflow execution ID == Tracecat workflow run ID
-        self.tracecat_wf_run_id = workflow.info().workflow_id
+        wf_info = workflow.info()
+        self.tracecat_wf_run_id = wf_info.workflow_id
 
         self.run_ctx = RunContext(
             wf_id=args.wf_id,
-            wf_exec_id=workflow.info().workflow_id,
-            wf_run_id=workflow.info().run_id,
+            wf_exec_id=wf_info.workflow_id,
+            wf_run_id=wf_info.run_id,
         )
         self.logger = logger.bind(run_ctx=self.run_ctx, role=self.role)
         ctx_logger.set(self.logger)
@@ -202,6 +206,7 @@ class DSLWorkflow:
             ACTIONS={},
             INPUTS=self.dsl.inputs,
             TRIGGER=self.dsl.trigger_inputs,
+            ENV={"workflow": {"start_time": wf_info.start_time}},
         )
         self.dep_list = {task.ref: task.depends_on for task in self.dsl.actions}
         self.action_test_map = {test.ref: test for test in self.dsl.tests}
