3dbf39e3cfbf1d39defeda2b6e9306540b28149e
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-07-09 16:35:52 +0100

    refactor(ui): Remove preview banner from case management in prod

--- frontend/src/app/workflows/[workflowId]/cases/page.tsx
+++ frontend/src/app/workflows/[workflowId]/cases/page.tsx
@@ -1,19 +1,12 @@
 import CasesProvider from "@/providers/cases"
 
 import CaseTable from "@/components/cases/table"
-import { AlertNotification } from "@/components/notifications"
 
 export default function CasesPage() {
   return (
     <CasesProvider>
       <div className="flex h-screen flex-col overflow-auto">
         <div className="flex-1 space-y-8 p-16">
-          {process.env.NEXT_PUBLIC_APP_ENV === "production" && (
-            <AlertNotification
-              message="Cases is in preview mode, and may not work as expected"
-              className="max-w-[600px]"
-            />
-          )}
           <CaseTable />
         </div>
       </div>
