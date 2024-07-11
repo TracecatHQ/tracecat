849a6ab6c9e16403f3d571ff7e1daf0c5e5cbc1f
Chris Lo <46541035+topher-lo@users.noreply.github.com>
2024-07-10 20:08:15 -0700

    Add api hosted zone and cert

--- aws/stack/route53.py
+++ aws/stack/route53.py
@@ -3,7 +3,12 @@ from aws_cdk import aws_certificatemanager as acm
 from aws_cdk import aws_route53 as route53
 from constructs import Construct
 
-from .config import APP_DOMAIN_NAME, CERTIFICATE_ARN
+from .config import (
+    API_CERTIFICATE_ARN,
+    API_DOMAIN_NAME,
+    APP_DOMAIN_NAME,
+    CERTIFICATE_ARN,
+)
 
 
 class Route53Stack(Stack):
@@ -15,8 +20,18 @@ class Route53Stack(Stack):
             "HostedZone",
             domain_name=APP_DOMAIN_NAME,
         )
+        self.api_hosted_zone = route53.HostedZone.from_lookup(
+            self,
+            "ApiHostedZone",
+            domain_name=API_DOMAIN_NAME,
+        )
         self.certificate = acm.Certificate.from_certificate_arn(
             self,
             "Certificate",
             certificate_arn=CERTIFICATE_ARN,
+        )
+        self.api_certificate = acm.Certificate.from_certificate_arn(
+            self,
+            "ApiCertificate",
+            certificate_arn=API_CERTIFICATE_ARN,
         )
