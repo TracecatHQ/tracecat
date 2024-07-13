ca84a958466e4d5e9ca051d10af235f9af861c07
Chris Lo <46541035+topher-lo@users.noreply.github.com>
2024-07-13 10:30:57 -0700

    fix(ui): next.js fail to download google fonts
    See: https://stackoverflow.com/questions/76478043/next-js-always-fail-at-downloading-fonts-from-google-fonts/76484168#76484168

--- frontend/src/lib/fonts.ts
+++ frontend/src/lib/fonts.ts
@@ -3,9 +3,13 @@ import { JetBrains_Mono as FontMono, Inter as FontSans } from "next/font/google"
 export const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
+  display: "swap",
+  adjustFontFallback: false,
 })
 
 export const fontMono = FontMono({
   subsets: ["latin"],
   variable: "--font-mono",
+  display: "swap",
+  adjustFontFallback: false,
 })
