f81468ccfa766385bc62bb4ab0b64afdc54f9f4f
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-07-13 23:39:43 +0100

    refactor(engine): Move DSL dispatcher into WorkflowExecutionsService + change workflow run trigger to create workflow execution

--- tracecat/workflow/service.py
+++ tracecat/workflow/service.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
 
+import asyncio
 import datetime
+import json
+import os
 from collections.abc import AsyncGenerator
+from typing import Any
 
 import orjson
 from temporalio.api.enums.v1 import EventType
@@ -10,17 +14,24 @@ from temporalio.client import (
     WorkflowExecution,
     WorkflowExecutionDescription,
     WorkflowExecutionStatus,
+    WorkflowFailureError,
     WorkflowHandle,
     WorkflowHistory,
     WorkflowHistoryEventFilterType,
 )
 
-from tracecat import identifiers
+from tracecat import config, identifiers
 from tracecat.contexts import ctx_role
 from tracecat.dsl.client import get_temporal_client
+from tracecat.dsl.common import DSLInput
+from tracecat.dsl.validation import validate_trigger_inputs
+from tracecat.dsl.workflow import DSLRunArgs, DSLWorkflow, retry_policies
 from tracecat.logging import logger
 from tracecat.types.auth import Role
+from tracecat.types.exceptions import TracecatValidationError
 from tracecat.workflow.models import (
+    CreateWorkflowExecutionResponse,
+    DispatchWorkflowResult,
     EventFailure,
     EventGroup,
     EventHistoryResponse,
@@ -212,3 +223,90 @@ class WorkflowExecutionsService:
             page_size=10, event_filter_type=event_filter_type, **kwargs
         ):
             yield event
+
+    def create_workflow_execution(
+        self,
+        dsl: DSLInput,
+        *,
+        wf_id: identifiers.WorkflowID,
+        payload: dict[str, Any] | None = None,
+        enable_runtime_tests: bool = False,
+    ) -> CreateWorkflowExecutionResponse:
+        """Create a new workflow execution.
+
+        Note: This method schedules the workflow execution and returns immediately.
+        """
+        configured_dsl = self._configure_dsl_inputs(
+            dsl=dsl, payload=payload, enable_runtime_tests=enable_runtime_tests
+        )
+        wf_exec_id = identifiers.workflow.exec_id(wf_id)
+        _ = asyncio.create_task(
+            self._dispatch_workflow(
+                dsl=configured_dsl, wf_id=wf_id, wf_exec_id=wf_exec_id
+            )
+        )
+        return CreateWorkflowExecutionResponse(
+            message="Workflow execution started",
+            wf_id=wf_id,
+            wf_exec_id=identifiers.workflow.exec_id(wf_id),
+        )
+
+    def _configure_dsl_inputs(
+        self,
+        dsl: DSLInput,
+        *,
+        payload: dict[str, Any] | None = None,
+        enable_runtime_tests: bool = False,
+    ) -> DSLInput:
+        # Set runtime configuration
+        validation_result = validate_trigger_inputs(dsl=dsl, payload=payload)
+        if validation_result.status == "error":
+            logger.error(validation_result.msg, detail=validation_result.detail)
+            raise TracecatValidationError(
+                validation_result.msg, detail=validation_result.detail
+            )
+
+        if payload:
+            dsl.trigger_inputs = payload
+
+        dsl.config.enable_runtime_tests = enable_runtime_tests
+        return dsl
+
+    async def _dispatch_workflow(
+        self,
+        dsl: DSLInput,
+        wf_id: identifiers.WorkflowID,
+        wf_exec_id: identifiers.WorkflowExecutionID,
+        **kwargs: Any,
+    ) -> DispatchWorkflowResult:
+        logger.info(
+            f"Executing DSL workflow: {dsl.title}",
+            role=self.role,
+            wf_exec_id=wf_exec_id,
+        )
+        try:
+            result = await self._client.execute_workflow(
+                DSLWorkflow.run,
+                DSLRunArgs(dsl=dsl, role=self.role, wf_id=wf_id),
+                id=wf_exec_id,
+                task_queue=config.TEMPORAL__CLUSTER_QUEUE,
+                retry_policy=retry_policies["workflow:fail_fast"],
+                **kwargs,
+            )
+        except WorkflowFailureError as e:
+            logger.exception(str(e), role=self.role, wf_exec_id=wf_exec_id, e=e)
+            raise e
+        except Exception as e:
+            logger.exception(
+                "Workflow exception", role=self.role, wf_exec_id=wf_exec_id, e=e
+            )
+            raise e
+        # Write result to file for debugging
+        if os.getenv("DUMP_TRACECAT_RESULT", "0") in ("1", "true"):
+            path = config.TRACECAT__EXECUTIONS_DIR / f"{wf_exec_id}.json"
+            path.touch()
+            with path.open("w") as f:
+                json.dump(result, f, indent=2)
+        else:
+            logger.debug(f"Workflow result:\n{json.dumps(result, indent=2)}")
+        return DispatchWorkflowResult(wf_id=wf_id, final_context=result)
