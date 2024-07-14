14b89b2d5954d092aa9e4a8c2720095011fd48ae
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-07-14 19:30:52 +0100

    refactor(ui): Move community nav into user nav

--- frontend/src/components/nav/navbar.tsx
+++ frontend/src/components/nav/navbar.tsx
@@ -4,7 +4,6 @@ import React from "react"
 import Link from "next/link"
 
 import { Icons } from "@/components/icons"
-import { CommunityNav } from "@/components/nav/community"
 import DynamicNavbar from "@/components/nav/dynamic-nav"
 import UserNav from "@/components/nav/user-nav"
 
@@ -18,7 +17,6 @@ export default function Navbar(props: NavbarProps) {
         </Link>
         <DynamicNavbar />
         <div className="flex flex-1 items-center justify-end space-x-6">
-          <CommunityNav />
           <UserNav />
         </div>
       </div>
