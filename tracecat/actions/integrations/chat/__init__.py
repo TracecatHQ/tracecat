2126682ec7d2a0fdaa8be176f6123192e6c3c320
Chris Lo <46541035+topher-lo@users.noreply.github.com>
2024-07-16 18:50:53 -0400

    fix look up exception

--- tracecat/actions/integrations/chat/__init__.py
+++ tracecat/actions/integrations/chat/__init__.py
@@ -1,3 +1,3 @@
-from .slack import list_slack_users, post_slack_message
+from .slack import list_slack_conversations, list_slack_users, post_slack_message
 
-__all__ = ["list_slack_users", "post_slack_message"]
+__all__ = ["list_slack_users", "post_slack_message", "list_slack_conversations"]
