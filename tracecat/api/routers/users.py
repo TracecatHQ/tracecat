b5c8cedf62d9ca5d05b0e19afd79fc30c6b0dd95
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-07-31 00:37:29 +0100

    feat(engine): Make users async

--- tracecat/api/routers/users.py
+++ tracecat/api/routers/users.py
@@ -2,10 +2,11 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.exc import NoResultFound
-from sqlmodel import Session, select
+from sqlmodel import select
+from sqlmodel.ext.asyncio.session import AsyncSession
 
 from tracecat.auth.credentials import authenticate_user
-from tracecat.db.engine import get_session
+from tracecat.db.engine import get_async_session
 from tracecat.db.schemas import User
 from tracecat.types.api import UpdateUserParams
 from tracecat.types.auth import Role
@@ -14,15 +15,15 @@ router = APIRouter(prefix="/users")
 
 
 @router.post("", status_code=status.HTTP_201_CREATED, tags=["users"])
-def create_user(
+async def create_user(
     role: Annotated[Role, Depends(authenticate_user)],
-    session: Session = Depends(get_session),
+    session: AsyncSession = Depends(get_async_session),
 ) -> User:
     """Create new user."""
 
     # Check if user exists
     statement = select(User).where(User.id == role.user_id).limit(1)
-    result = session.exec(statement)
+    result = await session.exec(statement)
 
     user = result.one_or_none()
     if user is not None:
@@ -32,40 +33,40 @@ def create_user(
     user = User(owner_id="tracecat", id=role.user_id)
 
     session.add(user)
-    session.commit()
-    session.refresh(user)
+    await session.commit()
+    await session.refresh(user)
     return user
 
 
 @router.get("", tags=["users"])
-def get_user(
+async def get_user(
     role: Annotated[Role, Depends(authenticate_user)],
-    session: Session = Depends(get_session),
+    session: AsyncSession = Depends(get_async_session),
 ) -> User:
     """Get a user."""
 
     # Get user given user_id
     statement = select(User).where(User.id == role.user_id)
-    result = session.exec(statement)
+    result = await session.exec(statement)
     try:
         user = result.one()
-        return user
     except NoResultFound as e:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
         ) from e
+    return user
 
 
 @router.post("", status_code=status.HTTP_204_NO_CONTENT, tags=["users"])
-def update_user(
+async def update_user(
     role: Annotated[Role, Depends(authenticate_user)],
     params: UpdateUserParams,
-    session: Session = Depends(get_session),
+    session: AsyncSession = Depends(get_async_session),
 ) -> None:
     """Update a user."""
 
     statement = select(User).where(User.id == role.user_id)
-    result = session.exec(statement)
+    result = await session.exec(statement)
     try:
         user = result.one()
     except NoResultFound as e:
@@ -79,23 +80,23 @@ def update_user(
         user.settings = params.settings
 
     session.add(user)
-    session.commit()
+    await session.commit()
 
 
 @router.delete("", status_code=status.HTTP_204_NO_CONTENT, tags=["users"])
-def delete_user(
+async def delete_user(
     role: Annotated[Role, Depends(authenticate_user)],
-    session: Session = Depends(get_session),
+    session: AsyncSession = Depends(get_async_session),
 ) -> None:
     """Delete a user."""
 
     statement = select(User).where(User.id == role.user_id)
-    result = session.exec(statement)
+    result = await session.exec(statement)
     try:
         user = result.one()
     except NoResultFound as e:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
         ) from e
-    session.delete(user)
-    session.commit()
+    await session.delete(user)
+    await session.commit()
