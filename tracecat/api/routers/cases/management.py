8a070993b7a9462106defe6d41d9f8721af91775
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-07-31 00:30:47 +0100

    feat(engine): Make cases async

--- tracecat/api/routers/cases/management.py
+++ tracecat/api/routers/cases/management.py
@@ -2,10 +2,11 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.exc import NoResultFound
-from sqlmodel import Session, select
+from sqlmodel import select
+from sqlmodel.ext.asyncio.session import AsyncSession
 
 from tracecat.auth.credentials import authenticate_service, authenticate_user
-from tracecat.db.engine import get_session
+from tracecat.db.engine import get_async_session
 from tracecat.db.schemas import Case, CaseEvent
 from tracecat.types.api import CaseEventParams, CaseParams, CaseResponse
 from tracecat.types.auth import Role
@@ -18,11 +19,11 @@ router = APIRouter()
     status_code=status.HTTP_201_CREATED,
     tags=["cases"],
 )
-def create_case(
+async def create_case(
     role: Annotated[Role, Depends(authenticate_service)],  # M2M
     workflow_id: str,
     cases: list[CaseParams],
-    session: Session = Depends(get_session),
+    session: AsyncSession = Depends(get_async_session),
 ) -> CaseResponse:
     """Create a new case for a workflow."""
     case = Case(
@@ -31,8 +32,8 @@ def create_case(
         **cases.model_dump(),
     )
     session.add(case)
-    session.commit()
-    session.refresh(case)
+    await session.commit()
+    await session.refresh(case)
 
     return CaseResponse(
         id=case.id,
@@ -52,17 +53,17 @@ def create_case(
 
 
 @router.get("/workflows/{workflow_id}/cases", tags=["cases"])
-def list_cases(
+async def list_cases(
     role: Annotated[Role, Depends(authenticate_user)],
     workflow_id: str,
     limit: int = 100,
-    session: Session = Depends(get_session),
+    session: AsyncSession = Depends(get_async_session),
 ) -> list[CaseResponse]:
     """List all cases for a workflow."""
     query = select(Case).where(
         Case.owner_id == role.user_id, Case.workflow_id == workflow_id
     )
-    result = session.exec(query)
+    result = await session.exec(query)
     try:
         cases = result.fetchmany(size=limit)
     except NoResultFound as e:
@@ -91,11 +92,11 @@ def list_cases(
 
 
 @router.get("/workflows/{workflow_id}/cases/{case_id}", tags=["cases"])
-def get_case(
+async def get_case(
     role: Annotated[Role, Depends(authenticate_user)],
     workflow_id: str,
     case_id: str,
-    session: Session = Depends(get_session),
+    session: AsyncSession = Depends(get_async_session),
 ) -> CaseResponse:
     """Get a specific case for a workflow."""
     query = select(Case).where(
@@ -103,7 +104,8 @@ def get_case(
         Case.workflow_id == workflow_id,
         Case.id == case_id,
     )
-    case = session.exec(query).one_or_none()
+    result = await session.exec(query)
+    case = result.one_or_none()
     if case is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Resource not found"
@@ -126,12 +128,12 @@ def get_case(
 
 
 @router.post("/workflows/{workflow_id}/cases/{case_id}", tags=["cases"])
-def update_case(
+async def update_case(
     role: Annotated[Role, Depends(authenticate_user)],
     workflow_id: str,
     case_id: str,
     params: CaseParams,
-    session: Session = Depends(get_session),
+    session: AsyncSession = Depends(get_async_session),
 ) -> CaseResponse:
     """Update a specific case for a workflow."""
     query = select(Case).where(
@@ -139,7 +141,8 @@ def update_case(
         Case.workflow_id == workflow_id,
         Case.id == case_id,
     )
-    case = session.exec(query).one_or_none()
+    result = await session.exec(query)
+    case = result.one_or_none()
     if case is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Resource not found"
@@ -150,8 +153,8 @@ def update_case(
         setattr(case, key, value)
 
     session.add(case)
-    session.commit()
-    session.refresh(case)
+    await session.commit()
+    await session.refresh(case)
     return CaseResponse(
         id=case.id,
         owner_id=case.owner_id,
@@ -174,12 +177,12 @@ def update_case(
     status_code=status.HTTP_201_CREATED,
     tags=["cases"],
 )
-def create_case_event(
+async def create_case_event(
     role: Annotated[Role, Depends(authenticate_user)],
     workflow_id: str,
     case_id: str,
     params: CaseEventParams,
-    session: Session = Depends(get_session),
+    session: AsyncSession = Depends(get_async_session),
 ) -> None:
     """Create a new Case Event."""
     case_event = CaseEvent(
@@ -190,17 +193,17 @@ def create_case_event(
         **params.model_dump(),
     )
     session.add(case_event)
-    session.commit()
-    session.refresh(case_event)
+    await session.commit()
+    await session.refresh(case_event)
     return case_event
 
 
 @router.get("/workflows/{workflow_id}/cases/{case_id}/events", tags=["cases"])
-def list_case_events(
+async def list_case_events(
     role: Annotated[Role, Depends(authenticate_user)],
     workflow_id: str,
     case_id: str,
-    session: Session = Depends(get_session),
+    session: AsyncSession = Depends(get_async_session),
 ) -> list[CaseEvent]:
     """List all Case Events."""
     query = select(CaseEvent).where(
@@ -208,19 +211,20 @@ def list_case_events(
         CaseEvent.workflow_id == workflow_id,
         CaseEvent.case_id == case_id,
     )
-    case_events = session.exec(query).all()
+    result = await session.exec(query)
+    case_events = result.all()
     return case_events
 
 
 @router.get(
     "/workflows/{workflow_id}/cases/{case_id}/events/{event_id}", tags=["cases"]
 )
-def get_case_event(
+async def get_case_event(
     role: Annotated[Role, Depends(authenticate_user)],
     workflow_id: str,
     case_id: str,
     event_id: str,
-    session: Session = Depends(get_session),
+    session: AsyncSession = Depends(get_async_session),
 ):
     """Get a specific case event."""
     query = select(CaseEvent).where(
@@ -229,7 +233,8 @@ def get_case_event(
         CaseEvent.case_id == case_id,
         CaseEvent.id == event_id,
     )
-    case_event = session.exec(query).one_or_none()
+    result = await session.exec(query)
+    case_event = result.one_or_none()
     if case_event is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Resource not found"
