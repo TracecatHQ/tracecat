8eba10cb10784a4b2070384d1b27e613a8d60f2f
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-08-06 11:59:20 -0400

    refactor(ui): Remove disable auth

--- frontend/src/config/auth.ts
+++ frontend/src/config/auth.ts
@@ -1,7 +1,6 @@
 import { env } from "next-runtime-env"
 
 export const authConfig = {
-  disabled: ["1", "true"].includes(env("NEXT_PUBLIC_AUTH_DISABLED") || "false"),
   authType: env("NEXT_PUBLIC_AUTH_TYPE") || "basic",
   staleTime: 5 * 60 * 1000, // 5 minutes
 }
