55da7bb9aff6dee74cf07d31fa9d8f64b622833d
topher-lo <46541035+topher-lo@users.noreply.github.com>
2024-08-08 17:55:02 -0700

    fix Ownership mapping table types

--- tracecat/db/schemas.py
+++ tracecat/db/schemas.py
@@ -69,7 +69,7 @@ class Membership(SQLModel, table=True):
 
 
 class Ownership(SQLModel, table=True):
-    """Mapping table to store ownership of resources.
+    """Table to map resources to owners.
 
     - Organization owns all workspaces
     - One specific user owns the organization
@@ -81,10 +81,10 @@ class Ownership(SQLModel, table=True):
     - Organization (given by a  UUID4 sentinel value created on database creation)
     """
 
-    owner_id: UUID4 = Field(primary_key=True)
-    owner_type: str = Field(primary_key=True)
-    resource_id: str | None = Field(primary_key=True)
-    resource_type: str = Field(primary_key=True)
+    resource_id: str = Field(nullable=False, primary_key=True)
+    resource_type: str
+    owner_id: UUID4
+    owner_type: str
 
 
 class Workspace(Resource, table=True):
