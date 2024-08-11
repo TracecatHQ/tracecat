6e915e0906ed36c26f5a07d21187f4d0aafc969a
topher-lo <46541035+topher-lo@users.noreply.github.com>
2024-08-10 19:01:36 -0700

    Missing imports into env.py

--- alembic/env.py
+++ alembic/env.py
@@ -1,9 +1,11 @@
 from logging.config import fileConfig
 
+import alembic_postgresql_enum  # noqa: F401
 from sqlalchemy import engine_from_config, pool
 from sqlmodel import SQLModel
 
 from alembic import context
+from tracecat.db import schemas  # noqa: F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
