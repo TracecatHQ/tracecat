2517a4cce49c3ca2352e4bf23a91211800e72b96
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-08-17 02:12:50 +0100

    Add view when user not in any workspace

--- frontend/src/app/workspaces/layout.tsx
+++ frontend/src/app/workspaces/layout.tsx
@@ -1,10 +1,15 @@
 "use client"
 
+import Image from "next/image"
 import { useParams } from "next/navigation"
+import { useAuth } from "@/providers/auth"
 import { WorkflowProvider } from "@/providers/workflow"
 import { WorkspaceProvider } from "@/providers/workspace"
+import { LogOut } from "lucide-react"
+import TracecatIcon from "public/icon.png"
 
 import { useWorkspaceManager } from "@/lib/hooks"
+import { Button } from "@/components/ui/button"
 import { CenteredSpinner } from "@/components/loading/spinner"
 import { DynamicNavbar } from "@/components/nav/dynamic-nav"
 
@@ -23,15 +28,12 @@ export default function WorkspaceLayout({
     throw workspacesError
   }
   let wsId: string
-  if (!workspaceId) {
-    // If no workspaceId is provided, use the first workspace
-    if (workspaces.length === 0) {
-      throw new Error("No workspaces found")
-    } else {
-      wsId = workspaces[0].id
-    }
-  } else {
+  if (workspaceId) {
     wsId = workspaceId
+  } else if (workspaces.length > 0) {
+    wsId = workspaces[0].id
+  } else {
+    return <NoWorkspaces />
   }
 
   console.log("Redirecting to workspace", wsId)
@@ -39,11 +41,32 @@ export default function WorkspaceLayout({
     <WorkspaceProvider workspaceId={wsId}>
       <WorkflowProvider workspaceId={wsId}>
         <div className="no-scrollbar flex h-screen max-h-screen flex-col">
-          {/* DynamicNavbar needs a WorkflowProvider */}
+          {/* DynamicNavbar needs a WorkflowProvider and a WorkspaceProvider */}
           <DynamicNavbar />
           {children}
         </div>
       </WorkflowProvider>
     </WorkspaceProvider>
+  )
+}
+
+function NoWorkspaces() {
+  const { logout } = useAuth()
+  const handleLogout = async () => {
+    await logout()
+  }
+  return (
+    <main className="container flex size-full max-w-[400px] flex-col items-center justify-center space-y-4">
+      <Image src={TracecatIcon} alt="Tracecat" className="mb-4 size-16" />
+      <h1 className="text-2xl font-semibold tracking-tight">No Workspaces</h1>
+      <span className="text-center text-muted-foreground">
+        You are not a member of any workspace. Please contact your
+        administrator.
+      </span>
+      <Button variant="outline" onClick={handleLogout}>
+        <LogOut className="mr-2 size-4" />
+        <span>Logout</span>
+      </Button>
+    </main>
   )
 }
