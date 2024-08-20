2fc8f6b84a8c381ade9664cdd69555b513037d34
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-08-20 20:11:32 +0100

    feat(ui): Add playbooks to siteConfig

--- frontend/src/config/site.ts
+++ frontend/src/config/site.ts
@@ -15,6 +15,7 @@ export const siteConfig: SiteConfig = {
     github: "https://github.com/TracecatHQ/tracecat",
     discord: "https://discord.gg/n3GF4qxFU8",
     docs: "https://docs.tracecat.com",
+    playbooks: "https://github.com/TracecatHQ/tracecat/tree/main/playbooks",
   },
   ogImage: `${env.NEXT_PUBLIC_APP_URL}/og.jpg`,
 }
