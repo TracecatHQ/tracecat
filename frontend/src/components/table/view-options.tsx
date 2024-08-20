10a99d664441d33f192abf3a656a1371e07e1f78
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-08-20 17:29:30 +0100

    feat(ui): Don't show table view if no filter cols are available

--- frontend/src/components/table/view-options.tsx
+++ frontend/src/components/table/view-options.tsx
@@ -19,6 +19,15 @@ interface DataTableViewOptionsProps<TData> {
 export function DataTableViewOptions<TData>({
   table,
 }: DataTableViewOptionsProps<TData>) {
+  const filterCols = table
+    .getAllColumns()
+    .filter(
+      (column) =>
+        typeof column.accessorFn !== "undefined" && column.getCanHide()
+    )
+  if (filterCols.length === 0) {
+    return null
+  }
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -27,29 +36,23 @@ export function DataTableViewOptions<TData>({
           size="sm"
           className="ml-auto hidden h-8 lg:flex"
         >
-          <MixerHorizontalIcon className="mr-2 size-4" />
+          <MixerHorizontalIcon className="mr-2 size-4 text-xs" />
           View
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-[150px]">
         <DropdownMenuSeparator />
-        {table
-          .getAllColumns()
-          .filter(
-            (column) =>
-              typeof column.accessorFn !== "undefined" && column.getCanHide()
+        {filterCols.map((column) => {
+          return (
+            <DropdownMenuCheckboxItem
+              key={column.id}
+              checked={column.getIsVisible()}
+              onCheckedChange={(value) => column.toggleVisibility(!!value)}
+            >
+              {column.id}
+            </DropdownMenuCheckboxItem>
           )
-          .map((column) => {
-            return (
-              <DropdownMenuCheckboxItem
-                key={column.id}
-                checked={column.getIsVisible()}
-                onCheckedChange={(value) => column.toggleVisibility(!!value)}
-              >
-                {column.id}
-              </DropdownMenuCheckboxItem>
-            )
-          })}
+        })}
       </DropdownMenuContent>
     </DropdownMenu>
   )
