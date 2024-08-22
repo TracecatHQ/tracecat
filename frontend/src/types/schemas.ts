73a8a34a2b649d677352f315ba9504ed11c958e7
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-08-22 14:57:56 +0100

    refactor(ui): Cleanup unused schemas and remove unused validators

--- frontend/src/types/schemas.ts
+++ frontend/src/types/schemas.ts
@@ -1,52 +1,5 @@
 import { z } from "zod"
 
-import { keyValueSchema } from "@/types/validators"
-
-export const strAsDate = z.string().transform((x) => new Date(x))
-
-// Common resource Schema
-export const resourceSchema = z.object({
-  id: z.string(),
-  owner_id: z.string(),
-  created_at: strAsDate,
-  updated_at: strAsDate,
-})
-
-export type Resource = z.infer<typeof resourceSchema>
-
-/**
- * A trigger contains one or more webhooks and or schedules that can trigger a workflow.
- */
-export const webhookSchema = z
-  .object({
-    status: z.enum(["online", "offline"]),
-    method: z.enum(["GET", "POST"]),
-    filters: z.record(z.any()),
-    // Computed fields
-    url: z.string().url(),
-    secret: z.string(),
-  })
-  .and(resourceSchema)
-export type Webhook = z.infer<typeof webhookSchema>
-
-/**
- * A trigger contains one or more webhooks and or schedules that can trigger a workflow.
- */
-export const scheduleSchema = z
-  .object({
-    status: z.enum(["online", "offline"]),
-    inputs: z.record(z.any()),
-    cron: z.string().nullish(),
-    every: z.string(),
-    offset: z.string().nullable(),
-    start_at: strAsDate.nullable(),
-    end_at: strAsDate.nullable(),
-    workflow_id: z.string(),
-  })
-  .and(resourceSchema)
-export type Schedule = z.infer<typeof scheduleSchema>
-/** Workflow Schemas */
-
 export const caseActionTypes = [
   "ignore",
   "quarantine",
@@ -74,22 +27,17 @@ export type CasePriorityType = (typeof casePriorityTypes)[number]
 export const secretTypes = ["custom", "token", "oauth2"] as const
 export type SecretType = (typeof secretTypes)[number]
 
+export const secretKeyValueSchema = z.object({
+  key: z.string().min(1, "Please enter a key."),
+  value: z.string().min(1, "Please enter a value."),
+})
+
 export const createSecretSchema = z.object({
   id: z.string().min(1).optional(),
   type: z.enum(secretTypes),
   name: z.string().min(1, "Please enter a secret name."),
   description: z.string().max(255).nullish(),
-  keys: z.array(keyValueSchema),
+  keys: z.array(secretKeyValueSchema),
 })
 
 export type CreateSecretParams = z.infer<typeof createSecretSchema>
-
-export const secretSchema = z.object({
-  id: z.string().min(1).optional(),
-  type: z.enum(secretTypes),
-  name: z.string().min(1, "Please enter a secret name."),
-  description: z.string().max(255).nullish(),
-  keys: z.array(z.string()),
-})
-
-export type Secret = z.infer<typeof secretSchema>
