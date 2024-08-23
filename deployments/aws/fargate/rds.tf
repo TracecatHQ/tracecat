d6d81e5c35fd993942ac194468a6d858d38e2da7
topher-lo <46541035+topher-lo@users.noreply.github.com>
2024-08-23 13:16:22 -0400

    Move rds vars into variables and locals into same file

--- deployments/aws/fargate/rds.tf
+++ deployments/aws/fargate/rds.tf
@@ -1,23 +1,13 @@
-variable "instance_class" {
-  type    = string
-  default = "db.t3"
-}
-
-variable "instance_size" {
-  type    = string
-  default = "medium" 
-}
-
 resource "aws_db_instance" "core_database" {
   identifier           = "core-database"
   engine               = "postgres"
   engine_version       = "16.3"
-  instance_class       = "${var.instance_class}.${var.instance_size}"
+  instance_class       = "${var.db_instance_class}.${var.db_instance_size}"
   allocated_storage    = 20
   storage_type         = "gp2"
   username             = "postgres"
   multi_az              = true
-  password             = var.db_pass_value 
+  password = local.secrets.TRACECAT__DB_PASSWORD
   db_subnet_group_name = aws_db_subnet_group.tracecat_db_subnet.name
   vpc_security_group_ids = [aws_security_group.core_db_security.id]
   skip_final_snapshot  = true
@@ -27,11 +17,11 @@ resource "aws_db_instance" "temporal_database" {
   identifier           = "temporal-database"
   engine               = "postgres"
   engine_version       = "13.11"
-  instance_class       = "${var.instance_class}.${var.instance_size}"
+  instance_class       = "${var.db_instance_class}.${var.db_instance_size}"
   allocated_storage    = 20
   storage_type         = "gp2"
   username             = "postgres"
-  password             = var.db_pass_value 
+  password = local.secrets.TEMPORAL__DB_PASSWORD
   multi_az              = true
   db_subnet_group_name = aws_db_subnet_group.tracecat_db_subnet.name
   vpc_security_group_ids = [aws_security_group.core_db_security.id]
@@ -41,4 +31,9 @@ resource "aws_db_instance" "temporal_database" {
 resource "aws_db_subnet_group" "tracecat_db_subnet" {
   name       = "tracecat-db-subnet"
   subnet_ids = aws_subnet.private[*].id
+}
+
+locals {
+  core_db_hostname = split(":", aws_db_instance.core_database.endpoint)[0]
+  temp_db_hostname = split(":", aws_db_instance.temporal_database.endpoint)[0]
 }
