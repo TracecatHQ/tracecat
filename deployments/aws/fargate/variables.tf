a88028588d516465ff465e3b1ddfc07f4c546c4c
topher-lo <46541035+topher-lo@users.noreply.github.com>
2024-08-24 12:50:54 -0400

    Organize tracecat env vars into locals

--- deployments/aws/fargate/variables.tf
+++ deployments/aws/fargate/variables.tf
@@ -5,8 +5,8 @@ variable "aws_region" {
 }
 
 variable "az_count" {
-    description = "Number of AZs to cover in a given region"
-    default = "2"
+  description = "Number of AZs to cover in a given region"
+  default     = "2"
 }
 
 variable "log_level" {
@@ -15,7 +15,7 @@ variable "log_level" {
 }
 
 variable "tracecat_app_env" {
-  type = string
+  type    = string
   default = "production"
 }
 
@@ -42,40 +42,40 @@ variable "tracecat_image_api_tag" {
 ### Secret ARNs
 
 variable "tracecat_db_password_arn" {
-  type = string
+  type        = string
   description = "The ARN of the secret containing the Tracecat database password"
 }
 
 variable "temporal_db_password_arn" {
-  type = string
+  type        = string
   description = "The ARN of the secret containing the Temporal database password"
 }
 
 variable "tracecat_db_encryption_key_arn" {
-  type = string
+  type        = string
   description = "The ARN of the secret containing the Tracecat database encryption key"
 }
 
 variable "tracecat_service_key_arn" {
-  type = string
+  type        = string
   description = "The ARN of the secret containing the Tracecat service key"
 }
 
 variable "tracecat_signing_secret_arn" {
-  type = string
+  type        = string
   description = "The ARN of the secret containing the Tracecat signing secret"
 }
 
 variable "oauth_client_id" {
-  type = string
+  type        = string
   description = "The OAuth client ID (optional)"
-  default = null
+  default     = null
 }
 
 variable "oauth_client_secret" {
-  type = string
+  type        = string
   description = "The OAuth client secret (optional)"
-  default = null
+  default     = null
 }
 
 ### DNS
@@ -92,7 +92,7 @@ variable "domain_name" {
 
 variable "api_cpu" {
   default = "256"
-} 
+}
 
 variable "api_memory" {
   default = "512"
@@ -100,7 +100,7 @@ variable "api_memory" {
 
 variable "worker_cpu" {
   default = "256"
-} 
+}
 
 variable "worker_memory" {
   default = "512"
@@ -108,19 +108,19 @@ variable "worker_memory" {
 
 variable "ui_cpu" {
   default = "256"
-} 
+}
 
 variable "ui_memory" {
   default = "512"
-} 
+}
 
 variable "temporal_cpu" {
   default = "256"
-} 
+}
 
 variable "temporal_memory" {
   default = "512"
-} 
+}
 
 variable "db_instance_class" {
   type    = string
@@ -129,7 +129,7 @@ variable "db_instance_class" {
 
 variable "db_instance_size" {
   type    = string
-  default = "medium" 
+  default = "medium"
 }
 
 ### Container Env Vars
@@ -137,109 +137,116 @@ variable "db_instance_size" {
 # and specified directly in the task definition via a secret reference
 
 ### Container Env Vars
+# PUBLIC_APP_URL = https://{var.domain_name}
+# PUBLIC_API_URL= https://{var.domain_name}/api/
+# INTERNAL_API_URL =  http://api-service:8000
 
-variable "tracecat_api_url" {
-  type = string
-  description = "The URL of the Tracecat API"
-}
-
-variable "tracecat_api_root_path" {
-  type = string
-  description = "The root path of the Tracecat API"
-  default = "/api"
-}
 
 variable "log_level" {
-  type = string
+  type        = string
   description = "Log level for the application"
 }
 
 variable "tracecat_app_env" {
-  type = string
+  type        = string
   description = "The environment of the Tracecat application"
 }
 
 variable "tracecat_db_sslmode" {
-  type = string
+  type        = string
   description = "SSL mode for the database connection"
 }
 
 variable "tracecat_db_uri" {
-  type = string
+  type        = string
   description = "Database URI"
 }
 
 variable "tracecat_public_runner_url" {
-  type = string
+  type        = string
   description = "Public URL for the runner"
 }
 
 variable "tracecat_public_app_url" {
-  type = string
+  type        = string
   description = "Public URL for the application"
 }
 
 variable "tracecat_allow_origins" {
-  type = string
+  type        = string
   description = "Allowed origins for CORS"
 }
 
 # UI
 
 variable "auth_types" {
-  type = string
+  type    = string
   default = "basic,google_oauth"
-}
-
-# Temporal
-
-variable "temporal_cluster_url" {
-  type = string
-  description = "Temporal cluster URL"
-}
-
-variable "temporal_cluster_queue" {
-  type = string
-  description = "Temporal cluster queue"
 }
 
 ### Locals to organize env vars
 locals {
-  api_env = {
-    LOG_LEVEL                    = var.log_level
-    TRACECAT__API_URL            = var.tracecat_api_url
-    TRACECAT__API_ROOT_PATH      = var.tracecat_api_root_path
-    TRACECAT__APP_ENV            = var.tracecat_app_env
-    TRACECAT__DB_SSLMODE         = var.tracecat_db_sslmode
-    TRACECAT__DB_URI             = var.tracecat_db_uri
-    TRACECAT__PUBLIC_RUNNER_URL  = var.tracecat_public_runner_url
-    TRACECAT__PUBLIC_APP_URL     = var.tracecat_public_app_url
-    TRACECAT__ALLOW_ORIGINS      = var.tracecat_allow_origins
-    TRACECAT__AUTH_TYPES         = var.auth_types
-    TEMPORAL__CLUSTER_URL        = var.temporal_cluster_url
-    TEMPORAL__CLUSTER_QUEUE      = var.temporal_cluster_queue
+
+  public_app_url         = "https://${var.domain_name}"
+  public_api_url         = "https://${var.domain_name}/api"
+  internal_api_url       = "http://api-service:8000" # Service connect DNS name
+  temporal_cluster_url   = "temporal-service:7233"
+  temporal_cluster_queue = "tracecat-task-queue"
+
+  # Tracecat postgres env vars
+  # See: https://github.com/TracecatHQ/tracecat/blob/abd5ff/tracecat/db/engine.py#L21
+  tracecat_db_configs = {
+    # NOTE: still missing
+    # TRACECAT__DB_ENDPOINT which is the hostname of the RDS instance (from RDS resource)
+    # TRACECAT__DB_PASS which is the password for the database (from secrets manager)
+    TRACECAT__DB_USER     = "postgres"
+    TRACECAT__DB_PORT     = "5432"
+    TRACECAT__DB_NAME     = "postgres"  # Hardcoded in RDS resource configs
   }
-  worker_env = {
-    LOG_LEVEL                    = var.log_level
-    TRACECAT__API_URL            = var.tracecat_api_url
-    TRACECAT__API_ROOT_PATH      = var.tracecat_api_root_path
-    TRACECAT__APP_ENV            = var.tracecat_app_env
-    TRACECAT__DB_SSLMODE         = var.tracecat_db_sslmode
-    TRACECAT__DB_URI             = var.tracecat_db_uri
-    TRACECAT__PUBLIC_RUNNER_URL  = var.tracecat_public_runner_url
-    TEMPORAL__CLUSTER_URL        = var.temporal_cluster_url
-    TEMPORAL__CLUSTER_QUEUE      = var.temporal_cluster_queue
-  }
+
+  api_env = merge({
+    LOG_LEVEL                   = var.log_level
+    TRACECAT__API_URL           = local.internal_api_url
+    TRACECAT__API_ROOT_PATH     = "/api"
+    TRACECAT__APP_ENV           = var.tracecat_app_env    
+    TRACECAT__PUBLIC_RUNNER_URL = local.public_api_url
+    TRACECAT__PUBLIC_APP_URL    = var.tracecat_public_app_url
+    TRACECAT__ALLOW_ORIGINS     = var.tracecat_allow_origins
+    TRACECAT__AUTH_TYPES        = var.auth_types
+    TEMPORAL__CLUSTER_URL       = local.temporal_cluster_url
+    TEMPORAL__CLUSTER_QUEUE     = local.temporal_cluster_queue
+  }, local.tracecat_db_configs)
+
+  worker_env = merge({
+    LOG_LEVEL                   = var.log_level
+    TRACECAT__API_URL           = local.internal_api_url
+    TRACECAT__API_ROOT_PATH     = "/api"
+    TRACECAT__APP_ENV           = var.tracecat_app_env
+    TRACECAT__PUBLIC_RUNNER_URL = local.public_api_url
+    TEMPORAL__CLUSTER_URL       = local.temporal_cluster_url
+    TEMPORAL__CLUSTER_QUEUE     = local.temporal_cluster_queue
+    }, local.tracecat_db_configs)
+
   ui_env = {
-    NEXT_PUBLIC_API_URL          = "${var.tracecat_api_url}/api/"
-    NEXT_PUBLIC_APP_ENV          = var.tracecat_app_env
-    NEXT_PUBLIC_APP_URL          = var.tracecat_public_app_url
-    NEXT_PUBLIC_AUTH_TYPES       = var.auth_types
-    NEXT_SERVER_API_URL          = "http://api-service:8000"  # Service connect DNS name
-    NODE_ENV                     = var.tracecat_app_env
+    NEXT_PUBLIC_API_URL    = local.public_api_url
+    NEXT_PUBLIC_APP_ENV    = var.tracecat_app_env
+    NEXT_PUBLIC_APP_URL    = var.tracecat_public_app_url
+    NEXT_PUBLIC_AUTH_TYPES = var.auth_types
+    NEXT_SERVER_API_URL    = local.internal_api_url
+    NODE_ENV               = var.tracecat_app_env
   }
+
   temporal_env = {
-    TEMPORAL__CLUSTER_URL        = var.temporal_cluster_url
-    TEMPORAL__CLUSTER_QUEUE      = var.temporal_cluster_queue
+    # NOTE: still missing
+    # POSTGRES_SEEDS which is the hostname of the RDS instance (from RDS resource)
+    # POSTGRES_PWD which is the password for the database (from secrets manager)
+    DB                      = "postgres12"
+    DB_PORT                 = "5432"
+    POSTGRES_USER           = "postgres" # Hardcoded in RDS resource configs
+    LOG_LEVEL               = "warn"
+    # NOTE: The following two variables are required for
+    # Temporal in Fargate. They are not required in docker compose.
+    TEMPORAL_BROADCAST_ADDRESS = "0.0.0.0"
+    BIND_ON_IP                 = "0.0.0.0"
   }
 }
