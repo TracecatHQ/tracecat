412bc71d1006ac7682908e5adf459368f5c8a9dc
topher-lo <46541035+topher-lo@users.noreply.github.com>
2024-08-25 18:46:29 -0400

    Replace cname with a record

--- deployments/aws/fargate/dns.tf
+++ deployments/aws/fargate/dns.tf
@@ -1,7 +1,11 @@
-resource "aws_route53_record" "this" {
+resource "aws_route53_record" "a_record" {
   zone_id = var.hosted_zone_id
   name    = var.domain_name
-  type    = "CNAME"
-  ttl     = "300"
-  records = [aws_alb.this.dns_name]
+  type    = "A"
+
+  alias {
+    name                   = aws_alb.this.dns_name
+    zone_id                = aws_alb.this.zone_id
+    evaluate_target_health = true
+  }
 }
