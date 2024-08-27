0f47080ad1bb1303eae5c012016c8d881066c863
xander Luedtke <xander@luedtke.ca>
2024-08-27 13:25:18 -0600

    Removing sublime_analyze_email back to emailrep.

--- tracecat/actions/integrations/email/__init__.py
+++ tracecat/actions/integrations/email/__init__.py
@@ -1,12 +1,11 @@
 """Sending email."""
 
 from .resend import send_email, send_email_resend
-from .sublime import sublime_analyze_email, sublime_analyze_link, sublime_message_group, sublime_message_data_model, sublime_message_attack_score
+from .sublime import sublime_analyze_link, sublime_message_group, sublime_message_data_model, sublime_message_attack_score
 
 __all__ = [
   "send_email_resend", 
-  "send_email", 
-  "sublime_analyze_email", 
+  "send_email",  
   "sublime_analyze_link", 
   "sublime_message_group",
   "sublime_message_data_model", 
