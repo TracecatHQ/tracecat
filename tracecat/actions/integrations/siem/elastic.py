f8deb1eaf0d6de04f52a21e68845a8bdf077421d
benacumen <ben.jackson@acumencyber.com>
2024-09-06 11:54:58 +0000

    Add update alert status action

--- tracecat/actions/integrations/siem/elastic.py
+++ tracecat/actions/integrations/siem/elastic.py
@@ -102,3 +102,103 @@ async def list_elastic_alerts(
         response = await client.post(url, headers=headers, json=query)
         response.raise_for_status()  # Raise an exception for HTTP errors
         return response.json()
+
+@registry.register(
+    default_title="Acumen - List Elastic Security alerts",
+    description="Fetch all alerts from Elastic Security and filter by time range, excluding Building Block rules.",
+    display_group="Elastic",
+    namespace="integrations.elastic",
+    secrets=[elastic_secret],
+)
+async def acumen_list_elastic_alerts(
+    start_time: Annotated[
+        datetime,
+        Field(..., description="Start time, return alerts created after this time."),
+    ],
+    end_time: Annotated[
+        datetime,
+        Field(..., description="End time, return alerts created before this time."),
+    ],
+    limit: Annotated[
+        int, Field(default=100, description="Maximum number of alerts to return.")
+    ] = 100,
+) -> list[dict[str, Any]]:
+    api_key = os.getenv("ELASTIC_API_KEY")
+    api_url = os.getenv("ELASTIC_API_URL")
+
+    url = f"{api_url}/api/detection_engine/signals/search"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"ApiKey {api_key}",
+        "kbn-xsrf": "kibana",
+    }
+    query = {
+    "size": limit,
+    "query": {
+        "bool": {
+            "filter": [
+                {
+                    "range": {
+                        "@timestamp": {
+                            "gte": start_time.isoformat(),
+                            "lte": end_time.isoformat(),
+                        }
+                    }
+                },
+                {"match": {"signal.status": "open"}}
+            ],
+            "must_not": [
+                {
+                    "exists": {
+                        "field": "kibana.alert.building_block_type"
+                    }
+                }
+            ]
+
+        }
+    }
+}
+
+    async with httpx.AsyncClient(timeout=10.0) as client:
+        response = await client.post(url, headers=headers, json=query)
+        response.raise_for_status()  # Raise an exception for HTTP errors
+        return response.json()
+    
+    
+@registry.register(
+    default_title="Update Elastic Security Alert Status",
+    description="Updates the status of Elastic Security Alerts.",
+    display_group="Elastic",
+    namespace="integrations.elastic",
+    secrets=[elastic_secret],
+)
+async def update_alert_status(
+    status: Annotated[
+        str,
+        Field(..., description="The desired status for the alert ('open', 'acknowledged', 'closed')")
+    ],
+    alert_ids: Annotated[
+        list[str],
+        Field(..., description="An array of alert IDs you wish to update.")
+    ]
+) -> dict[str, Any]:
+    
+    api_key = os.getenv("ELASTIC_API_KEY")
+    api_url = os.getenv("ELASTIC_API_URL")
+    url = f"{api_url}/api/detection_engine/signals/status"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"ApiKey {api_key}",
+        "kbn-xsrf": "kibana",
+    }
+    
+    payload = {
+        "status": status,
+        "signal_ids": alert_ids
+  }
+    
+    async with httpx.AsyncClient(timeout=10.0) as client:
+        response = await client.post(url,headers=headers, json=payload)
+        response.raise_for_status()
+        return response.json()
+    
