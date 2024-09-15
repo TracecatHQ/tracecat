b8f7ee4d3c968d7f3e23a18d8369a4d9e29bdc65
topher-lo <46541035+topher-lo@users.noreply.github.com>
2024-09-15 14:29:14 -0400

    MongoDB twas

---
+++ templates/mongodb/udfs/pymongo.py
@@ -0,0 +1,62 @@
+"""Generic interface to MongoDB objects and their methods (e.g. Collection) via PyMongo."""
+
+import os
+from typing import Annotated, Any
+
+from pymongo import MongoClient
+from pymongo.cursor import Cursor
+
+from tracecat.registry import Field, RegistrySecret, registry
+
+mongodb_secret = RegistrySecret(
+    name="mongodb",
+    keys=["MONGODB_CONNECTION_STRING"],
+)
+
+"""MongoDB Secret.
+
+- name: `mongodb`
+- key:
+    - `MONGODB_CONNECTION_STRING`
+"""
+
+
+@registry.register(
+    default_title="Perform MongoDB CRUD",
+    description="Performs a MongoDB operation on a specified collection.",
+    display_group="MongoDB",
+    namespace="integrations.mongodb",
+    secrets=[mongodb_secret],
+)
+async def perform_mongodb_crud(
+    operation: Annotated[
+        str,
+        Field(
+            ...,
+            description="Operation to perform on the MongoDB Collection, e.g. 'find', 'insert_one'.",
+        ),
+    ],
+    database_name: Annotated[
+        str,
+        Field(..., description="The name of the target database."),
+    ],
+    collection_name: Annotated[
+        str,
+        Field(..., description="The name of the target collection"),
+    ],
+    params: Annotated[
+        dict[str, Any],
+        Field(..., description="Parameters for the operation."),
+    ] = None,
+) -> dict[str, Any] | list[dict[str, Any]]:
+    params = params or {}
+    connection_string = os.getenv("MONGODB_CONNECTION_STRING")
+    client = MongoClient(connection_string)
+    db = client[database_name]
+    collection = db[collection_name]
+    result = getattr(collection, operation)(**params)
+
+    if isinstance(result, Cursor):
+        result = list(result)
+
+    return result
