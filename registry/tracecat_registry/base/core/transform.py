182204d498c13b3bd7b4b9552d286e5dd38b320a
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-09-24 21:16:26 +0800

    Allow usage of tracecat platform package inside registry

--- registry/tracecat_registry/base/core/transform.py
+++ registry/tracecat_registry/base/core/transform.py
@@ -5,13 +5,8 @@
 from typing import Annotated, Any
 
 from pydantic import Field
+from tracecat.expressions import functions
 
-# from tracecat.expressions import functions
-# from tracecat.expressions.functions import (
-#     FunctionConstraint,
-#     OperatorConstraint,
-#     custom_filter,
-# )
 from tracecat_registry import registry
 
 
@@ -27,48 +22,48 @@ def reshape(
     return value
 
 
-# @registry.register(
-#     namespace="core.transform",
-#     description="Filter a collection based on a condition.",
-#     default_title="Filter",
-#     display_group="Data Transform",
-# )
-# def filter(
-#     items: Annotated[list[Any], Field(..., description="A collection of items.")],
-#     constraint: Annotated[
-#         str | list[Any] | FunctionConstraint | OperatorConstraint,
-#         Field(
-#             ...,
-#             description=(
-#                 "A constraint to filter the collection."
-#                 "If a list is provided, it will be used as a set filter."
-#                 "If a string is provided, it will be evaluated as a restricted conditional expression."
-#                 " Container items are refereneced by `x` e.g. 'x > 2 and x < 6'"
-#             ),
-#         ),
-#     ],
-# ) -> Any:
-#     return custom_filter(items, constraint)
+@registry.register(
+    namespace="core.transform",
+    description="Filter a collection based on a condition.",
+    default_title="Filter",
+    display_group="Data Transform",
+)
+def filter(
+    items: Annotated[list[Any], Field(..., description="A collection of items.")],
+    constraint: Annotated[
+        str | list[Any] | functions.FunctionConstraint | functions.OperatorConstraint,
+        Field(
+            ...,
+            description=(
+                "A constraint to filter the collection."
+                "If a list is provided, it will be used as a set filter."
+                "If a string is provided, it will be evaluated as a restricted conditional expression."
+                " Container items are refereneced by `x` e.g. 'x > 2 and x < 6'"
+            ),
+        ),
+    ],
+) -> Any:
+    return functions.custom_filter(items, constraint)
 
 
-# @registry.register(
-#     namespace="core.transform",
-#     description="Build a reference table from a collection of items.",
-#     default_title="Build Reference Table",
-#     display_group="Data Transform",
-# )
-# def build_reference_table(
-#     items: Annotated[list[Any], Field(..., description="A collection of items.")],
-#     key: Annotated[
-#         str, Field(..., description="The key to index the reference table.")
-#     ],
-# ) -> Any:
-#     # Key is a jsonpath that references a field
-#     # This field will be used as the dict key
-#     mapping = {}
-#     for item in items:
-#         _key = functions.eval_jsonpath(key, operand=item)
-#         if not isinstance(_key, int | str):
-#             continue
-#         mapping[_key] = item
-#     return mapping
+@registry.register(
+    namespace="core.transform",
+    description="Build a reference table from a collection of items.",
+    default_title="Build Reference Table",
+    display_group="Data Transform",
+)
+def build_reference_table(
+    items: Annotated[list[Any], Field(..., description="A collection of items.")],
+    key: Annotated[
+        str, Field(..., description="The key to index the reference table.")
+    ],
+) -> Any:
+    # Key is a jsonpath that references a field
+    # This field will be used as the dict key
+    mapping = {}
+    for item in items:
+        _key = functions.eval_jsonpath(key, operand=item)
+        if not isinstance(_key, int | str):
+            continue
+        mapping[_key] = item
+    return mapping
