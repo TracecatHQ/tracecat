9332e2663bca9c57e75a7c6dbe3854b1e5ebf5cb
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-09-26 23:13:43 +0800

    app: Clearer description for auth router dependencies

--- tracecat/auth/dependencies.py
+++ tracecat/auth/dependencies.py
@@ -9,9 +9,15 @@ from tracecat.auth.credentials import (
 from tracecat.types.auth import Role
 
 WorkspaceUserRole = Annotated[Role, Depends(authenticate_user_for_workspace)]
-"""Dependency for a user role for a workspace."""
+"""Dependency for a user role for a workspace.
+
+Sets the `ctx_role` context variable.
+"""
 
 WorkspaceUserOrServiceRole = Annotated[
     Role, Depends(authenticate_user_or_service_for_workspace)
 ]
-"""Dependency for a user or service role for a workspace."""
+"""Dependency for a user or service role for a workspace.
+
+Sets the `ctx_role` context variable.
+"""
