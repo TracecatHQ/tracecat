7d95f5774321dea5cb1ef8c93af088b6da24cc53
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-09-26 23:12:32 +0800

    test: Add test registry endpoints

---
+++ tracecat/testing/registry.py
@@ -0,0 +1,51 @@
+"""Use this only for testing purposes."""
+
+import textwrap
+from importlib.machinery import ModuleSpec
+from types import ModuleType
+
+from fastapi import APIRouter, HTTPException, status
+from pydantic import BaseModel
+
+from tracecat import config
+from tracecat.auth.dependencies import WorkspaceUserOrServiceRole
+from tracecat.registry.manager import RegistryManager
+from tracecat.registry.store import Registry
+
+
+class TestRegistryParams(BaseModel):
+    version: str
+    code: str
+    module_name: str
+    validate_keys: list[str] | None = None
+
+
+router = APIRouter(prefix="/test-registry", tags=["test-registry"])
+
+
+@router.post("")
+async def register_test_module(
+    role: WorkspaceUserOrServiceRole, params: TestRegistryParams
+):
+    """Use this only for testing purposes."""
+    if config.TRACECAT__APP_ENV != "development":
+        # Failsafe
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="This endpoint is only available in development mode",
+        )
+    registry = Registry(params.version)
+
+    test_module = ModuleType(params.module_name)
+
+    # Create a module spec for the test module
+    module_spec = ModuleSpec(params.module_name, None)
+    test_module.__spec__ = module_spec
+    code = textwrap.dedent(params.code)
+    exec(code, test_module.__dict__)
+    registry._register_udfs_from_module(test_module)
+    for key in params.validate_keys or []:
+        assert key in registry
+    manager = RegistryManager()
+    manager.add_registry(registry)
+    assert params.version in manager.list_registries()
