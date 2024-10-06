0c0e3762c3318d85b85d158775892929d8a5d04a
topher-lo <46541035+topher-lo@users.noreply.github.com>
2024-10-06 15:13:47 -0700

    ci(infra): Add final_snapshot_identifier

--- deployments/aws/fargate/rds.tf
+++ deployments/aws/fargate/rds.tf
@@ -1,3 +1,15 @@
+resource "random_string" "core_snapshot_suffix" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
+resource "random_string" "temporal_snapshot_suffix" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
 resource "aws_db_instance" "core_database" {
   identifier                   = "core-database"
   engine                       = "postgres"
@@ -12,6 +24,7 @@ resource "aws_db_instance" "core_database" {
   db_subnet_group_name         = aws_db_subnet_group.tracecat_db_subnet.name
   vpc_security_group_ids       = [aws_security_group.core_db.id]
   skip_final_snapshot          = var.rds_skip_final_snapshot
+  final_snapshot_identifier    = "final-core-db-${local.snapshot_timestamp}-${random_string.core_snapshot_suffix.result}"
   deletion_protection          = var.rds_deletion_protection
   apply_immediately            = var.rds_apply_immediately
   backup_retention_period      = var.rds_backup_retention_period
@@ -33,6 +46,7 @@ resource "aws_db_instance" "temporal_database" {
   db_subnet_group_name         = aws_db_subnet_group.tracecat_db_subnet.name
   vpc_security_group_ids       = [aws_security_group.temporal_db.id]
   skip_final_snapshot          = var.rds_skip_final_snapshot
+  final_snapshot_identifier    = "final-temporal-db-${local.snapshot_timestamp}-${random_string.temporal_snapshot_suffix.result}"
   deletion_protection          = var.rds_deletion_protection
   apply_immediately            = var.rds_apply_immediately
   backup_retention_period      = var.rds_backup_retention_period
@@ -47,6 +61,7 @@ resource "aws_db_subnet_group" "tracecat_db_subnet" {
 
 # Local variables for database hostnames
 locals {
-  core_db_hostname = sensitive(split(":", aws_db_instance.core_database.endpoint)[0])
-  temp_db_hostname = sensitive(split(":", aws_db_instance.temporal_database.endpoint)[0])
+  snapshot_timestamp = formatdate("YYYY-MM-DD-hhmm", timestamp())
+  core_db_hostname   = sensitive(split(":", aws_db_instance.core_database.endpoint)[0])
+  temp_db_hostname   = sensitive(split(":", aws_db_instance.temporal_database.endpoint)[0])
 }
