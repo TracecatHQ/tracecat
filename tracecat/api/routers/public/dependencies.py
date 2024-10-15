34ae468fd066fd4a775260dfcf19d6de517fd336
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-10-15 08:47:53 +0100

    refactor(engine): Remove legacy callback router (#430)

--- tracecat/api/routers/public/dependencies.py
+++ tracecat/api/routers/public/dependencies.py
@@ -1,9 +1,6 @@
-import orjson
-from fastapi import (
-    HTTPException,
-    Request,
-    status,
-)
+from typing import Annotated
+
+from fastapi import Depends, HTTPException, Request, status
 from sqlalchemy.exc import NoResultFound
 from sqlmodel import select
 
@@ -11,95 +8,11 @@ from tracecat.contexts import ctx_role
 from tracecat.db.engine import get_async_session_context_manager
 from tracecat.db.schemas import Webhook, WorkflowDefinition
 from tracecat.logger import logger
-from tracecat.parse import parse_child_webhook
-from tracecat.types.api import ServiceCallbackAction
 from tracecat.types.auth import Role
 
 
-async def handle_service_callback(
-    request: Request, service: str
-) -> ServiceCallbackAction | None:
-    if service == "slack":
-        # if (
-        #     request.headers["user-agent"]
-        #     != "Slackbot 1.0 (+https://api.slack.com/robots)"
-        # ):
-        #     raise HTTPException(
-        #         status_code=status.HTTP_400_BAD_REQUEST,
-        #         detail="Invalid User-Agent",
-        #     )
-        # NOTE: This coroutine can only be consumed once!
-        form_data = await request.form()
-        json_payload = form_data.get("payload")
-        payload = orjson.loads(json_payload)
-        # Extract out the webhook
-        if (actions := payload.get("actions")) is None:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail="Slack callback: Invalid payload",
-            )
-        logger.info("Received slack action", actions=actions)
-        if len(actions) < 1:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail="Slack callback: No actions",
-            )
-        action = actions[0]
-        match action:
-            case {
-                "type": "static_select",
-                "action_id": url,
-                "selected_option": {"value": kv_params},
-            }:
-                # e.g.
-                # {
-                #     "type": "static_select",
-                #     "action_id": "...",
-                #     "block_id": "nMMIK",
-                #     "selected_option": {
-                #         "text": {
-                #             "type": "plain_text",
-                #             "text": "True Positive",
-                #             "emoji": True,
-                #         },
-                #         "value": '["closed", "true_positive"]',
-                #     },
-                #     "action_ts": "1719281272.742854",
-                # }
-
-                logger.info("Matched static select action", action=action)
-                child_wh = parse_child_webhook(url, [kv_params])
-
-                if not child_wh:
-                    raise HTTPException(
-                        status_code=status.HTTP_400_BAD_REQUEST,
-                        detail="Invalid child webhook URL",
-                    )
-                return ServiceCallbackAction(
-                    action="webhook",
-                    payload=child_wh["payload"],
-                    metadata={"path": child_wh["path"], "secret": child_wh["secret"]},
-                )
-
-            case {"type": action_type}:
-                logger.error(
-                    "Invalid action type", action_type=action_type, action=action
-                )
-                raise HTTPException(
-                    status_code=status.HTTP_400_BAD_REQUEST,
-                    detail=f"Invalid action type {action_type}",
-                )
-            case _:
-                logger.error("Invalid action", action=action)
-                raise HTTPException(
-                    status_code=status.HTTP_400_BAD_REQUEST,
-                    detail="Invalid action",
-                )
-    return None
-
-
 async def validate_incoming_webhook(
-    path: str, secret: str, request: Request, *, validate_method: bool = True
+    path: str, secret: str, request: Request
 ) -> WorkflowDefinition:
     """Validate incoming webhook request.
 
@@ -133,7 +46,7 @@ async def validate_incoming_webhook(
                 detail="Webhook is offline",
             )
 
-        if validate_method and webhook.method.lower() != request.method.lower():
+        if webhook.method.lower() != request.method.lower():
             logger.error("Method does not match")
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
@@ -181,3 +94,8 @@ async def validate_incoming_webhook(
             )
         )
         return validated_defn
+
+
+WorkflowDefinitionFromWebhook = Annotated[
+    WorkflowDefinition, Depends(validate_incoming_webhook)
+]
