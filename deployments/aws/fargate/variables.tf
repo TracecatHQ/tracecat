672ea9a969cfe3a0ef137e708e135aa31b41c1c5
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-10-18 11:20:11 -0700

    release: Bump 0.11.6

--- deployments/aws/fargate/variables.tf
+++ deployments/aws/fargate/variables.tf
@@ -55,7 +55,7 @@ variable "TFC_CONFIGURATION_VERSION_GIT_COMMIT_SHA" {
 
 variable "tracecat_image_tag" {
   type    = string
-  default = "0.11.5"
+  default = "0.11.6"
 }
 
 variable "use_git_commit_sha" {
