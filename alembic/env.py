9f88eee7ae217e4b2b2f5f4e980da1e8ce9728c9
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-10-18 11:16:45 -0700

    Revert "fix(engine): Add retries to recreate DB engine if test conn raises an exception (#433)"
    This reverts commit 9aa43b8a729172d6c1adecafd1ab66ac9b5f4ee4.

--- alembic/env.py
+++ alembic/env.py
@@ -1,40 +1,38 @@
-import asyncio
+import json
+import logging
 import os
 from logging.config import fileConfig
 
 import alembic_postgresql_enum  # noqa: F401
+import boto3
 from sqlalchemy import engine_from_config, pool
-from sqlalchemy.exc import OperationalError
 from sqlmodel import SQLModel
 
 from alembic import context
 from tracecat.db import schemas  # noqa: F401
-from tracecat.db.engine import fetch_db_password, get_connection_string
-from tracecat.logger import logger
 
-
-def _get_db_pass() -> str:
-    loop = asyncio.get_event_loop()
-    return loop.run_until_complete(fetch_db_password())
-
-
-def _get_db_uri() -> str:
+TRACECAT__DB_URI = os.getenv("TRACECAT__DB_URI")
+if not TRACECAT__DB_URI:
     username = os.getenv("TRACECAT__DB_USER", "postgres")
     host = os.getenv("TRACECAT__DB_ENDPOINT")
     port = os.getenv("TRACECAT__DB_PORT", 5432)
     database = os.getenv("TRACECAT__DB_NAME", "postgres")
-    password = _get_db_pass()
 
-    return get_connection_string(
-        username=username,
-        password=password,
-        host=host,
-        port=port,
-        database=database,
+    # Check if in AWS environment
+    if os.getenv("TRACECAT__DB_PASS__ARN"):
+        logging.info("Fetching database password from AWS Secrets Manager")
+        session = boto3.Session()
+        client = session.client("secretsmanager")
+        response = client.get_secret_value(SecretId=os.getenv("TRACECAT__DB_PASS__ARN"))
+        password = json.loads(response["SecretString"])["password"]
+    else:
+        logging.info("Fetching database password from environment variable")
+        password = os.getenv("TRACECAT__DB_PASS")
+
+    TRACECAT__DB_URI = (
+        f"postgresql+psycopg://{username}:{password}@{host}:{port!s}/{database}"
     )
 
-
-TRACECAT__DB_URI = os.getenv("TRACECAT__DB_URI") or _get_db_uri()
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -72,8 +70,6 @@ def run_migrations_offline() -> None:
 
 def run_migrations_online() -> None:
     """Run migrations in 'online' mode."""
-    global TRACECAT__DB_URI
-
     connectable = engine_from_config(
         configuration=config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
@@ -81,32 +77,14 @@ def run_migrations_online() -> None:
         url=TRACECAT__DB_URI,
     )
 
-    try:
-        with connectable.connect() as connection:
-            context.configure(connection=connection, target_metadata=target_metadata)
+    with connectable.connect() as connection:
+        context.configure(connection=connection, target_metadata=target_metadata)
 
-            with context.begin_transaction():
-                context.run_migrations()
-    except OperationalError as e:
-        logger.error("Error connecting to database, password may have rotated", error=e)
-        # Perhaps the database key has rotated. Try to get the password again.
-        password = _get_db_pass()
-        # If password hasn't changed, raise
-        if password in TRACECAT__DB_URI:
-            msg = "Database password has rotated, but new password not found in DB URI"
-            logger.error(msg)
-            raise ValueError(msg) from e
-        # Try to run migrations again with new password
-        TRACECAT__DB_URI = _get_db_uri()
-        return run_migrations_online()
-    except Exception as e:
-        logger.error("Unexpected error connecting to database", error=e)
-        raise
+        with context.begin_transaction():
+            context.run_migrations()
 
 
 if context.is_offline_mode():
-    logger.info("Running migrations offline")
     run_migrations_offline()
 else:
-    logger.info("Running migrations online")
     run_migrations_online()
