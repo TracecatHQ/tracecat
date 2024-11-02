cbe2b9233c42ee1f4f52cfc3846dfa1d19388f6e
topher-lo <46541035+topher-lo@users.noreply.github.com>
2024-11-02 11:37:48 -0700

    release: Bump 0.12.6

--- deployments/aws/fargate/variables.tf
+++ deployments/aws/fargate/variables.tf
@@ -55,7 +55,7 @@ variable "TFC_CONFIGURATION_VERSION_GIT_COMMIT_SHA" {
 
 variable "tracecat_image_tag" {
   type    = string
-  default = "0.12.5"
+  default = "0.12.6"
 }
 
 variable "use_git_commit_sha" {
