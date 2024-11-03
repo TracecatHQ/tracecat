c6d520ad496dfa283cddde56d467c13dd9198666
topher-lo <46541035+topher-lo@users.noreply.github.com>
2024-11-03 12:16:30 -0800

    Implemen jwt getters

--- registry/tracecat_registry/integrations/_http_types.py
+++
@@ -1,6 +0,0 @@
-"""HTTP types."""
-
-from typing import Any, Literal
-
-RequestMethods = Literal["GET", "POST", "PUT", "DELETE"]
-JSONObjectOrArray = dict[str, Any] | list[Any]
--- registry/tracecat_registry/integrations/limacharlie.py
+++ registry/tracecat_registry/integrations/limacharlie.py
@@ -1,17 +1,14 @@
-"""Generic interface to Limacharlie endpoints.
-
-Abstracts away JWT authentication logic.
+"""Limacharlie authentication.
 
 Docs: https://docs.limacharlie.io/apidocs/introduction
 """
 
-from typing import Annotated, Any
-
 import httpx
-from pydantic import Field
 
 from tracecat_registry import RegistrySecret, registry, secrets
-from tracecat_registry.integrations._http_types import JSONObjectOrArray, RequestMethods
+
+LIMACHARLIE_AUTH_URL = "https://jwt.limacharlie.io"
+
 
 limacharlie_secret = RegistrySecret(
     name="limacharlie",
@@ -28,49 +25,18 @@ limacharlie_secret = RegistrySecret(
     - `LIMACHARLIE_OID` (organization ID)
 """
 
-LIMACHARLIE_API_URL = "https://api.limacharlie.io/v1"
-LIMACHARLIE_AUTH_URL = "https://jwt.limacharlie.io"
-
 
 @registry.register(
-    default_title="Call Limacharlie API",
-    description="Call any Limacharlie API.",
+    default_title="Get Limacharlie JWT Token",
+    description="Get a JWT token for Limacharlie API calls.",
     namespace="integrations.limacharlie",
     secrets=[limacharlie_secret],
 )
-async def call_limacharlie_api(
-    endpoint: Annotated[str, Field(description="API endpoint to call.")],
-    headers: Annotated[
-        dict[str, str],
-        Field(description="HTTP request headers"),
-    ] = None,
-    payload: Annotated[
-        JSONObjectOrArray,
-        Field(description="HTTP request payload"),
-    ] = None,
-    params: Annotated[
-        dict[str, Any],
-        Field(description="URL query parameters"),
-    ] = None,
-    form_data: Annotated[
-        dict[str, Any],
-        Field(description="HTTP form encoded data"),
-    ] = None,
-    method: Annotated[
-        RequestMethods,
-        Field(description="HTTP request method"),
-    ] = "GET",
-) -> dict[str, Any]:
-    headers = headers or {}
-    payload = payload or {}
-    params = params or {}
-    form_data = form_data or {}
-
-    # Retrieve JWT token
+async def get_jwt_token() -> str:
     secret = await secrets.get("limacharlie")
     async with httpx.AsyncClient() as client:
         response = await client.get(
-            f"{LIMACHARLIE_AUTH_URL}",
+            LIMACHARLIE_AUTH_URL,
             params={
                 "uid": secret["LIMACHARLIE_UID"],
                 "secret": secret["LIMACHARLIE_SECRET"],
@@ -78,18 +44,4 @@ async def call_limacharlie_api(
             },
         )
         response.raise_for_status()
-        token = response.json()["jwt"]
-
-    headers["Authorization"] = f"Bearer {token}"
-
-    async with httpx.AsyncClient() as client:
-        response = await client.request(
-            method,
-            f"{LIMACHARLIE_API_URL}/{endpoint}",
-            headers=headers,
-            params=params,
-            json=payload,
-            form_data=form_data,
-        )
-        response.raise_for_status()
-        return response.json()
+        return response.json()["jwt"]
