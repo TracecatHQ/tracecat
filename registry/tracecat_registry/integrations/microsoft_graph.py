84ccb1576e1fccfe5c67d7035d7221f3df5d5992
Chris Lo <46541035+topher-lo@users.noreply.github.com>
2024-11-22 11:50:04 -0800

    feat(integrations)!: Use MSAL for microsoft graph token flow (#541)

--- registry/tracecat_registry/integrations/microsoft_graph.py
+++ registry/tracecat_registry/integrations/microsoft_graph.py
@@ -1,56 +1,72 @@
-"""Microsoft Graph authentication.
+"""Microsoft Graph authentication via MSAL Python library.
 
-Currently supports app-only authentication.
+Currently supports confidential app-only authentication (i.e. `acquire_token_for_client` method)
 
-Docs: https://learn.microsoft.com/en-us/graph/auth-v2-service
+Docs:
+-https://learn.microsoft.com/en-us/graph/auth-v2-service
+- https://msal-python.readthedocs.io/en/latest/#confidentialclientapplication
+- https://learn.microsoft.com/en-us/entra/msal/python/getting-started/acquiring-tokens#confidential-clients-interactive-token-acquisition
 """
 
-import httpx
+from msal import ConfidentialClientApplication
+from tracecat import __version__
 
 from tracecat_registry import RegistrySecret, registry, secrets
 
 microsoft_graph_secret = RegistrySecret(
     name="microsoft_graph",
     keys=[
-        "MICROSOFT_GRAPH_TENANT_ID",
         "MICROSOFT_GRAPH_CLIENT_ID",
         "MICROSOFT_GRAPH_CLIENT_SECRET",
     ],
-    optional_keys=["MICROSOFT_GRAPH_SCOPE"],
+    optional_keys=[
+        "MICROSOFT_GRAPH_SCOPE",
+        "MICROSOFT_TOKEN_AUTHORITY",
+        "MICROSOFT_OIDC_AUTHORITY",
+    ],
 )
 """Microsoft Graph API secret.
 
 - name: `microsoft_graph`
 - keys:
-    - `MICROSOFT_GRAPH_TENANT_ID`
     - `MICROSOFT_GRAPH_CLIENT_ID`
     - `MICROSOFT_GRAPH_CLIENT_SECRET`
 - optional_keys:
     - `MICROSOFT_GRAPH_SCOPE` (default: `https://graph.microsoft.com/.default`)
+    - `MICROSOFT_TOKEN_AUTHORITY` (default: `https://login.microsoftonline.com/common`)
+    - `MICROSOFT_OIDC_AUTHORITY`
 """
 
 
 @registry.register(
     default_title="Get Microsoft Graph auth token",
-    description="Get an auth token for Microsoft Graph API calls.",
+    description="Get an auth token for Microsoft Graph API calls from a confidential application.",
     display_group="Microsoft Graph",
     namespace="integrations.microsoft_graph",
     secrets=[microsoft_graph_secret],
 )
-async def get_auth_token() -> str:
-    auth_url = f"https://login.microsoftonline.com/{secrets.get('MICROSOFT_GRAPH_TENANT_ID')}/oauth2/v2.0/token"
-    async with httpx.AsyncClient() as client:
-        response = await client.post(
-            auth_url,
-            headers={"Content-Type": "application/x-www-form-urlencoded"},
-            data={
-                "client_id": secrets.get("MICROSOFT_GRAPH_CLIENT_ID"),
-                "client_secret": secrets.get("MICROSOFT_GRAPH_CLIENT_SECRET"),
-                "scope": secrets.get(
-                    "MICROSOFT_GRAPH_SCOPE", "https://graph.microsoft.com/.default"
-                ),
-                "grant_type": "client_credentials",
-            },
-        )
-        response.raise_for_status()
-        return response.json()["access_token"]
+def get_auth_token() -> str:
+    client_id = secrets.get("MICROSOFT_GRAPH_CLIENT_ID")
+    client_secret = secrets.get("MICROSOFT_GRAPH_CLIENT_SECRET")
+    scopes = secrets.get(
+        "MICROSOFT_GRAPH_SCOPE", "https://graph.microsoft.com/.default"
+    )
+    authority = secrets.get(
+        "MICROSOFT_TOKEN_AUTHORITY", "https://login.microsoftonline.com/common"
+    )
+    oidc_authority = secrets.get("MICROSOFT_OIDC_AUTHORITY")
+    app = ConfidentialClientApplication(
+        client_id=client_id,
+        client_credential=client_secret,
+        authority=authority,
+        oidc_authority=oidc_authority,
+        app_name="tracecat",
+        app_version=__version__,
+    )
+    result = app.acquire_token_for_client(scopes=[scopes])
+    if result is None:
+        raise ValueError("Failed to acquire token. Empty result returned.")
+    elif "access_token" in result:
+        return result["access_token"]
+    else:
+        raise ValueError(f"Failed to acquire token: {result}")
