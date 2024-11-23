a0b8e7fa670a694e8a5416806a37815160883ed6
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-11-23 08:06:23 -0800

    ci(infra): Add temporal client rpc timeout variable

--- deployments/aws/fargate/variables.tf
+++ deployments/aws/fargate/variables.tf
@@ -177,6 +177,12 @@ variable "temporal_memory" {
   default = "512"
 }
 
+variable "temporal_client_rpc_timeout" {
+  type        = string
+  description = "RPC timeout for Temporal client in seconds"
+  default     = "10"
+}
+
 variable "caddy_cpu" {
   type    = string
   default = "256"
