2e9c5b501af22cf7917da9c2058b52aee34d6289
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-11-27 23:36:47 -0800

    ui: Super basic intellisense working

--- frontend/src/components/editor.tsx
+++ frontend/src/components/editor.tsx
@@ -3,7 +3,7 @@ import {
   Editor as ReactMonacoEditor,
   type Monaco,
 } from "@monaco-editor/react"
-import { editor } from "monaco-editor"
+import { editor, languages } from "monaco-editor"
 
 import { cn } from "@/lib/utils"
 import { CenteredSpinner } from "@/components/loading/spinner"
@@ -46,6 +46,64 @@ export function CustomEditor({
       },
     })
     monaco.editor.setTheme("myCustomTheme")
+
+    // Register for yaml language
+    monaco.languages.registerCompletionItemProvider("yaml", {
+      triggerCharacters: ["$", "{", "{"],
+      provideCompletionItems: (model, position) => {
+        const wordUntilPosition = model.getWordUntilPosition(position)
+        const textUntilPosition = model.getValueInRange({
+          startLineNumber: position.lineNumber,
+          startColumn: 1,
+          endLineNumber: position.lineNumber,
+          endColumn: position.column,
+        })
+
+        console.group("Completion Provider")
+        console.log("Triggered by character at position:", position)
+        console.log("Text until position:", textUntilPosition)
+
+        const shouldShowSuggestions = textUntilPosition.endsWith("${{")
+
+        console.log("Should show suggestions:", shouldShowSuggestions)
+        console.groupEnd()
+
+        if (!shouldShowSuggestions) {
+          return { suggestions: [] }
+        }
+
+        const suggestions: languages.CompletionItem[] = [
+          {
+            label: "ACTIONS",
+            kind: monaco.languages.CompletionItemKind.Keyword,
+            insertText: "ACTIONS",
+            documentation: "Insert ACTIONS reference",
+            range: {
+              startLineNumber: position.lineNumber,
+              endLineNumber: position.lineNumber,
+              startColumn: wordUntilPosition.startColumn,
+              endColumn: position.column,
+            },
+          },
+          {
+            label: "FN",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "FN",
+            documentation: "Insert Function reference",
+            range: {
+              startLineNumber: position.lineNumber,
+              endLineNumber: position.lineNumber,
+              startColumn: wordUntilPosition.startColumn,
+              endColumn: position.column,
+            },
+          },
+        ]
+
+        return {
+          suggestions: suggestions,
+        }
+      },
+    })
   }
   return (
     <div className={cn("h-36", className)}>
@@ -54,6 +112,7 @@ export function CustomEditor({
         loading={<CenteredSpinner />}
         onMount={handleEditorDidMount}
         theme="myCustomTheme"
+        language="yaml"
         options={{
           tabSize: 2,
           minimap: { enabled: false },
