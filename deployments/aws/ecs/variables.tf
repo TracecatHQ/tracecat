74db2243e33b52de36b6edbf7d0788d1baf41dcc
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-12-03 00:54:56 -0800

    release: Bump 0.15.3

--- deployments/aws/ecs/variables.tf
+++ deployments/aws/ecs/variables.tf
@@ -108,7 +108,7 @@ variable "TFC_CONFIGURATION_VERSION_GIT_COMMIT_SHA" {
 
 variable "tracecat_image_tag" {
   type    = string
-  default = "0.15.2"
+  default = "0.15.3"
 }
 
 variable "use_git_commit_sha" {
