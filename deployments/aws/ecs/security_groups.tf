e270eae217eb379c331c6ab6615fba0df0e5b3a2
topher-lo <46541035+topher-lo@users.noreply.github.com>
2024-12-02 21:07:00 -0800

    Add vpc endpoint for secrets manager

--- deployments/aws/ecs/security_groups.tf
+++ deployments/aws/ecs/security_groups.tf
@@ -138,3 +138,25 @@ resource "aws_security_group" "temporal_db" {
   }
 
 }
+
+resource "aws_vpc_endpoint" "secretsmanager" {
+  vpc_id              = var.vpc_id
+  service_name        = "com.amazonaws.${var.aws_region}.secretsmanager"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = var.private_subnet_ids
+  security_group_ids  = [aws_security_group.secretsmanager_vpc_endpoint.id]
+  private_dns_enabled = true
+}
+
+resource "aws_security_group" "secretsmanager_vpc_endpoint" {
+  name        = "secretsmanager-vpc-endpoint"
+  description = "Security group for Secrets Manager VPC endpoint"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    security_groups = [aws_security_group.core.id]
+  }
+}
