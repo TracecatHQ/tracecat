85fd66f8b8835e97913fecaf6a91a35afc3c3061
topher-lo <46541035+topher-lo@users.noreply.github.com>
2024-12-05 17:19:55 -0800

    fix(ui): Redirect SAML

--- frontend/src/lib/ss-utils.ts
+++ frontend/src/lib/ss-utils.ts
@@ -1,32 +1,3 @@
-import { NextRequest } from "next/server"
-
-export const getDomain = (request: NextRequest) => {
-  // use env variable if set
-  if (process.env.NEXT_PUBLIC_APP_URL) {
-    console.log(
-      "Redirecting to NEXT_PUBLIC_APP_URL:",
-      process.env.NEXT_PUBLIC_APP_URL
-    )
-    return process.env.NEXT_PUBLIC_APP_URL
-  }
-
-  // next, try and build domain from headers
-  const requestedHost = request.headers.get("X-Forwarded-Host")
-  const requestedPort = request.headers.get("X-Forwarded-Port")
-  const requestedProto = request.headers.get("X-Forwarded-Proto")
-  if (requestedHost) {
-    const url = request.nextUrl.clone()
-    url.host = requestedHost
-    url.protocol = requestedProto || url.protocol
-    url.port = requestedPort || url.port
-    console.log("Redirecting to requestedHost", url.origin)
-    return url.origin
-  }
-
-  // finally just use whatever is in the request
-  return request.nextUrl.origin
-}
-
 export function buildUrl(path: string) {
   const url = process.env.NEXT_SERVER_API_URL || "http://api:8000"
   if (path.startsWith("/")) {
