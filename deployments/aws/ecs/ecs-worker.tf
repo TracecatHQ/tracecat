9ee0154985e0500dcec3c9eac7d5a8d3209cdd7d
topher-lo <46541035+topher-lo@users.noreply.github.com>
2024-12-05 19:07:13 -0800

    fix(infra): saml secrets arn policy

--- deployments/aws/ecs/ecs-worker.tf
+++ deployments/aws/ecs/ecs-worker.tf
@@ -30,7 +30,7 @@ resource "aws_ecs_task_definition" "worker_task_definition" {
         }
       }
       environment = local.worker_env
-      secrets     = local.tracecat_secrets
+      secrets     = local.tracecat_base_secrets
       dockerPullConfig = {
         maxAttempts = 3
         backoffTime = 30
