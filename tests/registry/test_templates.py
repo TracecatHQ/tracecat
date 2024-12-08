771d02527e1380c1ecc792bc75673061fd329a47
topher-lo <46541035+topher-lo@users.noreply.github.com>
2024-12-08 12:37:49 -0800

    test(registry): Test sync

--- tests/registry/test_templates.py
+++ tests/registry/test_templates.py
@@ -4,6 +4,14 @@ import pytest
 import yaml
 
 from tracecat.registry.actions.models import TemplateAction
+from tracecat.registry.repository import Repository
+
+
+@pytest.fixture(scope="module")
+def registry():
+    registry = Repository()
+    registry.init(include_base=False, include_templates=True)
+    return registry
 
 
 @pytest.mark.anyio
@@ -12,9 +20,15 @@ from tracecat.registry.actions.models import TemplateAction
     list(Path("registry/tracecat_registry/templates").rglob("*.yml")),
     ids=lambda path: str(path.parts[-2:]),
 )
-async def test_template_action_validation(file_path):
+async def test_template_action_validation(file_path, registry):
     with open(file_path) as file:
         template = yaml.safe_load(file)
+
+    # Test parsing
     action = TemplateAction(**template)
     assert action.type == "action"
     assert action.definition
+
+    # Test registration
+    registry.register_template_action(action)
+    assert action.definition.action in registry
