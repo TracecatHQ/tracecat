ae30af8d8f5fb9e82da8a4cad74ac9aaab2195a5
topher-lo <46541035+topher-lo@users.noreply.github.com>
2024-12-11 17:54:34 -0500

    revert(ci): cert validation resource name

--- deployments/aws/network/acm.tf
+++ deployments/aws/network/acm.tf
@@ -7,7 +7,7 @@ resource "aws_acm_certificate" "this" {
   }
 }
 
-resource "aws_route53_record" "this" {
+resource "aws_route53_record" "cert_validation" {
   for_each = {
     for dvo in aws_acm_certificate.this.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
@@ -26,5 +26,5 @@ resource "aws_route53_record" "this" {
 
 resource "aws_acm_certificate_validation" "this" {
   certificate_arn         = aws_acm_certificate.this.arn
-  validation_record_fqdns = [for record in aws_route53_record.this : record.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
