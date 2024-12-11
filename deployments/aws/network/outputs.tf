ded5876c729a0ac09811cfc51ed9191eeb67efa0
topher-lo <46541035+topher-lo@users.noreply.github.com>
2024-12-11 17:52:28 -0500

    revert(ci): Extra hosted zone for temporal

--- deployments/aws/network/outputs.tf
+++ deployments/aws/network/outputs.tf
@@ -10,10 +10,6 @@ output "private_subnet_ids" {
   value = aws_subnet.private[*].id
 }
 
-output "tracecat_acm_certificate_arn" {
-  value = aws_acm_certificate.tracecat.arn
-}
-
-output "temporal_ui_acm_certificate_arn" {
-  value = aws_acm_certificate.temporal_ui.arn
+output "acm_certificate_arn" {
+  value = aws_acm_certificate.this.arn
 }
--- deployments/aws/network/variables.tf
+++ deployments/aws/network/variables.tf
@@ -16,13 +16,3 @@ variable "hosted_zone_id" {
   type        = string
   description = "The hosted zone ID associated with the Tracecat domain"
 }
-
-variable "temporal_ui_domain_name" {
-  type        = string
-  description = "The domain name to use for the Temporal UI"
-}
-
-variable "temporal_ui_hosted_zone_id" {
-  type        = string
-  description = "The hosted zone ID associated with the Temporal UI domain"
-}
