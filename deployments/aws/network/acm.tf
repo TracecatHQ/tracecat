e1e9dd137f77d7f69d445d4ff106b7258968793d
topher-lo <46541035+topher-lo@users.noreply.github.com>
2024-12-11 17:52:28 -0500

    revert(ci): Extra hosted zone for temporal

--- deployments/aws/network/acm.tf
+++ deployments/aws/network/acm.tf
@@ -1,4 +1,4 @@
-resource "aws_acm_certificate" "tracecat" {
+resource "aws_acm_certificate" "this" {
   domain_name       = var.domain_name
   validation_method = "DNS"
 
@@ -7,9 +7,9 @@ resource "aws_acm_certificate" "tracecat" {
   }
 }
 
-resource "aws_route53_record" "tracecat" {
+resource "aws_route53_record" "this" {
   for_each = {
-    for dvo in aws_acm_certificate.tracecat.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.this.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
@@ -24,34 +24,7 @@ resource "aws_route53_record" "tracecat" {
   zone_id         = var.hosted_zone_id
 }
 
-resource "aws_acm_certificate_validation" "tracecat" {
-  certificate_arn         = aws_acm_certificate.tracecat.arn
-  validation_record_fqdns = [for record in aws_route53_record.tracecat : record.fqdn]
-}
-
-resource "aws_acm_certificate" "temporal_ui" {
-  domain_name       = var.temporal_ui_domain_name
-  validation_method = "DNS"
-}
-
-resource "aws_route53_record" "temporal_ui" {
-  for_each = {
-    for dvo in aws_acm_certificate.temporal_ui.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = var.temporal_ui_hosted_zone_id
-}
-
-resource "aws_acm_certificate_validation" "temporal_ui" {
-  certificate_arn         = aws_acm_certificate.temporal_ui.arn
-  validation_record_fqdns = [for record in aws_route53_record.temporal_ui : record.fqdn]
+resource "aws_acm_certificate_validation" "this" {
+  certificate_arn         = aws_acm_certificate.this.arn
+  validation_record_fqdns = [for record in aws_route53_record.this : record.fqdn]
 }
