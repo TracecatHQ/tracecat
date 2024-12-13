c8a9e39f07b0087536eb93cb9bab5847693e9c94
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-12-13 23:03:45 +0800

    test: Add executor run udf with secret test

--- tests/unit/test_executor.py
+++ tests/unit/test_executor.py
@@ -45,7 +45,7 @@ def mock_run_context():
     )
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 def mock_package(tmp_path):
     """Pytest fixture that creates a mock package with files and cleans up after the test."""
     import sys
@@ -77,7 +77,7 @@ def mock_package(tmp_path):
         del sys.modules["test_module"]
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 async def db_session_with_repo(test_role):
     """Fixture that creates a db session and temporary repository."""
 
@@ -97,6 +97,69 @@ async def db_session_with_repo(test_role):
 
 
 @pytest.mark.anyio
+async def test_executor_can_run_udf_with_secrets(
+    mock_package, test_role, db_session_with_repo, mock_run_context
+):
+    """Test that the executor can run a UDF with secrets.
+
+    A UDF should be able to pull secrets directly from the secrets store.
+    """
+
+    session, db_repo_id = db_session_with_repo
+    # Arrange
+    # 1. Register test udfs
+    repo = Repository()
+    repo._register_udfs_from_package(mock_package)
+
+    # Sanity check: Returns None because we haven't set secrets
+    assert repo.get("testing.fetch_secret").fn("TEST_UDF_SECRET_KEY") is None  # type: ignore
+
+    # 2. Add secrets
+    sec_service = SecretsService(session, role=test_role)
+    try:
+        await sec_service.create_secret(
+            SecretCreate(
+                name="test",
+                environment="default",
+                keys=[
+                    SecretKeyValue(
+                        key="TEST_UDF_SECRET_KEY",
+                        value=SecretStr("__SECRET_VALUE_UDF__"),
+                    )
+                ],
+            )
+        )
+
+        ra_service = RegistryActionsService(session, role=test_role)
+        await ra_service.create_action(
+            RegistryActionCreate.from_bound(
+                repo.get("testing.fetch_secret"), db_repo_id
+            )
+        )
+
+        input = RunActionInput(
+            task=ActionStatement(
+                ref="test",
+                action="testing.fetch_secret",
+                run_if=None,
+                for_each=None,
+                args={"secret_key_name": "TEST_UDF_SECRET_KEY"},
+            ),
+            exec_context=create_default_dsl_context(),
+            run_context=mock_run_context,
+        )
+
+        # Act
+        result = await executor.run_action_from_input(input)
+
+        # Assert
+        assert result == "__SECRET_VALUE_UDF__"
+    finally:
+        secret = await sec_service.get_secret_by_name("test", raise_on_error=True)
+        await sec_service.delete_secret_by_id(secret.id)
+
+
+@pytest.mark.anyio
 async def test_executor_can_run_template_action_with_secret(
     mock_package, test_role, db_session_with_repo, mock_run_context
 ):
@@ -113,76 +176,89 @@ async def test_executor_can_run_template_action_with_secret(
     assert repo.get("testing.add_100").fn(100) == 200  # type: ignore
 
     # Sanity check: Returns None because we haven't set secrets
-    assert repo.get("testing.fetch_secret").fn("KEY") is None  # type: ignore
+    assert repo.get("testing.fetch_secret").fn("TEST_TEMPLATE_SECRET_KEY") is None  # type: ignore
 
     # 2. Add secrets
     sec_service = SecretsService(session, role=test_role)
-    await sec_service.create_secret(
-        SecretCreate(
-            name="test",
-            environment="default",
-            keys=[SecretKeyValue(key="KEY", value=SecretStr("__SECRET_VALUE__"))],
+    try:
+        await sec_service.create_secret(
+            SecretCreate(
+                name="test",
+                environment="default",
+                keys=[
+                    SecretKeyValue(
+                        key="TEST_TEMPLATE_SECRET_KEY",
+                        value=SecretStr("__SECRET_VALUE__"),
+                    )
+                ],
+            )
         )
-    )
 
-    # Here, 'testing.template_action' wraps 'testing.fetch_secret'.
-    # It then returns the fetched secret
-    action = TemplateAction(
-        type="action",
-        definition=TemplateActionDefinition(
-            title="Test Action",
-            description="This is just a test",
-            name="template_action",
-            namespace="testing",
-            display_group="Testing",
-            expects={
-                "secret_key_name": ExpectedField(
-                    type="str",
-                    description="Secret name to fetch",
-                )
-            },
-            secrets=[],  # NOTE: We have no secrets at the template level
-            steps=[
-                ActionStep(
-                    ref="base",
-                    action="testing.fetch_secret",
-                    args={
-                        "secret_key_name": "${{ inputs.secret_key_name }}",
-                    },
-                )
-            ],
-            returns="${{ steps.base.result }}",
-        ),
-    )
+        # Here, 'testing.template_action' wraps 'testing.fetch_secret'.
+        # It then returns the fetched secret
+        action = TemplateAction(
+            type="action",
+            definition=TemplateActionDefinition(
+                title="Test Action",
+                description="This is just a test",
+                name="template_action",
+                namespace="testing",
+                display_group="Testing",
+                expects={
+                    "secret_key_name": ExpectedField(
+                        type="str",
+                        description="Secret name to fetch",
+                    )
+                },
+                secrets=[],  # NOTE: We have no secrets at the template level
+                steps=[
+                    ActionStep(
+                        ref="base",
+                        action="testing.fetch_secret",
+                        args={
+                            "secret_key_name": "${{ inputs.secret_key_name }}",
+                        },
+                    )
+                ],
+                returns="${{ steps.base.result }}",
+            ),
+        )
 
-    repo.register_template_action(action)
-    logger.info("REPO", store=repo.store.keys())
+        repo.register_template_action(action)
+        logger.info("REPO", store=repo.store.keys())
 
-    ra_service = RegistryActionsService(session, role=test_role)
-    await ra_service.create_action(
-        RegistryActionCreate.from_bound(repo.get("testing.template_action"), db_repo_id)
-    )
-    await ra_service.create_action(
-        RegistryActionCreate.from_bound(repo.get("testing.fetch_secret"), db_repo_id)
-    )
+        ra_service = RegistryActionsService(session, role=test_role)
+        await ra_service.create_action(
+            RegistryActionCreate.from_bound(
+                repo.get("testing.template_action"), db_repo_id
+            )
+        )
+        await ra_service.create_action(
+            RegistryActionCreate.from_bound(
+                repo.get("testing.fetch_secret"), db_repo_id
+            )
+        )
 
-    input = RunActionInput(
-        task=ActionStatement(
-            ref="test",
-            action="testing.template_action",
-            run_if=None,
-            for_each=None,
-            args={"secret_key_name": "KEY"},
-        ),
-        exec_context=create_default_dsl_context(),
-        run_context=mock_run_context,
-    )
+        input = RunActionInput(
+            task=ActionStatement(
+                ref="test",
+                action="testing.template_action",
+                run_if=None,
+                for_each=None,
+                args={"secret_key_name": "TEST_TEMPLATE_SECRET_KEY"},
+            ),
+            exec_context=create_default_dsl_context(),
+            run_context=mock_run_context,
+        )
 
-    # Act
-    result = await executor.run_action_from_input(input)
+        # Act
+        result = await executor.run_action_from_input(input)
 
-    # Assert
-    assert result == "__SECRET_VALUE__"
+        # Assert
+        assert result == "__SECRET_VALUE__"
+    finally:
+        secret = await sec_service.get_secret_by_name("test", raise_on_error=True)
+        await sec_service.delete_secret_by_id(secret.id)
 
 
 @pytest.mark.anyio
