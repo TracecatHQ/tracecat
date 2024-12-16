e6c4050b6affc3c509cf9cdc3da209fe39752690
topher-lo <46541035+topher-lo@users.noreply.github.com>
2024-12-15 19:39:07 -0800

    ci(infra): Allow temporal egress for secrets manager (temporary)

--- deployments/aws/ecs/security_groups.tf
+++ deployments/aws/ecs/security_groups.tf
@@ -188,6 +188,13 @@ resource "aws_security_group" "temporal" {
     protocol    = "tcp"
     self        = true
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_security_group" "temporal_db" {
