71f6a44fd4045516b596d5f65dfa0fb6b94c5dae
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2024-12-18 20:45:03 +0800

    Cleanup executions router

--- tracecat/workflow/executions/router.py
+++ tracecat/workflow/executions/router.py
@@ -37,16 +37,14 @@ async def list_workflow_executions(
     workflow_id: WorkflowID | None = Query(None),
 ) -> list[WorkflowExecutionResponse]:
     """List all workflow executions."""
-    with logger.contextualize(role=role):
-        service = await WorkflowExecutionsService.connect()
-        if workflow_id:
-            executions = await service.list_executions_by_workflow_id(workflow_id)
-        else:
-            executions = await service.list_executions()
-        return [
-            WorkflowExecutionResponse.from_dataclass(execution)
-            for execution in executions
-        ]
+    service = await WorkflowExecutionsService.connect(role=role)
+    if workflow_id:
+        executions = await service.list_executions_by_workflow_id(workflow_id)
+    else:
+        executions = await service.list_executions()
+    return [
+        WorkflowExecutionResponse.from_dataclass(execution) for execution in executions
+    ]
 
 
 @router.get("/{execution_id}", tags=["workflow-executions"])
@@ -55,10 +53,9 @@ async def get_workflow_execution(
     execution_id: UnquotedExecutionID,
 ) -> WorkflowExecutionResponse:
     """Get a workflow execution."""
-    with logger.contextualize(role=role):
-        service = await WorkflowExecutionsService.connect()
-        execution = await service.get_execution(execution_id)
-        return WorkflowExecutionResponse.from_dataclass(execution)
+    service = await WorkflowExecutionsService.connect(role=role)
+    execution = await service.get_execution(execution_id)
+    return WorkflowExecutionResponse.from_dataclass(execution)
 
 
 @router.get("/{execution_id}/history", tags=["workflow-executions"])
@@ -67,10 +64,9 @@ async def list_workflow_execution_event_history(
     execution_id: UnquotedExecutionID,
 ) -> list[EventHistoryResponse]:
     """Get a workflow execution."""
-    with logger.contextualize(role=role):
-        service = await WorkflowExecutionsService.connect()
-        events = await service.list_workflow_execution_event_history(execution_id)
-        return events
+    service = await WorkflowExecutionsService.connect(role=role)
+    events = await service.list_workflow_execution_event_history(execution_id)
+    return events
 
 
 @router.post("", tags=["workflow-executions"])
@@ -80,41 +76,40 @@ async def create_workflow_execution(
     session: AsyncSession = Depends(get_async_session),
 ) -> CreateWorkflowExecutionResponse:
     """Create and schedule a workflow execution."""
-    with logger.contextualize(role=role):
-        service = await WorkflowExecutionsService.connect()
-        # Get the dslinput from the workflow definition
-        try:
-            result = await session.exec(
-                select(WorkflowDefinition)
-                .where(WorkflowDefinition.workflow_id == params.workflow_id)
-                .order_by(WorkflowDefinition.version.desc())
-            )
-            defn = result.first()
-            if not defn:
-                raise NoResultFound("No workflow definition found for workflow ID")
-        except NoResultFound as e:
-            # No workflow associated with the webhook
-            logger.opt(exception=e).error("Invalid workflow ID", error=e)
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND, detail="Invalid workflow ID"
-            ) from e
-        dsl_input = DSLInput(**defn.content)
-        try:
-            response = service.create_workflow_execution_nowait(
-                dsl=dsl_input,
-                wf_id=params.workflow_id,
-                payload=params.inputs,
-            )
-            return response
-        except TracecatValidationError as e:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail={
-                    "type": "TracecatValidationError",
-                    "message": str(e),
-                    "detail": e.detail,
-                },
-            ) from e
+    service = await WorkflowExecutionsService.connect(role=role)
+    # Get the dslinput from the workflow definition
+    try:
+        result = await session.exec(
+            select(WorkflowDefinition)
+            .where(WorkflowDefinition.workflow_id == params.workflow_id)
+            .order_by(WorkflowDefinition.version.desc())  # type: ignore
+        )
+        defn = result.first()
+        if not defn:
+            raise NoResultFound("No workflow definition found for workflow ID")
+    except NoResultFound as e:
+        # No workflow associated with the webhook
+        logger.opt(exception=e).error("Invalid workflow ID", error=e)
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Invalid workflow ID"
+        ) from e
+    dsl_input = DSLInput(**defn.content)
+    try:
+        response = service.create_workflow_execution_nowait(
+            dsl=dsl_input,
+            wf_id=params.workflow_id,
+            payload=params.inputs,
+        )
+        return response
+    except TracecatValidationError as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail={
+                "type": "TracecatValidationError",
+                "message": str(e),
+                "detail": e.detail,
+            },
+        ) from e
 
 
 @router.post(
@@ -127,18 +122,17 @@ async def cancel_workflow_execution(
     execution_id: UnquotedExecutionID,
 ) -> None:
     """Get a workflow execution."""
-    with logger.contextualize(role=role):
-        service = await WorkflowExecutionsService.connect()
-        try:
-            await service.cancel_workflow_execution(execution_id)
-        except temporalio.service.RPCError as e:
-            if "workflow execution already completed" in e.message:
-                logger.info(
-                    "Workflow execution already completed, ignoring cancellation request",
-                )
-            else:
-                logger.error(e.message, error=e, execution_id=execution_id)
-                raise e
+    service = await WorkflowExecutionsService.connect(role=role)
+    try:
+        await service.cancel_workflow_execution(execution_id)
+    except temporalio.service.RPCError as e:
+        if "workflow execution already completed" in e.message:
+            logger.info(
+                "Workflow execution already completed, ignoring cancellation request",
+            )
+        else:
+            logger.error(e.message, error=e, execution_id=execution_id)
+            raise e
 
 
 @router.post(
@@ -152,17 +146,14 @@ async def terminate_workflow_execution(
     params: TerminateWorkflowExecutionParams,
 ) -> None:
     """Get a workflow execution."""
-    with logger.contextualize(role=role):
-        service = await WorkflowExecutionsService.connect()
-        try:
-            await service.terminate_workflow_execution(
-                execution_id, reason=params.reason
+    service = await WorkflowExecutionsService.connect(role=role)
+    try:
+        await service.terminate_workflow_execution(execution_id, reason=params.reason)
+    except temporalio.service.RPCError as e:
+        if "workflow execution already completed" in e.message:
+            logger.info(
+                "Workflow execution already completed, ignoring termination request",
             )
-        except temporalio.service.RPCError as e:
-            if "workflow execution already completed" in e.message:
-                logger.info(
-                    "Workflow execution already completed, ignoring termination request",
-                )
-            else:
-                logger.error(e.message, error=e, execution_id=execution_id)
-                raise e
+        else:
+            logger.error(e.message, error=e, execution_id=execution_id)
+            raise e
