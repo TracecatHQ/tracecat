47c9e527dd802458db418ac2d802b3bf57814fd1
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2025-01-09 15:46:31 +0000

    feat(ui): Make settings pages look more consistent (#720)

--- frontend/src/app/organization/settings/git/page.tsx
+++ frontend/src/app/organization/settings/git/page.tsx
@@ -1,25 +1,22 @@
 "use client"
 
-import { Separator } from "@/components/ui/separator"
 import { OrgSettingsGitForm } from "@/components/organization/org-settings-git"
 
 export default function GitSettingsPage() {
   return (
     <div className="size-full overflow-auto">
       <div className="container flex h-full max-w-[1000px] flex-col space-y-12">
-        <div className="space-y-8">
-          <div className="flex w-full">
-            <div className="items-start space-y-3 text-left">
-              <h2 className="text-2xl font-semibold tracking-tight">
-                Git Repository
-              </h2>
-              <p className="text-md text-muted-foreground">
-                View and manage your organization Git settings here.
-              </p>
-            </div>
+        <div className="flex w-full">
+          <div className="items-start space-y-3 text-left">
+            <h2 className="text-2xl font-semibold tracking-tight">
+              Git Repository
+            </h2>
+            <p className="text-md text-muted-foreground">
+              View and manage your organization Git settings here.
+            </p>
           </div>
-          <Separator />
         </div>
+
         <OrgSettingsGitForm />
       </div>
     </div>
