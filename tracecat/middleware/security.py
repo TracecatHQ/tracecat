cbe4366d212af5dd5231a9399d86aeb473ee4360
topher-lo <46541035+topher-lo@users.noreply.github.com>
2025-01-09 16:58:31 -0800

    Update class name

--- tracecat/middleware/security.py
+++ tracecat/middleware/security.py
@@ -5,11 +5,10 @@ from fastapi import Request
 from starlette.middleware.base import BaseHTTPMiddleware
 
 
-class SecurityMiddleware(BaseHTTPMiddleware):
+class SecurityHeadersMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         response = await call_next(request)
         
-        # Base CSP directives
         csp_directives = [
             "default-src 'self' blob: data:",
             "frame-ancestors 'none'"
