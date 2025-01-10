d72c6ed24bb004a5f334e0e4e9b17dec85843753
topher-lo <46541035+topher-lo@users.noreply.github.com>
2025-01-09 17:18:13 -0800

    Update directives

--- tracecat/middleware/security.py
+++ tracecat/middleware/security.py
@@ -3,16 +3,34 @@
 
 from fastapi import Request
 from starlette.middleware.base import BaseHTTPMiddleware
+import os
 
 
 class SecurityHeadersMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         response = await call_next(request)
-        
-        csp_directives = [
-            "default-src 'self' blob: data:",
-            "frame-ancestors 'none'"
-        ]
+        if os.getenv("POSTHOG_KEY"):
+            csp_directives = [
+                "connect-src 'self' https://*.posthog.com",
+                "default-src 'self'",
+                "worker-src 'self' blob:",
+                "frame-ancestors 'none'",
+                "img-src 'self' data:",
+                "object-src 'none'",
+                "script-src 'self' 'unsafe-inline' https://*.posthog.com",
+                "style-src 'self' 'unsafe-inline'",
+            ]
+        else:
+            csp_directives = [
+                "connect-src 'self'",
+                "default-src 'self'",
+                "worker-src 'self' blob:",
+                "frame-ancestors 'none'",
+                "img-src 'self' data:",
+                "object-src 'none'",
+                "script-src 'self' 'unsafe-inline'",
+                "style-src 'self' 'unsafe-inline'",
+            ]
         headers = {
             "Strict-Transport-Security": "max-age=7776000; includeSubDomains",
             "Content-Security-Policy": "; ".join(csp_directives),
@@ -20,6 +38,5 @@ class SecurityHeadersMiddleware(BaseHTTPMiddleware):
             "Referrer-Policy": "strict-origin-when-cross-origin",
             "Permissions-Policy": "document-domain=()"
         }
-        
         response.headers.update(headers)
         return response
