557379e8c40bdc87f9f0eea676bb211998d28fce
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2025-01-15 20:16:48 +0800

    Don't sync executor on start

--- tracecat/api/executor.py
+++ tracecat/api/executor.py
@@ -6,7 +6,6 @@ from fastapi.responses import ORJSONResponse
 
 from tracecat import config
 from tracecat.api.common import (
-    bootstrap_role,
     custom_generate_unique_id,
     generic_exception_handler,
     setup_oss_models,
@@ -16,9 +15,6 @@ from tracecat.executor.engine import setup_ray
 from tracecat.executor.router import router as executor_router
 from tracecat.logger import logger
 from tracecat.middleware import RequestLoggingMiddleware
-from tracecat.registry.repositories.service import RegistryReposService
-from tracecat.registry.repository import Repository
-from tracecat.settings.service import get_setting
 from tracecat.types.exceptions import TracecatException
 
 
@@ -28,47 +24,8 @@ async def lifespan(app: FastAPI):
         await setup_oss_models()
     except Exception as e:
         logger.error("Failed to preload OSS models", error=e)
-    try:
-        await setup_custom_remote_repository()
-    except Exception as e:
-        logger.error("Error setting up custom remote repository", exc=e)
-
     with setup_ray():
         yield
-
-
-async def setup_custom_remote_repository():
-    """Install the remote repository if it is set.
-
-    Steps
-    -----
-    1. Get the SHA of the remote repository from the DB
-    2. If it doesn't exist, create it
-    3. If it does exist, sync it
-    """
-    role = bootstrap_role()
-    url = await get_setting(
-        "git_repo_url",
-        role=role,
-        # TODO: Deprecate in future version
-        default=config.TRACECAT__REMOTE_REPOSITORY_URL,
-    )
-    if not url:
-        logger.info("Remote repository URL not set, skipping")
-        return
-    logger.info("Remote repository URL found", url=url)
-    async with RegistryReposService.with_session(role) as service:
-        db_repo = await service.get_repository(url)
-        # If it doesn't exist, do nothing
-        if db_repo is None:
-            logger.warning("Remote repository not found in DB, skipping")
-            return
-        # If it does exist, sync it
-        if db_repo.last_synced_at is None:
-            logger.info("Remote repository not synced, skipping")
-            return
-        repo = Repository(db_repo.origin, role=role)
-        await repo.load_from_origin(commit_sha=db_repo.commit_sha)
 
 
 def create_app(**kwargs) -> FastAPI:
