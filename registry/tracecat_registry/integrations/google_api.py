fda6d185974ad0ee2bddd009f8b420f4f991c979
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2025-01-20 12:17:37 +0000

    Ensure GOOGLE_API_CREDENTIALS is a mapping object

--- registry/tracecat_registry/integrations/google_api.py
+++ registry/tracecat_registry/integrations/google_api.py
@@ -3,8 +3,10 @@
 Docs: https://googleapis.dev/python/google-auth/latest/reference/google.oauth2.service_account.html
 """
 
+from collections.abc import Mapping
 from typing import Annotated
 
+import orjson
 from google.oauth2 import service_account
 from pydantic import Field
 
@@ -36,7 +38,12 @@ def get_auth_token(
     subject: Annotated[str, Field(..., description="Google API subject.")] = None,
 ) -> str:
     """Retrieve an auth token for Google API calls for a service account."""
-    creds = secrets.get("GOOGLE_API_CREDENTIALS")
+    creds_json_str = secrets.get("GOOGLE_API_CREDENTIALS")
+    creds = orjson.loads(creds_json_str)
+    if not isinstance(creds, Mapping):
+        raise ValueError(
+            "SECRETS.google_api.GOOGLE_API_CREDENTIALS is not a valid JSON string."
+        )
     credentials = service_account.Credentials.from_service_account_info(
         creds,
         scopes=scopes,
