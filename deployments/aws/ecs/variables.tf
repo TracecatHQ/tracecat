d1882a71c0051ce8747b0bfcda9f831e74590094
topher-lo <46541035+topher-lo@users.noreply.github.com>
2025-01-20 11:16:46 -0800

    release: 0.21.3

--- deployments/aws/ecs/variables.tf
+++ deployments/aws/ecs/variables.tf
@@ -103,7 +103,7 @@ variable "tracecat_ui_image" {
 
 variable "tracecat_image_tag" {
   type    = string
-  default = "0.21.2"
+  default = "0.21.3"
 }
 
 variable "temporal_server_image" {
--- deployments/aws/variables.tf
+++ deployments/aws/variables.tf
@@ -71,7 +71,7 @@ variable "tracecat_ui_image" {
 
 variable "tracecat_image_tag" {
   type    = string
-  default = "0.21.2"
+  default = "0.21.3"
 }
 
 variable "temporal_server_image" {
