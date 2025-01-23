bd0b3a2260a685ff321e32c3ff209f5cfb97a806
topher-lo <46541035+topher-lo@users.noreply.github.com>
2025-01-23 15:07:42 -0800

    Update jamf

--- registry/tracecat_registry/integrations/jamf.py
+++ registry/tracecat_registry/integrations/jamf.py
@@ -1,7 +1,4 @@
-"""Jamf Pro Authentication.
-
-Docs: https://developer.jamf.com/jamf-pro/docs/client-credentials
-"""
+"""Jamf Pro Authentication."""
 
 import httpx
 
@@ -9,29 +6,29 @@ from tracecat_registry import RegistrySecret, registry, secrets
 
 jamf_secret = RegistrySecret(
     name="jamf",
-    keys=["JAMF_BASE_URL", "JAMF_CLIENT_ID", "JAMF_CLIENT_SECRET"],
+    keys=["JAMF_CLIENT_ID", "JAMF_CLIENT_SECRET"],
 )
 """Jamf secret.
 
 - name: `jamf`
 - keys:
-    - `JAMF_BASE_URL`
     - `JAMF_CLIENT_ID`
     - `JAMF_CLIENT_SECRET`
 """
 
 
 @registry.register(
-    default_title="Get Jamf Pro auth token",
-    description="Get an auth token for Jamf Pro API calls.",
+    default_title="Get auth token",
+    description="Retrieve a bearer token for Jamf Pro API calls.",
     display_group="Jamf",
+    doc_url="https://developer.jamf.com/jamf-pro/docs/jamf-pro-api-overview#authentication-and-authorization",
     namespace="integrations.jamf",
     secrets=[jamf_secret],
 )
-async def get_auth_token() -> str:
+async def get_auth_token(base_url: str) -> str:
     async with httpx.AsyncClient() as client:
         response = await client.post(
-            f"{secrets.get('JAMF_BASE_URL')}/api/oauth/token",
+            f"{base_url}/api/oauth/token",
             headers={"Content-Type": "application/x-www-form-urlencoded"},
             data={
                 "client_id": secrets.get("JAMF_CLIENT_ID"),
