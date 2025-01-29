80a992100c5daf75219ca96a3f0570a92facc11e
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2025-01-29 23:47:53 +0000

    feat(engine): Add http polling action (#810)

--- tests/stress/server.py
+++ tests/stress/server.py
@@ -5,9 +5,12 @@ Use a reverse tunnel like bore.pub to expose this server for testing.
 """
 
 import json
+import os
 from pathlib import Path
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+
+from tracecat.logger import logger
 
 app = FastAPI()
 
@@ -22,6 +25,54 @@ def data():
     with Path(__file__).parent.joinpath("data.json").open() as f:
         x = json.load(f)
         return [x for _ in range(10)]
+
+
+counter = int(os.getenv("COUNTER", 3))
+
+
+@app.get("/poll-status")
+def simulate_job() -> dict[str, str]:
+    """Endpoint that simulates a long-running job by failing a few times before succeeding.
+
+    This endpoint is designed to test HTTP polling functionality:
+    - Returns 500 error for first 2 calls
+    - Returns success message on 3rd call
+    - Resets counter after success
+    """
+    global counter
+    logger.info("Simulating job", counter=counter)
+    if counter == 0:
+        logger.info("Simulating job success")
+        counter = 3
+        return {"message": "Hello world. I am the Test Stress Server."}
+    else:
+        counter -= 1
+        logger.info("Simulating job failure")
+        raise HTTPException(status_code=404, detail="Not found")
+
+
+@app.get("/poll-response")
+def simulate_job_2() -> dict[str, str]:
+    """Endpoint that simulates a long-running job by failing a few times before succeeding.
+
+    This endpoint is designed to test HTTP polling functionality:
+    - Returns 500 error for first 2 calls
+    - Returns success message on 3rd call
+    - Resets counter after success
+    """
+    global counter
+    logger.info("Simulating job", counter=counter)
+    if counter == 0:
+        logger.info("Simulating job success")
+        counter = 3
+        return {
+            "status": "success",
+            "data": "Hello world. I am the Test Stress Server.",
+        }
+    else:
+        counter -= 1
+        logger.info("Simulating job failure")
+        return {"status": "loading"}
 
 
 if __name__ == "__main__":
