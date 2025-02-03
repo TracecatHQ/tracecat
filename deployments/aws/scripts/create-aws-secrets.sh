a816c8053ab943db4a6712f776634cfbf9cdf563
topher-lo <46541035+topher-lo@users.noreply.github.com>
2025-02-03 19:16:16 +0000

    Simplify prefix in create-aws-secrets

--- deployments/aws/scripts/create-aws-secrets.sh
+++ deployments/aws/scripts/create-aws-secrets.sh
@@ -19,14 +19,14 @@ DB_ENCRYPTION_KEY=$(docker run --rm python:3.12-slim-bookworm /bin/bash -c "\
     python -c 'from cryptography.fernet import Fernet; print(Fernet.generate_key().decode())'")
 
 # Set default KEY_NAMES if not provided
-DB_ENCRYPTION_KEY_NAME=${DB_ENCRYPTION_KEY_NAME:-"${APP_ENV:-production}/tracecat/db-encryption-key"}
-SERVICE_KEY_NAME=${SERVICE_KEY_NAME:-"${APP_ENV:-production}/tracecat/service-key"}
-SIGNING_SECRET_NAME=${SIGNING_SECRET_NAME:-"${APP_ENV:-production}/tracecat/signing-secret"}
+DB_ENCRYPTION_KEY_NAME=${DB_ENCRYPTION_KEY_NAME:-"${APP_ENV:-prod}/tracecat/db-encryption-key"}
+SERVICE_KEY_NAME=${SERVICE_KEY_NAME:-"${APP_ENV:-prod}/tracecat/service-key"}
+SIGNING_SECRET_NAME=${SIGNING_SECRET_NAME:-"${APP_ENV:-prod}/tracecat/signing-secret"}
 
 # Create AWS Secrets
-aws secretsmanager create-secret --name "$DB_ENCRYPTION_KEY_NAME" --secret-string "$DB_ENCRYPTION_KEY" --region "$AWS_DEFAULT_REGION"
-aws secretsmanager create-secret --name "$SERVICE_KEY_NAME" --secret-string "$SERVICE_KEY" --region "$AWS_DEFAULT_REGION"
-aws secretsmanager create-secret --name "$SIGNING_SECRET_NAME" --secret-string "$SIGNING_SECRET" --region "$AWS_DEFAULT_REGION"
+aws secretsmanager create-secret --name "$DB_ENCRYPTION_KEY_NAME" --secret-string "$DB_ENCRYPTION_KEY" --region "$AWS_DEFAULT_REGION" --no-cli-pager
+aws secretsmanager create-secret --name "$SERVICE_KEY_NAME" --secret-string "$SERVICE_KEY" --region "$AWS_DEFAULT_REGION" --no-cli-pager
+aws secretsmanager create-secret --name "$SIGNING_SECRET_NAME" --secret-string "$SIGNING_SECRET" --region "$AWS_DEFAULT_REGION" --no-cli-pager
 
 echo "AWS Secrets created successfully."
 echo "DB_ENCRYPTION_KEY_NAME: $DB_ENCRYPTION_KEY_NAME"
