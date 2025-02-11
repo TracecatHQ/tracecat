24c4aa1d5036843323a35001e4e1ba0093f63218
topher-lo <46541035+topher-lo@users.noreply.github.com>
2025-02-11 08:41:39 -0800

    release: 0.23

--- deployments/aws/variables.tf
+++ deployments/aws/variables.tf
@@ -77,7 +77,7 @@ variable "tracecat_ui_image" {
 
 variable "tracecat_image_tag" {
   type    = string
-  default = "0.22.5"
+  default = "0.23.0"
 }
 
 variable "temporal_server_image" {
--- tracecat/__init__.py
+++ tracecat/__init__.py
@@ -1,3 +1,3 @@
 """Tracecat is the open source Tines / Splunk SOAR alternative."""
 
-__version__ = "0.22.5"
+__version__ = "0.23.0"
