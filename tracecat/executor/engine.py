7e6a20fcc0b3d244b063525c877edc9886fd4fa4
Chris Lo <46541035+topher-lo@users.noreply.github.com>
2025-02-20 17:44:23 -0800

    ci(infra): Add temporal and ray dashboards to docker prod (#881)

--- tracecat/executor/engine.py
+++ tracecat/executor/engine.py
@@ -6,20 +6,26 @@ import ray
 from tracecat.logger import logger
 
 EXECUTION_TIMEOUT = 300
-DEFAULT_NUM_WORKERS = min(
-    os.cpu_count() or 8, 8
-)  # Similar to multiprocessing.Pool's default
+DEFAULT_NUM_WORKERS = min(os.cpu_count() or 8, 8)
 
 
 @contextmanager
 def setup_ray():
-    ray.init(
+    context = ray.init(
         namespace="tracecat",
+        dashboard_host="0.0.0.0",
+        include_dashboard=True,
         num_cpus=DEFAULT_NUM_WORKERS,
         resources={"cpu": DEFAULT_NUM_WORKERS},
     )
     try:
-        logger.info("Ray initialized", resources=ray.cluster_resources())
+        logger.info(
+            "Connected to Ray cluster",
+            resources=ray.cluster_resources(),
+            dashboard_url=context.dashboard_url,
+            ray_version=context.ray_version,
+            ray_commit=context.ray_commit,
+        )
         yield
     finally:
         ray.shutdown()
