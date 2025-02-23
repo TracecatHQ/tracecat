670ae9efe51450f7e5b8d8aa773990ef4e5a2f4a
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2025-02-23 01:24:25 +0000

    Add tables routes

---
+++ frontend/src/app/workspaces/[workspaceId]/tables/layout.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import { Suspense } from "react"
+import { useWorkspace } from "@/providers/workspace"
+
+import { CenteredSpinner } from "@/components/loading/spinner"
+import { TablesSidebar } from "@/components/tables/tables-side-nav"
+
+export default function TablesLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const { workspaceId } = useWorkspace()
+  return (
+    <div className="container grid h-full grid-cols-6 gap-8 py-16">
+      <div className="col-span-1">
+        <TablesSidebar workspaceId={workspaceId} />
+      </div>
+      <div className="no-scrollbar col-span-5 flex size-full flex-col overflow-auto">
+        <Suspense fallback={<CenteredSpinner />}>{children}</Suspense>
+      </div>
+    </div>
+  )
+}
