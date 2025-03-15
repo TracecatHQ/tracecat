89da98c8d5f39ef13c8e98f954dd4d2bd5ddbb9c
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2025-03-15 16:13:59 +0800

    Add InteractionContext contextvar

--- tracecat/contexts.py
+++ tracecat/contexts.py
@@ -5,11 +5,16 @@ from contextvars import ContextVar
 import loguru
 
 from tracecat.dsl.models import RunContext
+from tracecat.ee.interactions.models import InteractionContext
 from tracecat.types.auth import Role
 
-ctx_run: ContextVar[RunContext] = ContextVar("run", default=None)
-ctx_role: ContextVar[Role] = ContextVar("role", default=None)
-ctx_logger: ContextVar[loguru.Logger] = ContextVar("logger", default=None)
+ctx_run: ContextVar[RunContext] = ContextVar("run", default=None)  # type: ignore
+ctx_role: ContextVar[Role] = ContextVar("role", default=None)  # type: ignore
+ctx_logger: ContextVar[loguru.Logger] = ContextVar("logger", default=None)  # type: ignore
+ctx_interaction: ContextVar[InteractionContext | None] = ContextVar(
+    "interaction", default=None
+)
+
 
 ctx_env: ContextVar[dict[str, str] | None] = ContextVar("env", default=None)
 
