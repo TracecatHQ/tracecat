64886c2ef557bb8b7153fbca9b93224772fb09b3
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2025-03-17 14:44:45 +0800

    Add 'transparent' variant to button

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -19,6 +19,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        transparent: "bg-transparent",
       },
       size: {
         default: "h-9 px-4 py-2",
