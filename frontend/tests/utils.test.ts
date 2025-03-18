60e8149108bc465ac7e8638eaf384fd5e8f2d903
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2025-03-18 13:46:13 +0800

    chore(ui): Drop unused utility functions

--- frontend/tests/utils.test.ts
+++ frontend/tests/utils.test.ts
@@ -1,12 +1,4 @@
-import {
-  getSlugFromActionKey,
-  groupBy,
-  isServer,
-  parseActionRunId,
-  slugify,
-  undoSlugify,
-  undoSlugifyNamespaced,
-} from "@/lib/utils"
+import { isServer, slugify, undoSlugify } from "@/lib/utils"
 
 describe("slugify", () => {
   it("should convert a string to a slug", () => {
@@ -19,64 +11,6 @@ describe("undoSlugify", () => {
   it("should convert a slug back to a string", () => {
     const string = undoSlugify("hello_world")
     expect(string).toBe("Hello World")
-  })
-})
-
-describe("undoSlugifyNamespaced", () => {
-  it("should convert a namespaced slug back to a string", () => {
-    const string = undoSlugifyNamespaced("namespace1.namespace2.hello_world")
-    expect(string).toBe("Namespace1 Namespace2 Hello World")
-  })
-})
-
-describe("getSlugFromActionKey", () => {
-  it("should extract the action slug from an action key", () => {
-    const slug = getSlugFromActionKey("actionId.actionSlug")
-    expect(slug).toBe("actionSlug")
-  })
-})
-
-describe("parseActionRunId", () => {
-  it("should parse an action run ID and return the specified field", () => {
-    const actionRunId = "ar:actionId.actionSlug:workflowRunId"
-    const actionId = parseActionRunId(actionRunId, "actionId")
-    const actionSlug = parseActionRunId(actionRunId, "actionSlug")
-    const workflowRunId = parseActionRunId(actionRunId, "workflowRunId")
-    expect(actionId).toBe("actionId")
-    expect(actionSlug).toBe("actionSlug")
-    expect(workflowRunId).toBe("workflowRunId")
-  })
-
-  it("should throw an error for an invalid field", () => {
-    const actionRunId = "ar:actionId.actionSlug:workflowRunId"
-    const invalidField = "invalid"
-    expect(() => parseActionRunId(actionRunId, invalidField as any)).toThrow(
-      new Error("Invalid field")
-    )
-  })
-})
-
-describe("groupBy", () => {
-  it("should group an array of objects by a specified key", () => {
-    const array = [
-      { category: "fruit", name: "apple" },
-      { category: "fruit", name: "banana" },
-      { category: "vegetable", name: "carrot" },
-      { category: "fruit", name: "orange" },
-      { category: "vegetable", name: "broccoli" },
-    ]
-    const grouped = groupBy(array, "category")
-    expect(grouped).toEqual({
-      fruit: [
-        { category: "fruit", name: "apple" },
-        { category: "fruit", name: "banana" },
-        { category: "fruit", name: "orange" },
-      ],
-      vegetable: [
-        { category: "vegetable", name: "carrot" },
-        { category: "vegetable", name: "broccoli" },
-      ],
-    })
   })
 })
 
