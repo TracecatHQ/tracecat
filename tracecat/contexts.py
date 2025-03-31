273fce59690d1573eac253ac2e642831c255dbfa
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2025-03-31 18:05:58 +0100

    Use base logger as default in ctx_logger

--- tracecat/contexts.py
+++ tracecat/contexts.py
@@ -6,6 +6,7 @@ import loguru
 
 from tracecat.dsl.models import RunContext
 from tracecat.ee.interactions.models import InteractionContext
+from tracecat.logger import logger as _default_logger
 from tracecat.types.auth import Role
 
 __all__ = [
@@ -18,7 +19,7 @@ __all__ = [
 
 ctx_run: ContextVar[RunContext] = ContextVar("run", default=None)  # type: ignore
 ctx_role: ContextVar[Role] = ContextVar("role", default=None)  # type: ignore
-ctx_logger: ContextVar[loguru.Logger] = ContextVar("logger", default=None)  # type: ignore
+ctx_logger: ContextVar[loguru.Logger] = ContextVar("logger", default=_default_logger)
 ctx_interaction: ContextVar[InteractionContext | None] = ContextVar(
     "interaction", default=None
 )
