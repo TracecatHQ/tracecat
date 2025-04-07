93a5c7768f2b03f551d65846a6521b8b00b315a4
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2025-04-07 14:41:06 -0700

    Add unknown and other to case priority

--- tracecat/cases/enums.py
+++ tracecat/cases/enums.py
@@ -2,10 +2,23 @@ from enum import StrEnum
 
 
 class CasePriority(StrEnum):
+    """Case priority values aligned with urgency levels.
+
+    Values:
+        UNKNOWN (0): No priority is assigned
+        LOW (1): Application or personal procedure is unusable, where a workaround is available or a repair is possible
+        MEDIUM (2): Non-critical function or procedure is unusable or hard to use causing operational disruptions with no direct impact on a service's availability. A workaround is available
+        HIGH (3): Critical functionality or network access is interrupted, degraded or unusable, having a severe impact on services availability. No acceptable alternative is possible
+        CRITICAL (4): Interruption making a critical functionality inaccessible or a complete network interruption causing a severe impact on services availability. There is no possible alternative
+        OTHER (99): The priority is not normalized
+    """
+
+    UNKNOWN = "unknown"
     LOW = "low"
     MEDIUM = "medium"
     HIGH = "high"
     CRITICAL = "critical"
+    OTHER = "other"
 
 
 class CaseSeverity(StrEnum):
