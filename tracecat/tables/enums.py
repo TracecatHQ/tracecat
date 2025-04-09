d1d84474ffc708267ec46b4d47df4f99aa659b7d
topher-lo <46541035+topher-lo@users.noreply.github.com>
2025-04-08 21:19:42 -0700

    Limit set of dtypes in enums

--- tracecat/tables/enums.py
+++ tracecat/tables/enums.py
@@ -5,12 +5,7 @@ class SqlType(StrEnum):
     """Supported SQL types."""
 
     TEXT = "TEXT"
-    VARCHAR = "VARCHAR"
     INTEGER = "INTEGER"
-    BIGINT = "BIGINT"
     DECIMAL = "DECIMAL"
     BOOLEAN = "BOOLEAN"
-    TIMESTAMP = "TIMESTAMP"
-    TIMESTAMPTZ = "TIMESTAMPTZ"
     JSONB = "JSONB"
-    UUID = "UUID"
