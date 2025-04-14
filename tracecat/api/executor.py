5f36b3acc30d01d0d2aa3a39eb0bb24ba715ae52
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2025-04-14 12:10:57 -0700

    Don't setup temporal from executor

--- tracecat/api/executor.py
+++ tracecat/api/executor.py
@@ -6,7 +6,6 @@ from fastapi.responses import ORJSONResponse
 
 from tracecat import config
 from tracecat.api.common import (
-    add_temporal_search_attributes,
     custom_generate_unique_id,
     generic_exception_handler,
     tracecat_exception_handler,
@@ -20,10 +19,6 @@ from tracecat.types.exceptions import TracecatException
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # Temporal
-    await add_temporal_search_attributes()
-
-    # App
     with setup_ray():
         yield
 
