c04965acdba07b6826ace7c01cbcc98728b993b7
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2025-04-16 18:19:47 -0700

    Show role in user nav

--- frontend/src/components/nav/user-nav.tsx
+++ frontend/src/components/nav/user-nav.tsx
@@ -15,6 +15,7 @@ import {
 import { siteConfig } from "@/config/site"
 import { userDefaults } from "@/config/user"
 import { useWorkspaceManager } from "@/lib/hooks"
+import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 import {
   DropdownMenu,
@@ -47,13 +48,21 @@ export default function UserNav() {
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56 p-2" align="end" forceMount>
         <DropdownMenuLabel className="font-normal">
-          <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">
-              {user?.first_name ?? userDefaults.name}
-            </p>
-            <p className="text-xs leading-none text-muted-foreground">
-              {user?.email.toString() ?? userDefaults.email}
-            </p>
+          <div className="flex items-center justify-between">
+            <div className="flex flex-col space-y-1">
+              <p className="text-sm font-medium leading-none">
+                {user?.first_name ?? userDefaults.name}
+              </p>
+              <p className="text-xs leading-none text-muted-foreground">
+                {user?.email.toString() ?? userDefaults.email}
+              </p>
+            </div>
+            <Badge
+              variant="secondary"
+              className="pointer-events-none font-medium capitalize"
+            >
+              {user?.role}
+            </Badge>
           </div>
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
