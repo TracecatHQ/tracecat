24186e8a8748b3c41f4d2975ba77c173f443100e
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2025-04-17 21:56:26 -0700

    Update react query + add devtools

--- frontend/src/providers/query.tsx
+++ frontend/src/providers/query.tsx
@@ -2,6 +2,7 @@
 
 import { ReactNode, useState } from "react"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
 export const DefaultQueryClientProvider = ({
   children,
@@ -10,5 +11,12 @@ export const DefaultQueryClientProvider = ({
 }) => {
   const [client] = useState(new QueryClient())
 
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      {/* Only included in production
+      https://tanstack.com/query/latest/docs/framework/react/devtools#install-and-import-the-devtools /> */}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
