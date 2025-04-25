96ce0379cfb90288766c9cb3ccdb23225b0bdf1a
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2025-04-25 14:29:48 -0700

    Derive user avatar from first name then email

--- frontend/src/components/user-avatar.tsx
+++ frontend/src/components/user-avatar.tsx
@@ -1,15 +1,27 @@
+import { UserRead } from "@/client"
+import { UserIcon } from "lucide-react"
+
 import { cn } from "@/lib/utils"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 interface UserAvatarProps extends React.HTMLAttributes<HTMLElement> {
   src?: string
   alt?: string
+  user?: UserRead | null
 }
-export default function UserAvatar({ src, alt, className }: UserAvatarProps) {
+export default function UserAvatar({
+  src,
+  alt,
+  user,
+  className,
+}: UserAvatarProps) {
+  const initials = user?.first_name
+    ? `${user.first_name[0]}`.toUpperCase()
+    : user?.email[0].toUpperCase()
   return (
     <Avatar className={cn("size-8", className)}>
       <AvatarImage src={src} alt={alt} />
-      <AvatarFallback>TC</AvatarFallback>
+      <AvatarFallback>{initials}</AvatarFallback>
     </Avatar>
   )
 }
