102c6e770bd091e808b5e31688cabeed86810675
topher-lo <46541035+topher-lo@users.noreply.github.com>
2025-05-11 18:45:02 -0700

    docs: Update child workflow signature

--- registry/tracecat_registry/core/workflow.py
+++ registry/tracecat_registry/core/workflow.py
@@ -9,7 +9,7 @@ from tracecat_registry import ActionIsInterfaceError, registry
 @registry.register(
     namespace="core.workflow",
     description="Execute a child workflow. The child workflow inherits the parent's execution context.",
-    default_title="Execute Child Workflow",
+    default_title="Execute child workflow",
     display_group="Workflows",
 )
 async def execute(
@@ -17,23 +17,23 @@ async def execute(
     workflow_id: Annotated[
         AnyWorkflowID | None,
         Doc(
-            "The ID of the child workflow to execute. Must be provided if workflow_alias is not provided.",
+            "ID of the child workflow to execute. Must be provided if workflow_alias is not provided.",
         ),
     ] = None,
     workflow_alias: Annotated[
         str | None,
         Doc(
-            "The alias of the child workflow to execute. Must be provided if workflow_id is not provided.",
+            "Alias of the child workflow to execute. Must be provided if workflow_id is not provided.",
         ),
     ] = None,
     trigger_inputs: Annotated[
         dict[str, Any] | None,
-        Doc("The inputs to pass to the child workflow."),
+        Doc("Inputs to pass to the child workflow."),
     ] = None,
     environment: Annotated[
         str | None,
         Doc(
-            "The child workflow's target execution environment. "
+            "Child workflow's target execution environment. "
             "This is used to isolate secrets across different environments."
             "If not provided, the child workflow's default environment is used. "
         ),
@@ -41,21 +41,21 @@ async def execute(
     timeout: Annotated[
         float | None,
         Doc(
-            "The maximum number of seconds to wait for the child workflow to complete. "
+            "Maximum number of seconds to wait for the child workflow to complete. "
             "If not provided, the child workflow's default timeout is used. "
         ),
     ] = None,
     version: Annotated[
         int | None,
-        Doc("The version of the child workflow definition, if any."),
+        Doc("Version of the child workflow definition, if any."),
     ] = None,
     loop_strategy: Annotated[
         Literal["parallel", "batch", "sequential"],
-        Doc("The execution strategy to use for the child workflow."),
+        Doc("Execution strategy to use for the child workflow."),
     ] = "batch",
     batch_size: Annotated[
         int,
-        Doc("The number of child workflows to execute in parallel."),
+        Doc("Number of child workflows to execute in parallel."),
     ] = 32,
     fail_strategy: Annotated[
         Literal["isolated", "all"],
