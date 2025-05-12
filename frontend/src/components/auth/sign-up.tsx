8fd3d19c6df3f1f4eb25095e0e45cf3288693b1c
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2025-05-12 18:47:25 +0100

    fix(app): Use env vars when auth allowed domains list setting is empty list (#1120)

--- frontend/src/components/auth/sign-up.tsx
+++ frontend/src/components/auth/sign-up.tsx
@@ -133,7 +133,7 @@ export function BasicRegistrationForm() {
       })
     } catch (error) {
       if (error instanceof ApiError) {
-        console.error("ApiError registering user", error)
+        console.log("ApiError registering user", error)
         const apiError = error as TracecatApiError
 
         // Handle both string and object error details
@@ -172,7 +172,7 @@ export function BasicRegistrationForm() {
             }
           } else {
             // Handle RequestValidationError case
-            console.error("Validation error", detail)
+            console.log("Validation error", detail)
             form.setError("email", {
               message: detail[0].msg || "Unknown error",
             })
