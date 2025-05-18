f39e4649f86ce5bfad778f29ac1acd32d842a8e1
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2025-05-18 20:05:32 +0100

    fix(app): Fix logging in Temproal connection step (#1137)

--- tracecat/dsl/client.py
+++ tracecat/dsl/client.py
@@ -56,12 +56,11 @@ async def connect_to_temporal() -> Client:
     runtime = None
     # TODO: fix https://github.com/prometheus/client_python/issues/155
     if TEMPORAL__METRICS_PORT:
+        logger.info("Initializing Prometheus runtime", port=TEMPORAL__METRICS_PORT)
         try:
             runtime = init_runtime_with_prometheus(port=int(TEMPORAL__METRICS_PORT))
         except Exception as e:
-            logger.warning(
-                "Failed to initialize Prometheus runtime: %s", e, exc_info=True
-            )
+            logger.warning("Failed to initialize Prometheus runtime", error=e)
     client = await Client.connect(
         target_host=TEMPORAL__CLUSTER_URL,
         namespace=TEMPORAL__CLUSTER_NAMESPACE,
