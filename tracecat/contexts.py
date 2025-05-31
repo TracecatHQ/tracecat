56dbb2a7aaab1b686a9400c016953d021ff2e1a4
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2025-05-31 15:40:42 +0100

    Add scope ID contextvar

--- tracecat/contexts.py
+++ tracecat/contexts.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from contextvars import ContextVar
+from typing import TYPE_CHECKING
 
 import loguru
 
@@ -8,12 +9,16 @@ from tracecat.dsl.models import RunContext
 from tracecat.ee.interactions.models import InteractionContext
 from tracecat.types.auth import Role
 
+if TYPE_CHECKING:
+    from tracecat.dsl.scheduler import ScopeID
+
 __all__ = [
     "ctx_run",
     "ctx_role",
     "ctx_logger",
     "ctx_interaction",
     "get_env",
+    "ctx_scope_id",
 ]
 
 ctx_run: ContextVar[RunContext] = ContextVar("run", default=None)  # type: ignore
@@ -22,6 +27,7 @@ ctx_logger: ContextVar[loguru.Logger] = ContextVar("logger", default=None)  # ty
 ctx_interaction: ContextVar[InteractionContext | None] = ContextVar(
     "interaction", default=None
 )
+ctx_scope_id: ContextVar[ScopeID | None] = ContextVar("scope_id", default=None)
 
 
 ctx_env: ContextVar[dict[str, str] | None] = ContextVar("env", default=None)
