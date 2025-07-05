6b259807ae606f009308732da79c9c7898f1f92e
topher-lo <46541035+topher-lo@users.noreply.github.com>
2025-07-05 10:05:40 -0700

    tmp

--- alembic/versions/9a001807d27b_add_case_attachments.py
+++ alembic/versions/9a001807d27b_add_case_attachments.py
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "9a001807d27b"
-down_revision: str | None = "4c1a69e5b34b"
+down_revision: str | None = "89a8d57c3608"
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
 
