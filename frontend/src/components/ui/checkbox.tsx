9fc24585fe0e367ec94857d997704145b0b1db39
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2025-07-12 23:06:47 -0400

    stabilize checkbox position

--- frontend/src/components/ui/checkbox.tsx
+++ frontend/src/components/ui/checkbox.tsx
@@ -13,14 +13,15 @@ const Checkbox = React.forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      "peer size-4 shrink-0 rounded-sm border border-muted-foreground shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
+      `peer size-4 shrink-0 align-middle rounded-sm border border-muted-foreground shadow
+     focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring
+     disabled:cursor-not-allowed disabled:opacity-50
+     data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground`,
       className
     )}
     {...props}
   >
-    <CheckboxPrimitive.Indicator
-      className={cn("flex items-center justify-center text-current")}
-    >
+    <CheckboxPrimitive.Indicator className="flex items-center justify-center text-current">
       <CheckIcon className="size-4" />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
