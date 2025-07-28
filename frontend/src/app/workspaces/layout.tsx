b2154d677b68f9c19a3c3a9454ea5ab21801e1fc
topher-lo <46541035+topher-lo@users.noreply.github.com>
2025-07-28 11:48:12 -0700

    fix(ui): Inset not h-screen

--- frontend/src/app/workspaces/layout.tsx
+++ frontend/src/app/workspaces/layout.tsx
@@ -83,7 +83,7 @@ function WorkspaceChildren({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <SidebarInset className="h-screen">
+      <SidebarInset>
         <div className="flex h-full flex-1 flex-col">
           <ControlsHeader />
           <div className="flex-1 overflow-y-scroll">{children}</div>
