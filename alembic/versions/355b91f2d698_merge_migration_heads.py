95f1defa434eba973d545901668a49c860216715
Jordan Umusu <jordanumusu@gmail.com>
2025-10-22 11:57:32 -0400

    fix: migrations merge

---
+++ alembic/versions/355b91f2d698_merge_migration_heads.py
@@ -0,0 +1,28 @@
+"""merge migration heads
+
+Revision ID: 355b91f2d698
+Revises: 1f6f60502c17, d4fd132ccb50
+Create Date: 2025-10-22 11:47:08.705701
+
+"""
+
+from collections.abc import Sequence
+
+from alembic import op
+import sqlalchemy as sa
+import sqlmodel.sql.sqltypes
+
+
+# revision identifiers, used by Alembic.
+revision: str = "355b91f2d698"
+down_revision: str | None = ("1f6f60502c17", "d4fd132ccb50")
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
