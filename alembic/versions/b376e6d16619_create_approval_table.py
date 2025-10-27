430a32efbed88a732a5534c25459fd95ff429374
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2025-10-27 16:24:41 -0400

    rebase migration
    fix

--- alembic/versions/b376e6d16619_create_approval_table.py
+++ alembic/versions/b376e6d16619_create_approval_table.py
@@ -16,7 +16,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "b376e6d16619"
-down_revision: str | None = "f04f005837c9"
+down_revision: str | None = "2d6eadcf4976"
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
 
