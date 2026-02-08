563c6367ae7f577846c6c57199d336b0db447cb2
topher-lo <46541035+topher-lo@users.noreply.github.com>
2026-02-08 14:22:36 -0500

    chore(infra): Update Tracecat image tag to 1.0.0-beta.4

--- deployments/terraform/aws/modules/eks/variables.tf
+++ deployments/terraform/aws/modules/eks/variables.tf
@@ -119,7 +119,7 @@ variable "tracecat_version" {
 variable "tracecat_image_tag" {
   description = "Docker image tag for Tracecat services"
   type        = string
-  default     = "latest"
+  default     = "1.0.0-beta.4"
 }
 
 variable "tracecat_ingress_split" {
