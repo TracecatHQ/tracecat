4b53421725860f9083288656a3dacf268e4f1129
topher-lo <46541035+topher-lo@users.noreply.github.com>
2026-02-17 11:52:32 -0500

    revert(settings): roll back app settings limit validation

--- tracecat/settings/schemas.py
+++ tracecat/settings/schemas.py
@@ -3,7 +3,6 @@ from typing import Any
 
 from pydantic import BaseModel, Field, ValidationInfo, field_validator
 
-from tracecat import config
 from tracecat.git.constants import GIT_SSH_URL_REGEX
 
 
@@ -77,10 +76,7 @@ class AppSettingsRead(BaseSettingsGroup):
     """Settings for the app."""
 
     app_registry_validation_enabled: bool
-    app_executions_query_limit: int = Field(
-        ge=config.TRACECAT__LIMIT_MIN,
-        le=config.TRACECAT__LIMIT_WORKFLOW_EXECUTIONS_MAX,
-    )
+    app_executions_query_limit: int
     app_interactions_enabled: bool
     app_workflow_export_enabled: bool
     app_create_workspace_on_register: bool
@@ -95,9 +91,7 @@ class AppSettingsUpdate(BaseSettingsGroup):
         default=False, description="Whether registry validation is enabled."
     )
     app_executions_query_limit: int = Field(
-        default=config.TRACECAT__LIMIT_WORKFLOW_EXECUTIONS_DEFAULT,
-        ge=config.TRACECAT__LIMIT_MIN,
-        le=config.TRACECAT__LIMIT_WORKFLOW_EXECUTIONS_MAX,
+        default=100,
         description="The maximum number of executions to return in a single query.",
     )
     app_interactions_enabled: bool = Field(
