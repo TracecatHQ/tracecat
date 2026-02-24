ba85dc750b1d52e4c303eb17ffac7ccbdcbb852f
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2026-02-23 21:15:02 -0500

    fix(db): reparent workflow git sync migration

--- alembic/versions/672fecea1d32_add_workflow_git_sync_branch.py
+++ alembic/versions/672fecea1d32_add_workflow_git_sync_branch.py
@@ -1,7 +1,7 @@
 """add workflow git sync branch
 
 Revision ID: 672fecea1d32
-Revises: 8bec3e244487
+Revises: 2410092f4ce4
 Create Date: 2026-02-20 22:39:21.867394
 
 """
@@ -14,7 +14,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "672fecea1d32"
-down_revision: str | None = "8bec3e244487"
+down_revision: str | None = "2410092f4ce4"
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
 
