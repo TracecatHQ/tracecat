c548c2ee28e8e8548685fed1b78e8f3e530d14d5
Chris Lo <46541035+topher-lo@users.noreply.github.com>
2026-02-25 16:20:31 -0500

    release: 1.0.0-beta.20 (#2189)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- deployments/fargate/modules/ecs/variables.tf
+++ deployments/fargate/modules/ecs/variables.tf
@@ -108,7 +108,7 @@ variable "tracecat_ui_image" {
 
 variable "tracecat_image_tag" {
   type    = string
-  default = "1.0.0-beta.19"
+  default = "1.0.0-beta.20"
 }
 
 variable "temporal_server_image" {
--- packages/tracecat-registry/tracecat_registry/__init__.py
+++ packages/tracecat-registry/tracecat_registry/__init__.py
@@ -1,6 +1,6 @@
 """Tracecat managed actions and integrations registry."""
 
-__version__ = "1.0.0-beta.19"
+__version__ = "1.0.0-beta.20"
 
 
 from tracecat_registry import types
