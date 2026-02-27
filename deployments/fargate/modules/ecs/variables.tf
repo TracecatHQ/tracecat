e4d445bc6f7f43bb09cce6725424d68e3c3b30fd
Chris Lo <46541035+topher-lo@users.noreply.github.com>
2026-02-27 18:38:38 -0500

    release: 1.0.0-beta.23 (#2223)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- deployments/fargate/modules/ecs/variables.tf
+++ deployments/fargate/modules/ecs/variables.tf
@@ -108,7 +108,7 @@ variable "tracecat_ui_image" {
 
 variable "tracecat_image_tag" {
   type    = string
-  default = "1.0.0-beta.22"
+  default = "1.0.0-beta.23"
 }
 
 variable "temporal_server_image" {
