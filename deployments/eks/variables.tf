a9b752c55c44c3cdb0dd2a74d5d211513860cd2f
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2026-03-11 13:23:14 -0400

    release: 1.0.0-beta.30

--- deployments/eks/variables.tf
+++ deployments/eks/variables.tf
@@ -156,13 +156,13 @@ variable "spot_node_max_size" {
 variable "tracecat_image_tag" {
   description = "Docker image tag for Tracecat services"
   type        = string
-  default     = "1.0.0-beta.29"
+  default     = "1.0.0-beta.30"
 }
 
 variable "tracecat_chart_version" {
   description = "Helm chart version for Tracecat (from OCI registry)"
   type        = string
-  default     = "0.3.39"
+  default     = "0.3.40"
 }
 
 variable "tracecat_ingress_split" {
