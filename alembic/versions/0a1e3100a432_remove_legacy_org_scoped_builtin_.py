f3a3bebe0e7ec711a78f1e968f83927b54dc9d29
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2026-03-11 21:09:48 -0400

    fix: resolve alembic head divergence

--- alembic/versions/0a1e3100a432_remove_legacy_org_scoped_builtin_.py
+++ alembic/versions/0a1e3100a432_remove_legacy_org_scoped_builtin_.py
@@ -14,7 +14,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "0a1e3100a432"
-down_revision: str | None = "6171727be56a"
+down_revision: str | None = "cd84c08340a5"
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
 
