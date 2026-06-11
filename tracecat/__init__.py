ad0bf5deb2947863ed2e61885cb2bac0e6215ac8
Jordan Umusu <jordan@tracecat.com>
2026-06-11 10:01:56 -0400

    release: 1.0.0-beta.49-rc.5

--- tracecat/__init__.py
+++ tracecat/__init__.py
@@ -1,4 +1,4 @@
 """Tracecat is open source AI automation platform for mission critical workflows."""
 
-__version__ = "1.0.0-beta.48"
-__pep440_version__ = "1.0.0b48"
+__version__ = "1.0.0-beta.49-rc.5"
+__pep440_version__ = "1.0.0b49+rc.5"
