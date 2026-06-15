bdd0867b867dacc5d603a1b92afac9c6001ef63a
Jordan Umusu <jordan@tracecat.com>
2026-06-15 11:23:35 -0400

    release: 1.0.0-beta.49

--- deployments/fargate/modules/ecs/variables.tf
+++ deployments/fargate/modules/ecs/variables.tf
@@ -167,7 +167,7 @@ variable "tracecat_ui_image" {
 
 variable "tracecat_image_tag" {
   type    = string
-  default = "1.0.0-beta.48"
+  default = "1.0.0-beta.49"
 }
 
 variable "tracecat_migrations_image" {
