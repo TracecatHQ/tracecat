68e1d78d272d4e6f41bded35cde9d59564ec9542
Chris Lo <46541035+topher-lo@users.noreply.github.com>
2026-06-16 17:16:02 -0700

    fix(fargate): order agent executor after litellm

--- deployments/fargate/modules/ecs/ecs-agent-executor.tf
+++ deployments/fargate/modules/ecs/ecs-agent-executor.tf
@@ -65,6 +65,7 @@ resource "aws_ecs_service" "tracecat_agent_executor" {
 
   depends_on = [
     aws_ecs_service.temporal_service,
-    aws_ecs_service.tracecat_api
+    aws_ecs_service.tracecat_api,
+    aws_ecs_service.tracecat_litellm
   ]
 }
