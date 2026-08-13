68bf78d30473a35dd6869d91f3b29bb52fc21f9a
Daryl Lim <5508348+daryllimyt@users.noreply.github.com>
2026-08-13 15:05:59 -0400

    release: 1.0.0-beta.52-rc.5

--- tracecat/__init__.py
+++ tracecat/__init__.py
@@ -1,4 +1,4 @@
 """Tracecat is open source AI automation platform for mission critical workflows."""
 
-__version__ = "1.0.0-beta.50"
-__pep440_version__ = "1.0.0b50"
+__version__ = "1.0.0-beta.52-rc.5"
+__pep440_version__ = "1.0.0b52+rc.5"
