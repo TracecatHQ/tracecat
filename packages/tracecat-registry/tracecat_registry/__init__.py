463b350daafedfc210727a220743fae0f04eb5fa
Aaron Lee <aaron@tracecat.com>
2026-09-24 17:50:23 -0400

    release: 1.1.0-alpha.3

--- packages/tracecat-registry/tracecat_registry/__init__.py
+++ packages/tracecat-registry/tracecat_registry/__init__.py
@@ -1,7 +1,7 @@
 """Tracecat managed actions and integrations registry."""
 
-__version__ = "1.0.0"
-__pep440_version__ = "1.0.0"
+__version__ = "1.1.0-alpha.3"
+__pep440_version__ = "1.1.0a3"
 
 
 from tracecat_registry import types
--- tracecat/__init__.py
+++ tracecat/__init__.py
@@ -1,4 +1,4 @@
 """Tracecat is open source AI automation platform for mission critical workflows."""
 
-__version__ = "1.0.0"
-__pep440_version__ = "1.0.0"
+__version__ = "1.1.0-alpha.3"
+__pep440_version__ = "1.1.0a3"
